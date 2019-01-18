ef10448acb43c0da31a0ad672b76bc0c1a5228f5
Riley Dulin <dulinr@fb.com>
2019-01-18 14:22:46 -0800

    Move HasFinalizer to the hermes::vm namespace
    Summary:
    All symbols should be namespaced, and this symbol is used by the hermes VM, and nothing else.
    So put it in `hermes::vm`
    
    Reviewed By: avp
    
    Differential Revision: D13738399
    
    fbshipit-source-id: e8c58d1493540df3a377d78c7f0174425371322b

--- include/hermes/VM/HasFinalizer.h
+++ include/hermes/VM/HasFinalizer.h
@@ -7,9 +7,15 @@
 #ifndef HERMES_VM_HASFINALIZER_H
 #define HERMES_VM_HASFINALIZER_H
 
+namespace hermes {
+namespace vm {
+
 /// Template parameter passed in during allocation, that signifies whether or
 /// not the cell being allocated has a finalizer. Some GC implementations can
 /// use this information to speed up finalizer handling.
 enum class HasFinalizer { No = 0, Yes };
+
+} // namespace vm
+} // namespace hermes
 
 #endif
