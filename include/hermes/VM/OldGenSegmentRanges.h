48cae26ea6610228301d1347ce9559ba8e64811a
Ashok Menon <ashokmenon@fb.com>
2019-02-11 07:46:50 -0800

    OldGenFilledSegmentRange accepts starting index.
    Summary: So it can be used when iterating over the segments since last GC.
    
    Reviewed By: davedets
    
    Differential Revision: D13903359
    
    fbshipit-source-id: b73896ea330349436674544f88124d1cae48b73b

--- include/hermes/VM/OldGenSegmentRanges.h
+++ include/hermes/VM/OldGenSegmentRanges.h
@@ -24,15 +24,15 @@ class OldGenFilledSegmentRange : public GCSegmentRange {
  public:
   /// Factory function creating an instance of this implementation, owned by a
   /// unique_ptr.
-  static inline GCSegmentRange::Ptr create(OldGen *gen);
+  static inline GCSegmentRange::Ptr create(OldGen *gen, size_t start = 0);
 
-  inline OldGenFilledSegmentRange(OldGen *gen);
+  inline OldGenFilledSegmentRange(OldGen *gen, size_t start);
 
   AlignedHeapSegment *next() override;
 
  private:
   OldGen *gen_;
-  size_t cursor_{0};
+  size_t cursor_;
 };
 
 /// A class conforming to the interface described in the docs for
@@ -54,13 +54,17 @@ class OldGenMaterializingRange : public GCSegmentRange {
 };
 
 /* static */
-inline GCSegmentRange::Ptr OldGenFilledSegmentRange::create(OldGen *gen) {
+inline GCSegmentRange::Ptr OldGenFilledSegmentRange::create(
+    OldGen *gen,
+    size_t start) {
   return std::unique_ptr<OldGenFilledSegmentRange>(
-      new OldGenFilledSegmentRange{gen});
+      new OldGenFilledSegmentRange{gen, start});
 }
 
-inline OldGenFilledSegmentRange::OldGenFilledSegmentRange(OldGen *gen)
-    : gen_{gen} {}
+inline OldGenFilledSegmentRange::OldGenFilledSegmentRange(
+    OldGen *gen,
+    size_t start)
+    : gen_{gen}, cursor_{start} {}
 
 inline GCSegmentRange::Ptr OldGenMaterializingRange::create(OldGen *gen) {
   return std::unique_ptr<OldGenMaterializingRange>(
