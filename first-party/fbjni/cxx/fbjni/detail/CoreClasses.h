b9cecd011e1b4c7fe0cd54f8063609d80004b7e4
Chris Sarbora <csarbora@fb.com>
2019-02-22 14:17:05 -0800

    Rename makeCriticalNativeMethod to discourage over-use
    Summary: "Critical" or "Fast" JNI methods are enticing by their name, but carry dangers that are not trivially visible.
    
    Reviewed By: davidaurelio
    
    Differential Revision: D14184560
    
    fbshipit-source-id: 89ec70f53bb2cb89ff568d8b1fe222ede86c9824

--- first-party/fbjni/cxx/fbjni/detail/CoreClasses.h
+++ first-party/fbjni/cxx/fbjni/detail/CoreClasses.h
@@ -249,9 +249,9 @@ class JClass : public JavaClass<JClass, JObject, jclass> {
   ///     makeNativeMethod("nativeMethodWithExplicitDescriptor",
   ///                      "(Lcom/facebook/example/MyClass;)V",
   ///                      methodWithExplicitDescriptor),
-  ///     makeCriticalNativeMethod("criticalNativeMethodWithAutomaticDescriptor",
+  ///     makeCriticalNativeMethod_DO_NOT_USE_OR_YOU_WILL_BE_FIRED("criticalNativeMethodWithAutomaticDescriptor",
   ///                              criticalNativeMethodWithAutomaticDescriptor),
-  ///     makeCriticalNativeMethod("criticalNativeMethodWithExplicitDescriptor",
+  ///     makeCriticalNativeMethod_DO_NOT_USE_OR_YOU_WILL_BE_FIRED("criticalNativeMethodWithExplicitDescriptor",
   ///                              "(IIF)Z",
   ///                              criticalNativeMethodWithExplicitDescriptor),
   ///  });
