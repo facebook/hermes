db109d298277c10c98c7bff17e64fa6ff30d444c
David Reiss <dreiss@fb.com>
2019-02-25 17:27:24 -0800

    Reorder fbjni/ByteBuffer.cpp
    Summary:
    An upcoming diff will move some functions from JByteBuffer to JBuffer,
    so putting wrapBytes last will improve readability.
    
    Reviewed By: BurntBrunch
    
    Differential Revision: D14191895
    
    fbshipit-source-id: d5565437a0f10fa71eb9088fab5e453f83ecbe04

--- first-party/fbjni/cxx/fbjni/ByteBuffer.cpp
+++ first-party/fbjni/cxx/fbjni/ByteBuffer.cpp
@@ -34,21 +34,6 @@ void JBuffer::rewind() const {
   meth(self());
 }
 
-local_ref<JByteBuffer> JByteBuffer::wrapBytes(uint8_t* data, size_t size) {
-  // env->NewDirectByteBuffer requires that size is positive. Android's
-  // dalvik returns an invalid result and Android's art aborts if size == 0.
-  // Workaround this by using a slow path through Java in that case.
-  if (!size) {
-    return createEmpty();
-  }
-  auto res = adopt_local(static_cast<javaobject>(Environment::current()->NewDirectByteBuffer(data, size)));
-  FACEBOOK_JNI_THROW_PENDING_EXCEPTION();
-  if (!res) {
-    throw std::runtime_error("Direct byte buffers are unsupported.");
-  }
-  return res;
-}
-
 uint8_t* JByteBuffer::getDirectBytes() const {
   if (!self()) {
     throwNewJavaException("java/lang/NullPointerException", "java.lang.NullPointerException");
@@ -82,6 +67,21 @@ size_t JByteBuffer::getDirectSize() const {
 bool JByteBuffer::isDirect() const {
   static auto meth = javaClassStatic()->getMethod<jboolean()>("isDirect");
   return meth(self());
+}
+
+local_ref<JByteBuffer> JByteBuffer::wrapBytes(uint8_t* data, size_t size) {
+  // env->NewDirectByteBuffer requires that size is positive. Android's
+  // dalvik returns an invalid result and Android's art aborts if size == 0.
+  // Workaround this by using a slow path through Java in that case.
+  if (!size) {
+    return createEmpty();
+  }
+  auto res = adopt_local(static_cast<javaobject>(Environment::current()->NewDirectByteBuffer(data, size)));
+  FACEBOOK_JNI_THROW_PENDING_EXCEPTION();
+  if (!res) {
+    throw std::runtime_error("Direct byte buffers are unsupported.");
+  }
+  return res;
 }
 
 }}
