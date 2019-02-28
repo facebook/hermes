ef689634f1728749bf6214e988e4dfe40bbd35f7
Will Holen <willholen@fb.com>
2019-02-27 17:28:12 -0800

    Move libhermes from tools/ to API/hermes to avoid the .a reexporting logic
    Summary:
    This moves the definition of the libhermes library to API/hermes,
    where the source code used for the interface lives. This simplifies
    linking logic.
    
    Reviewed By: tmikov
    
    Differential Revision: D14249677
    
    fbshipit-source-id: b531d6deca707957beea3f9633fc3c0660c36d8c

--- tools/libhermes/hermes.cpp
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the LICENSE
- * file in the root directory of this source tree.
- */
-/* We just reexport static libraries, but CMake still requires
- * at least one source file.
- */
