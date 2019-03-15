6eec1cb3ded1f926f165bab09ddb5f4fd290723b
Ashok Menon <ashokmenon@fb.com>
2019-03-15 07:22:52 -0700

    Eliminate InternalProperty::Ordinal.
    Summary: Make use of the Predefined enum instead.
    
    Reviewed By: kodafb
    
    Differential Revision: D14323525
    
    fbshipit-source-id: 182ad045810f06137595a6d4cd12883efc9501a3

--- include/hermes/VM/InternalProperty.h
+++ include/hermes/VM/InternalProperty.h
@@ -7,6 +7,7 @@
 #ifndef HERMES_VM_INTERNALPROPERTY_H
 #define HERMES_VM_INTERNALPROPERTY_H
 
+#include "hermes/VM/Predefined.h"
 #include "hermes/VM/SymbolID.h"
 
 namespace hermes {
@@ -15,31 +16,19 @@ namespace vm {
 /// Anonymous internal propertes. Not part of the object map.
 namespace InternalProperty {
 
-/// An internal namespace mapping each symbol name to a consecutive number.
-namespace Ordinal {
-enum {
-#define PROP(i) InternalProperty##i,
-#include "hermes/VM/InternalProperties.def"
-  _LAST
-};
-} // namespace Ordinal
-
-#define PROP(i)                            \
-  constexpr SymbolID InternalProperty##i = \
-      SymbolID::unsafeCreate(Ordinal::InternalProperty##i);
-#include "hermes/VM/InternalProperties.def"
-
-constexpr unsigned NumSymbols = Ordinal::_LAST;
 /// Number of InternalPropertyX symbols defined.
-constexpr unsigned NumInternalProperties = 4;
+#define PROP(i) +1
+constexpr unsigned NumInternalProperties = 0
+#include "hermes/VM/InternalProperties.def"
+    ;
 
 inline bool isInternal(SymbolID id) {
-  return id.unsafeGetIndex() < NumSymbols;
+  return id.unsafeGetIndex() < NumInternalProperties;
 }
 
 inline SymbolID getSymbolID(unsigned i) {
   assert(i < NumInternalProperties && "Unsupported internal property index");
-  return SymbolID::unsafeCreateNotUniqued(Ordinal::InternalProperty0 + i);
+  return SymbolID::unsafeCreateNotUniqued(Predefined::InternalProperty0 + i);
 }
 
 } // namespace InternalProperty
