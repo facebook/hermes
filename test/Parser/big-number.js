4b5bbf187ef380408cb5fc2742dfd08e05bc13df
Riley Dulin <dulinr@fb.com>
2019-03-25 17:42:55 -0700

    Add test for UBSAN overflow and blacklist function in dtoa
    Summary:
    UBSAN found a problem in dtoa's function `g_strtod`, which turns a string into a double.
    When the string in question is a very large double (such as `55e5555555555555555`),
    it should become the double value `Infinity`. dtoa correctly does this, but assumes 2's complement
    signed integer arithmetic in order to do so. This is technically undefined behavior, but
    all the platforms we care about have the correct semantics.
    
    Add a blacklist for that function.
    
    Reviewed By: tmikov
    
    Differential Revision: D14603532
    
    fbshipit-source-id: 658058503aa731c2520035eebeaa79a0bcebf015

---
+++ test/Parser/big-number.js
@@ -0,0 +1,16 @@
+// RUN: %hermes -dump-ir %s | %FileCheck %s --match-full-lines
+
+// This test ensures that a very large numeric literal can be parsed as
+// Infinity.
+// It is only testing the parser, so it should use dump-ast; however, Infinity
+// is not serializable in JSON, so this test needs to happen at the IR level.
+
+55e55555555555555555555555555555555555;
+
+// CHECK-LABEL: function global()
+// CHECK-NEXT: frame = []
+// CHECK-NEXT: %BB0:
+// CHECK-NEXT:   %0 = AllocStackInst $?anon_0_ret
+// CHECK-NEXT:   %1 = StoreStackInst undefined : undefined, %0
+// CHECK-NEXT:   %2 = StoreStackInst Infinity : number, %0
+
