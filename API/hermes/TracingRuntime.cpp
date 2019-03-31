6df76865092aa734ee377d101a1e49d38aae2b83
Riley Dulin <dulinr@fb.com>
2019-03-31 12:28:37 -0700

    Move TracingRuntime function definitions into the cpp file
    Summary:
    The body of TracingRuntime was almost all inline in the header, but the functions
    are called virtually anyway, which prevents any inlining.
    API tracing is also not very perf-sensitive as it is a separate build mode right now.
    The benefit is that the bodies of these functions can be modified without re-compiling anything
    else.
    
    Differential Revision: D14672971
    
    fbshipit-source-id: a12dfd853802e4c3ab24cb0e14b021dfa1977b69

--- API/hermes/TracingRuntime.cpp
+++ API/hermes/TracingRuntime.cpp
@@ -12,6 +12,316 @@ namespace facebook {
 namespace hermes {
 namespace tracing {
 
+TracingRuntime::TracingRuntime(
+    std::unique_ptr<jsi::Runtime> runtime,
+    uint64_t globalID)
+    : RuntimeDecorator<jsi::Runtime>(*runtime),
+      runtime_(std::move(runtime)),
+      trace_(globalID) {}
+
+jsi::Value TracingRuntime::evaluateJavaScript(
+    const std::shared_ptr<const jsi::Buffer> &buffer,
+    const std::string &sourceURL) {
+  trace_.emplace_back<SynthTrace::BeginExecJSRecord>(getTimeSinceStart());
+  auto res = RD::evaluateJavaScript(buffer, sourceURL);
+  trace_.emplace_back<SynthTrace::EndExecJSRecord>(
+      getTimeSinceStart(), toTraceValue(res));
+  return res;
+}
+
+jsi::Object TracingRuntime::createObject() {
+  auto obj = RD::createObject();
+  trace_.emplace_back<SynthTrace::CreateObjectRecord>(
+      getTimeSinceStart(), getUniqueID(obj));
+  return obj;
+}
+
+jsi::Object TracingRuntime::createObject(std::shared_ptr<jsi::HostObject> ho) {
+  class TracingHostObject : public jsi::DecoratedHostObject {
+   public:
+    using jsi::DecoratedHostObject::DecoratedHostObject;
+
+    jsi::Value get(jsi::Runtime &rt, const jsi::PropNameID &name) override {
+      TracingRuntime &trt = static_cast<TracingRuntime &>(rt);
+
+      trt.trace_.emplace_back<SynthTrace::GetPropertyNativeRecord>(
+          trt.getTimeSinceStart(), objID_, name.utf8(rt));
+
+      try {
+        auto ret = DecoratedHostObject::get(rt, name);
+
+        trt.trace_.emplace_back<SynthTrace::GetPropertyNativeReturnRecord>(
+            trt.getTimeSinceStart(), trt.toTraceValue(ret));
+
+        return ret;
+      } catch (...) {
+        // TODO(T28293178): The trace currently has no way to model
+        // exceptions thrown from C++ code.
+        ::hermes::hermes_fatal(
+            "Exception happened in native code during trace");
+      }
+    }
+
+    void set(
+        jsi::Runtime &rt,
+        const jsi::PropNameID &name,
+        const jsi::Value &value) override {
+      TracingRuntime &trt = static_cast<TracingRuntime &>(rt);
+
+      trt.trace_.emplace_back<SynthTrace::SetPropertyNativeRecord>(
+          trt.getTimeSinceStart(),
+          objID_,
+          name.utf8(rt),
+          trt.toTraceValue(value));
+
+      try {
+        DecoratedHostObject::set(rt, name, value);
+      } catch (...) {
+        // TODO(T28293178): The trace currently has no way to model
+        // exceptions thrown from C++ code.
+        ::hermes::hermes_fatal(
+            "Exception happened in native code during trace");
+      }
+
+      trt.trace_.emplace_back<SynthTrace::SetPropertyNativeReturnRecord>(
+          trt.getTimeSinceStart());
+    }
+
+    // TODO(T31386973): getPropertyNames() is not implemented.
+
+    void setObjectID(SynthTrace::ObjectID id) {
+      objID_ = id;
+    }
+
+   private:
+    /// The object id of the host object that this is attached to.
+    SynthTrace::ObjectID objID_;
+  };
+
+  auto tracer = std::make_shared<TracingHostObject>(*this, ho);
+  auto obj = RD::createObject(tracer);
+  tracer->setObjectID(getUniqueID(obj));
+  trace_.emplace_back<SynthTrace::CreateHostObjectRecord>(
+      getTimeSinceStart(), getUniqueID(obj));
+  return obj;
+}
+
+jsi::Value TracingRuntime::getProperty(
+    const jsi::Object &obj,
+    const jsi::String &name) {
+  auto value = RD::getProperty(obj, name);
+  trace_.emplace_back<SynthTrace::GetPropertyRecord>(
+      getTimeSinceStart(), getUniqueID(obj), utf8(name), toTraceValue(value));
+  return value;
+}
+
+jsi::Value TracingRuntime::getProperty(
+    const jsi::Object &obj,
+    const jsi::PropNameID &name) {
+  auto value = RD::getProperty(obj, name);
+  trace_.emplace_back<SynthTrace::GetPropertyRecord>(
+      getTimeSinceStart(), getUniqueID(obj), utf8(name), toTraceValue(value));
+  return value;
+}
+
+bool TracingRuntime::hasProperty(
+    const jsi::Object &obj,
+    const jsi::String &name) {
+  trace_.emplace_back<SynthTrace::HasPropertyRecord>(
+      getTimeSinceStart(), getUniqueID(obj), utf8(name));
+  return RD::hasProperty(obj, name);
+}
+
+bool TracingRuntime::hasProperty(
+    const jsi::Object &obj,
+    const jsi::PropNameID &name) {
+  trace_.emplace_back<SynthTrace::HasPropertyRecord>(
+      getTimeSinceStart(), getUniqueID(obj), utf8(name));
+  return RD::hasProperty(obj, name);
+}
+
+void TracingRuntime::setPropertyValue(
+    jsi::Object &obj,
+    const jsi::String &name,
+    const jsi::Value &value) {
+  trace_.emplace_back<SynthTrace::SetPropertyRecord>(
+      getTimeSinceStart(), getUniqueID(obj), utf8(name), toTraceValue(value));
+  RD::setPropertyValue(obj, name, value);
+}
+
+void TracingRuntime::setPropertyValue(
+    jsi::Object &obj,
+    const jsi::PropNameID &name,
+    const jsi::Value &value) {
+  trace_.emplace_back<SynthTrace::SetPropertyRecord>(
+      getTimeSinceStart(), getUniqueID(obj), utf8(name), toTraceValue(value));
+  RD::setPropertyValue(obj, name, value);
+}
+
+jsi::Array TracingRuntime::getPropertyNames(const jsi::Object &o) {
+  jsi::Array arr = RD::getPropertyNames(o);
+  trace_.emplace_back<SynthTrace::GetPropertyNamesRecord>(
+      getTimeSinceStart(), getUniqueID(o), getUniqueID(arr));
+  return arr;
+}
+
+jsi::WeakObject TracingRuntime::createWeakObject(const jsi::Object &o) {
+  auto wo = RD::createWeakObject(o);
+  // TODO mhorowitz: add synthtrace support for WeakObject
+  return wo;
+}
+
+jsi::Value TracingRuntime::lockWeakObject(const jsi::WeakObject &wo) {
+  auto val = RD::lockWeakObject(wo);
+  // TODO mhorowitz: add synthtrace support for WeakObject
+  return val;
+}
+
+jsi::Array TracingRuntime::createArray(size_t length) {
+  auto arr = RD::createArray(length);
+  trace_.emplace_back<SynthTrace::CreateArrayRecord>(
+      getTimeSinceStart(), getUniqueID(arr), length);
+  return arr;
+}
+
+size_t TracingRuntime::size(const jsi::Array &arr) {
+  // Array size inquiries read from the length property, which is
+  // non-configurable and thus cannot have side effects.
+  return RD::size(arr);
+}
+
+size_t TracingRuntime::size(const jsi::ArrayBuffer &buf) {
+  // ArrayBuffer size inquiries read from the byteLength property, which is
+  // non-configurable and thus cannot have side effects.
+  return RD::size(buf);
+}
+
+uint8_t *TracingRuntime::data(const jsi::ArrayBuffer &buf) {
+  throw std::logic_error(
+      "Cannot write raw bytes into an ArrayBuffer in trace mode");
+}
+
+jsi::Value TracingRuntime::getValueAtIndex(const jsi::Array &arr, size_t i) {
+  auto value = RD::getValueAtIndex(arr, i);
+  trace_.emplace_back<SynthTrace::ArrayReadRecord>(
+      getTimeSinceStart(), getUniqueID(arr), i, toTraceValue(value));
+  return value;
+}
+
+void TracingRuntime::setValueAtIndexImpl(
+    jsi::Array &arr,
+    size_t i,
+    const jsi::Value &value) {
+  trace_.emplace_back<SynthTrace::ArrayWriteRecord>(
+      getTimeSinceStart(), getUniqueID(arr), i, toTraceValue(value));
+  return RD::setValueAtIndexImpl(arr, i, value);
+}
+
+jsi::Function TracingRuntime::createFunctionFromHostFunction(
+    const jsi::PropNameID &name,
+    unsigned int paramCount,
+    jsi::HostFunctionType func) {
+  class TracingHostFunction : public jsi::DecoratedHostFunction {
+   public:
+    using jsi::DecoratedHostFunction::DecoratedHostFunction;
+
+    jsi::Value operator()(
+        jsi::Runtime &rt,
+        const jsi::Value &thisVal,
+        const jsi::Value *args,
+        size_t count) {
+      TracingRuntime &trt = static_cast<TracingRuntime &>(rt);
+
+      trt.trace_.emplace_back<SynthTrace::CallToNativeRecord>(
+          trt.getTimeSinceStart(),
+          functionID_,
+          // A host function does not have a this.
+          SynthTrace::encodeUndefined(),
+          trt.argStringifyer(args, count));
+
+      try {
+        auto ret =
+            jsi::DecoratedHostFunction::operator()(rt, thisVal, args, count);
+
+        trt.trace_.emplace_back<SynthTrace::ReturnFromNativeRecord>(
+            trt.getTimeSinceStart(), trt.toTraceValue(ret));
+        return ret;
+      } catch (...) {
+        // TODO(T28293178): The trace currently has no way to model
+        // exceptions thrown from C++ code.
+        ::hermes::hermes_fatal(
+            "Exception happened in native code during trace");
+      }
+    }
+
+    void setFunctionID(SynthTrace::ObjectID functionID) {
+      functionID_ = functionID;
+    }
+
+   private:
+    /// The object id of the function that this is attached to.
+    SynthTrace::ObjectID functionID_;
+  };
+  auto tracer = TracingHostFunction(*this, func);
+  auto tfunc =
+      RD::createFunctionFromHostFunction(name, paramCount, std::move(tracer));
+  RD::getHostFunction(tfunc).target<TracingHostFunction>()->setFunctionID(
+      getUniqueID(tfunc));
+  trace_.emplace_back<SynthTrace::CreateHostFunctionRecord>(
+      getTimeSinceStart(), getUniqueID(tfunc));
+  return tfunc;
+}
+
+jsi::Value TracingRuntime::call(
+    const jsi::Function &func,
+    const jsi::Value &jsThis,
+    const jsi::Value *args,
+    size_t count) {
+  trace_.emplace_back<SynthTrace::CallFromNativeRecord>(
+      getTimeSinceStart(),
+      getUniqueID(func),
+      toTraceValue(jsThis),
+      argStringifyer(args, count));
+  auto retval = RD::call(func, jsThis, args, count);
+  trace_.emplace_back<SynthTrace::ReturnToNativeRecord>(
+      getTimeSinceStart(), toTraceValue(retval));
+  return retval;
+}
+
+jsi::Value TracingRuntime::callAsConstructor(
+    const jsi::Function &func,
+    const jsi::Value *args,
+    size_t count) {
+  trace_.emplace_back<SynthTrace::ConstructFromNativeRecord>(
+      getTimeSinceStart(),
+      getUniqueID(func),
+      // A construct call always has an undefined this.
+      // The ReturnToNativeRecord will contain the object that was either
+      // created by the new keyword, or the objec that's returned from the
+      // function.
+      SynthTrace::encodeUndefined(),
+      argStringifyer(args, count));
+  auto retval = RD::callAsConstructor(func, args, count);
+  trace_.emplace_back<SynthTrace::ReturnToNativeRecord>(
+      getTimeSinceStart(), toTraceValue(retval));
+  return retval;
+}
+
+void TracingRuntime::addTTIMarker() {
+  trace_.emplace_back<SynthTrace::MarkerRecord>(getTimeSinceStart(), "tti");
+}
+
+std::vector<SynthTrace::TraceValue> TracingRuntime::argStringifyer(
+    const jsi::Value *args,
+    size_t count) {
+  std::vector<SynthTrace::TraceValue> stringifiedArgs;
+  stringifiedArgs.reserve(count);
+  for (size_t i = 0; i < count; ++i) {
+    stringifiedArgs.emplace_back(toTraceValue(args[i]));
+  }
+  return stringifiedArgs;
+}
+
 SynthTrace::TraceValue TracingRuntime::toTraceValue(const jsi::Value &value) {
   if (value.isUndefined()) {
     return SynthTrace::encodeUndefined();
@@ -32,6 +342,25 @@ SynthTrace::TraceValue TracingRuntime::toTraceValue(const jsi::Value &value) {
   }
 }
 
+SynthTrace::TimeSinceStart TracingRuntime::getTimeSinceStart() const {
+  return std::chrono::duration_cast<SynthTrace::TimeSinceStart>(
+      std::chrono::steady_clock::now() - startTime_);
+}
+
+TracingHermesRuntime::TracingHermesRuntime(
+    std::unique_ptr<HermesRuntime> runtime,
+    const ::hermes::vm::RuntimeConfig &runtimeConfig)
+    : TracingHermesRuntime(
+          runtime,
+          runtime->getUniqueID(runtime->global()),
+          runtimeConfig) {}
+
+TracingHermesRuntime::TracingHermesRuntime(
+    std::unique_ptr<HermesRuntime> &runtime,
+    uint64_t globalID,
+    const ::hermes::vm::RuntimeConfig &runtimeConfig)
+    : TracingRuntime(std::move(runtime), globalID), conf_(runtimeConfig) {}
+
 void TracingHermesRuntime::writeTrace(llvm::raw_ostream &os) const {
   os << SynthTrace::Printable(
       trace(), hermesRuntime().getMockedEnvironment(), conf_);
@@ -46,6 +375,17 @@ void TracingHermesRuntime::writeBridgeTrafficTraceToFile(
   }
 
   writeTrace(fs);
+}
+
+jsi::Value TracingHermesRuntime::evaluateJavaScript(
+    const std::shared_ptr<const jsi::Buffer> &buffer,
+    const std::string &sourceURL) {
+  if (HermesRuntime::isHermesBytecode(buffer->data(), buffer->size())) {
+    trace().setSourceHash(
+        ::hermes::hbc::BCProviderFromBuffer::getSourceHashFromBytecode(
+            llvm::makeArrayRef(buffer->data(), buffer->size())));
+  }
+  return TracingRuntime::evaluateJavaScript(buffer, sourceURL);
 }
 
 namespace {
--- API/hermes/TracingRuntime.h
+++ API/hermes/TracingRuntime.h
@@ -17,24 +17,10 @@ namespace hermes {
 namespace tracing {
 
 class TracingRuntime : public jsi::RuntimeDecorator<jsi::Runtime> {
-  std::vector<SynthTrace::TraceValue> argStringifyer(
-      const jsi::Value *args,
-      size_t count) {
-    std::vector<SynthTrace::TraceValue> stringifiedArgs;
-    stringifiedArgs.reserve(count);
-    for (size_t i = 0; i < count; ++i) {
-      stringifiedArgs.emplace_back(toTraceValue(args[i]));
-    }
-    return stringifiedArgs;
-  }
-
  public:
   using RD = RuntimeDecorator<jsi::Runtime>;
 
-  TracingRuntime(std::unique_ptr<jsi::Runtime> runtime, uint64_t globalID)
-      : RuntimeDecorator<jsi::Runtime>(*runtime),
-        runtime_(std::move(runtime)),
-        trace_(globalID) {}
+  TracingRuntime(std::unique_ptr<jsi::Runtime> runtime, uint64_t globalID);
 
   virtual SynthTrace::ObjectID getUniqueID(const jsi::Object &o) = 0;
 
@@ -45,290 +31,66 @@ class TracingRuntime : public jsi::RuntimeDecorator<jsi::Runtime> {
 
   jsi::Value evaluateJavaScript(
       const std::shared_ptr<const jsi::Buffer> &buffer,
-      const std::string &sourceURL) override {
-    trace_.emplace_back<SynthTrace::BeginExecJSRecord>(getTimeSinceStart());
-    auto res = RD::evaluateJavaScript(buffer, sourceURL);
-    trace_.emplace_back<SynthTrace::EndExecJSRecord>(
-        getTimeSinceStart(), toTraceValue(res));
-    return res;
-  }
+      const std::string &sourceURL) override;
 
-  jsi::Object createObject() override {
-    auto obj = RD::createObject();
-    trace_.emplace_back<SynthTrace::CreateObjectRecord>(
-        getTimeSinceStart(), getUniqueID(obj));
-    return obj;
-  }
-
-  class TracingHostObject : public jsi::DecoratedHostObject {
-   public:
-    using jsi::DecoratedHostObject::DecoratedHostObject;
-
-    jsi::Value get(jsi::Runtime &rt, const jsi::PropNameID &name) override {
-      TracingRuntime &trt = static_cast<TracingRuntime &>(rt);
-
-      trt.trace_.emplace_back<SynthTrace::GetPropertyNativeRecord>(
-          trt.getTimeSinceStart(), objID_, name.utf8(rt));
-
-      try {
-        auto ret = DecoratedHostObject::get(rt, name);
-
-        trt.trace_.emplace_back<SynthTrace::GetPropertyNativeReturnRecord>(
-            trt.getTimeSinceStart(), trt.toTraceValue(ret));
-
-        return ret;
-      } catch (...) {
-        // TODO(T28293178): The trace currently has no way to model
-        // exceptions thrown from C++ code.
-        ::hermes::hermes_fatal(
-            "Exception happened in native code during trace");
-      }
-    }
-
-    void set(
-        jsi::Runtime &rt,
-        const jsi::PropNameID &name,
-        const jsi::Value &value) override {
-      TracingRuntime &trt = static_cast<TracingRuntime &>(rt);
-
-      trt.trace_.emplace_back<SynthTrace::SetPropertyNativeRecord>(
-          trt.getTimeSinceStart(),
-          objID_,
-          name.utf8(rt),
-          trt.toTraceValue(value));
-
-      try {
-        DecoratedHostObject::set(rt, name, value);
-      } catch (...) {
-        // TODO(T28293178): The trace currently has no way to model
-        // exceptions thrown from C++ code.
-        ::hermes::hermes_fatal(
-            "Exception happened in native code during trace");
-      }
-
-      trt.trace_.emplace_back<SynthTrace::SetPropertyNativeReturnRecord>(
-          trt.getTimeSinceStart());
-    }
-
-    // TODO(T31386973): getPropertyNames() is not implemented.
-
-    void setObjectID(SynthTrace::ObjectID id) {
-      objID_ = id;
-    }
-
-   private:
-    /// The object id of the host object that this is attached to.
-    SynthTrace::ObjectID objID_;
-  };
-
-  jsi::Object createObject(std::shared_ptr<jsi::HostObject> ho) override {
-    auto tracer = std::make_shared<TracingHostObject>(*this, ho);
-    auto obj = RD::createObject(tracer);
-    tracer->setObjectID(getUniqueID(obj));
-    trace_.emplace_back<SynthTrace::CreateHostObjectRecord>(
-        getTimeSinceStart(), getUniqueID(obj));
-    return obj;
-  }
+  jsi::Object createObject() override;
+  jsi::Object createObject(std::shared_ptr<jsi::HostObject> ho) override;
 
   jsi::Value getProperty(const jsi::Object &obj, const jsi::String &name)
-      override {
-    auto value = RD::getProperty(obj, name);
-    trace_.emplace_back<SynthTrace::GetPropertyRecord>(
-        getTimeSinceStart(), getUniqueID(obj), utf8(name), toTraceValue(value));
-    return value;
-  }
-
+      override;
   jsi::Value getProperty(const jsi::Object &obj, const jsi::PropNameID &name)
-      override {
-    auto value = RD::getProperty(obj, name);
-    trace_.emplace_back<SynthTrace::GetPropertyRecord>(
-        getTimeSinceStart(), getUniqueID(obj), utf8(name), toTraceValue(value));
-    return value;
-  }
+      override;
 
-  bool hasProperty(const jsi::Object &obj, const jsi::String &name) override {
-    trace_.emplace_back<SynthTrace::HasPropertyRecord>(
-        getTimeSinceStart(), getUniqueID(obj), utf8(name));
-    return RD::hasProperty(obj, name);
-  }
-
+  bool hasProperty(const jsi::Object &obj, const jsi::String &name) override;
   bool hasProperty(const jsi::Object &obj, const jsi::PropNameID &name)
-      override {
-    trace_.emplace_back<SynthTrace::HasPropertyRecord>(
-        getTimeSinceStart(), getUniqueID(obj), utf8(name));
-    return RD::hasProperty(obj, name);
-  }
+      override;
 
   void setPropertyValue(
       jsi::Object &obj,
       const jsi::String &name,
-      const jsi::Value &value) override {
-    trace_.emplace_back<SynthTrace::SetPropertyRecord>(
-        getTimeSinceStart(), getUniqueID(obj), utf8(name), toTraceValue(value));
-    RD::setPropertyValue(obj, name, value);
-  }
-
+      const jsi::Value &value) override;
   void setPropertyValue(
       jsi::Object &obj,
       const jsi::PropNameID &name,
-      const jsi::Value &value) override {
-    trace_.emplace_back<SynthTrace::SetPropertyRecord>(
-        getTimeSinceStart(), getUniqueID(obj), utf8(name), toTraceValue(value));
-    RD::setPropertyValue(obj, name, value);
-  }
+      const jsi::Value &value) override;
 
-  jsi::Array getPropertyNames(const jsi::Object &o) override {
-    jsi::Array arr = RD::getPropertyNames(o);
-    trace_.emplace_back<SynthTrace::GetPropertyNamesRecord>(
-        getTimeSinceStart(), getUniqueID(o), getUniqueID(arr));
-    return arr;
-  }
+  jsi::Array getPropertyNames(const jsi::Object &o) override;
 
-  jsi::WeakObject createWeakObject(const jsi::Object &o) override {
-    auto wo = RD::createWeakObject(o);
-    // TODO mhorowitz: add synthtrace support for WeakObject
-    return wo;
-  }
+  jsi::WeakObject createWeakObject(const jsi::Object &o) override;
 
-  jsi::Value lockWeakObject(const jsi::WeakObject &wo) override {
-    auto val = RD::lockWeakObject(wo);
-    // TODO mhorowitz: add synthtrace support for WeakObject
-    return val;
-  }
+  jsi::Value lockWeakObject(const jsi::WeakObject &wo) override;
 
-  jsi::Array createArray(size_t length) override {
-    auto arr = RD::createArray(length);
-    trace_.emplace_back<SynthTrace::CreateArrayRecord>(
-        getTimeSinceStart(), getUniqueID(arr), length);
-    return arr;
-  }
+  jsi::Array createArray(size_t length) override;
 
-  size_t size(const jsi::Array &arr) override {
-    // Array size inquiries read from the length property, which is
-    // non-configurable and thus cannot have side effects.
-    return RD::size(arr);
-  }
+  size_t size(const jsi::Array &arr) override;
+  size_t size(const jsi::ArrayBuffer &buf) override;
 
-  size_t size(const jsi::ArrayBuffer &buf) override {
-    // ArrayBuffer size inquiries read from the byteLength property, which is
-    // non-configurable and thus cannot have side effects.
-    return RD::size(buf);
-  }
+  uint8_t *data(const jsi::ArrayBuffer &buf) override;
 
-  uint8_t *data(const jsi::ArrayBuffer &buf) override {
-    throw std::logic_error(
-        "Cannot write raw bytes into an ArrayBuffer in trace mode");
-  }
-
-  jsi::Value getValueAtIndex(const jsi::Array &arr, size_t i) override {
-    auto value = RD::getValueAtIndex(arr, i);
-    trace_.emplace_back<SynthTrace::ArrayReadRecord>(
-        getTimeSinceStart(), getUniqueID(arr), i, toTraceValue(value));
-    return value;
-  }
+  jsi::Value getValueAtIndex(const jsi::Array &arr, size_t i) override;
 
   void setValueAtIndexImpl(jsi::Array &arr, size_t i, const jsi::Value &value)
-      override {
-    trace_.emplace_back<SynthTrace::ArrayWriteRecord>(
-        getTimeSinceStart(), getUniqueID(arr), i, toTraceValue(value));
-    return RD::setValueAtIndexImpl(arr, i, value);
-  }
-
-  class TracingHostFunction : public jsi::DecoratedHostFunction {
-   public:
-    using jsi::DecoratedHostFunction::DecoratedHostFunction;
-
-    jsi::Value operator()(
-        jsi::Runtime &rt,
-        const jsi::Value &thisVal,
-        const jsi::Value *args,
-        size_t count) {
-      TracingRuntime &trt = static_cast<TracingRuntime &>(rt);
-
-      trt.trace_.emplace_back<SynthTrace::CallToNativeRecord>(
-          trt.getTimeSinceStart(),
-          functionID_,
-          // A host function does not have a this.
-          SynthTrace::encodeUndefined(),
-          trt.argStringifyer(args, count));
-
-      try {
-        auto ret =
-            jsi::DecoratedHostFunction::operator()(rt, thisVal, args, count);
-
-        trt.trace_.emplace_back<SynthTrace::ReturnFromNativeRecord>(
-            trt.getTimeSinceStart(), trt.toTraceValue(ret));
-        return ret;
-      } catch (...) {
-        // TODO(T28293178): The trace currently has no way to model
-        // exceptions thrown from C++ code.
-        ::hermes::hermes_fatal(
-            "Exception happened in native code during trace");
-      }
-    }
-
-    void setFunctionID(SynthTrace::ObjectID functionID) {
-      functionID_ = functionID;
-    }
-
-    /// The object id of the function that this is attached to.
-    SynthTrace::ObjectID functionID_;
-  };
+      override;
 
   jsi::Function createFunctionFromHostFunction(
       const jsi::PropNameID &name,
       unsigned int paramCount,
-      jsi::HostFunctionType func) override {
-    auto tracer = TracingHostFunction(*this, func);
-    auto tfunc =
-        RD::createFunctionFromHostFunction(name, paramCount, std::move(tracer));
-    RD::getHostFunction(tfunc).target<TracingHostFunction>()->setFunctionID(
-        getUniqueID(tfunc));
-    trace_.emplace_back<SynthTrace::CreateHostFunctionRecord>(
-        getTimeSinceStart(), getUniqueID(tfunc));
-    return tfunc;
-  }
+      jsi::HostFunctionType func) override;
 
   jsi::Value call(
       const jsi::Function &func,
       const jsi::Value &jsThis,
       const jsi::Value *args,
-      size_t count) override {
-    trace_.emplace_back<SynthTrace::CallFromNativeRecord>(
-        getTimeSinceStart(),
-        getUniqueID(func),
-        toTraceValue(jsThis),
-        argStringifyer(args, count));
-    auto retval = RD::call(func, jsThis, args, count);
-    trace_.emplace_back<SynthTrace::ReturnToNativeRecord>(
-        getTimeSinceStart(), toTraceValue(retval));
-    return retval;
-  }
+      size_t count) override;
 
   jsi::Value callAsConstructor(
       const jsi::Function &func,
       const jsi::Value *args,
-      size_t count) override {
-    trace_.emplace_back<SynthTrace::ConstructFromNativeRecord>(
-        getTimeSinceStart(),
-        getUniqueID(func),
-        // A construct call always has an undefined this.
-        // The ReturnToNativeRecord will contain the object that was either
-        // created by the new keyword, or the objec that's returned from the
-        // function.
-        SynthTrace::encodeUndefined(),
-        argStringifyer(args, count));
-    auto retval = RD::callAsConstructor(func, args, count);
-    trace_.emplace_back<SynthTrace::ReturnToNativeRecord>(
-        getTimeSinceStart(), toTraceValue(retval));
-    return retval;
-  }
+      size_t count) override;
 
   /// @}
 
-  void addTTIMarker() {
-    trace_.emplace_back<SynthTrace::MarkerRecord>(getTimeSinceStart(), "tti");
-  }
+  void addTTIMarker();
 
   SynthTrace &trace() {
     return trace_;
@@ -341,10 +103,11 @@ class TracingRuntime : public jsi::RuntimeDecorator<jsi::Runtime> {
  private:
   SynthTrace::TraceValue toTraceValue(const jsi::Value &value);
 
-  SynthTrace::TimeSinceStart getTimeSinceStart() const {
-    return std::chrono::duration_cast<SynthTrace::TimeSinceStart>(
-        std::chrono::steady_clock::now() - startTime_);
-  }
+  std::vector<SynthTrace::TraceValue> argStringifyer(
+      const jsi::Value *args,
+      size_t count);
+
+  SynthTrace::TimeSinceStart getTimeSinceStart() const;
 
   std::unique_ptr<jsi::Runtime> runtime_;
   SynthTrace trace_;
@@ -357,11 +120,7 @@ class TracingHermesRuntime final : public TracingRuntime {
  public:
   TracingHermesRuntime(
       std::unique_ptr<HermesRuntime> runtime,
-      const ::hermes::vm::RuntimeConfig &runtimeConfig)
-      : TracingHermesRuntime(
-            runtime,
-            runtime->getUniqueID(runtime->global()),
-            runtimeConfig) {}
+      const ::hermes::vm::RuntimeConfig &runtimeConfig);
 
   SynthTrace::ObjectID getUniqueID(const jsi::Object &o) override {
     return static_cast<SynthTrace::ObjectID>(hermesRuntime().getUniqueID(o));
@@ -374,14 +133,7 @@ class TracingHermesRuntime final : public TracingRuntime {
 
   jsi::Value evaluateJavaScript(
       const std::shared_ptr<const jsi::Buffer> &buffer,
-      const std::string &sourceURL) override {
-    if (HermesRuntime::isHermesBytecode(buffer->data(), buffer->size())) {
-      trace().setSourceHash(
-          ::hermes::hbc::BCProviderFromBuffer::getSourceHashFromBytecode(
-              llvm::makeArrayRef(buffer->data(), buffer->size())));
-    }
-    return TracingRuntime::evaluateJavaScript(buffer, sourceURL);
-  }
+      const std::string &sourceURL) override;
 
   HermesRuntime &hermesRuntime() {
     return static_cast<HermesRuntime &>(plain());
@@ -402,8 +154,7 @@ class TracingHermesRuntime final : public TracingRuntime {
   TracingHermesRuntime(
       std::unique_ptr<HermesRuntime> &runtime,
       uint64_t globalID,
-      const ::hermes::vm::RuntimeConfig &runtimeConfig)
-      : TracingRuntime(std::move(runtime), globalID), conf_(runtimeConfig) {}
+      const ::hermes::vm::RuntimeConfig &runtimeConfig);
 
   const ::hermes::vm::RuntimeConfig conf_;
 };
