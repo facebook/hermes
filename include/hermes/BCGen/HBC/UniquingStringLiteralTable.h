8a81c64719281488efda40deafa2cacff49d3d74
Ashok Menon <ashokmenon@fb.com>
2019-04-18 03:47:00 -0700

    Moving UniquingStringLiteralTable inline impls out of class declaration.
    Summary:
    Whilst I'm here, moving the implementations of inline functions out of the
    class declaration, so that they become easier to search for with grep (it
    becomes possible to differentiate them from their call-sites).
    
    Reviewed By: kodafb
    
    Differential Revision: D14891337
    
    fbshipit-source-id: c37dbc74c85fa3b4a59b56ccf40d15ba56d4492b

--- include/hermes/BCGen/HBC/UniquingStringLiteralTable.h
+++ include/hermes/BCGen/HBC/UniquingStringLiteralTable.h
@@ -41,59 +41,74 @@ class UniquingStringLiteralTable final {
   /// Adds a string, \p str, if not already present.  Additionally marks that
   /// string as an identifier if \p isIdentifier is true (defaults to false).
   /// \returns the index identifier of the string (either existing or new).
-  uint32_t addString(llvm::StringRef str, bool isIdentifier = false) {
-    assert(strings_.size() == isIdentifier_.size());
-    auto id = strings_.insert(str);
-    if (id == isIdentifier_.size()) {
-      isIdentifier_.push_back(isIdentifier);
-    } else if (isIdentifier) {
-      isIdentifier_[id] = true;
-    }
-
-    return id;
-  }
+  inline uint32_t addString(llvm::StringRef str, bool isIdentifier = false);
 
   /// \return string id of an existing \p str in string table.
-  uint32_t getExistingStringId(llvm::StringRef str) const {
-    auto iter = strings_.find(str);
-    assert(iter != strings_.end() && "str should exist in string table.");
-    return std::distance(strings_.begin(), iter);
-  }
+  inline uint32_t getExistingStringId(llvm::StringRef str) const;
 
   /// \return string id of an existing \p str in string table, assuming it is
   /// marked as an identifier.
-  uint32_t getExistingIdentifierId(llvm::StringRef str) const {
-    auto idx = getExistingStringId(str);
-    assert(isIdentifier_[idx] && "str is not an identifier.");
-    return idx;
-  }
+  inline uint32_t getExistingIdentifierId(llvm::StringRef str) const;
 
   //// \return a ConsecutiveStringStorage for the strings in the table.
   ///  If \p optimize is set, attempt to pack the strings in the storage to
   ///  reduce their size.
-  ConsecutiveStringStorage generateStorage(bool optimize = false) {
-    // Flush unwritten strings to our existing storage and then transfer it
-    // to the caller. Mark our written strings as zero since our storage will
-    // now be empty.
-    flushUnwrittenStringsToStorage(optimize);
-    ConsecutiveStringStorage result = std::move(storage_);
-
-    for (uint32_t idx = 0; idx < isIdentifier_.size(); ++idx) {
-      if (isIdentifier_[idx]) {
-        result.markEntryAsIdentifier(idx);
-      }
-    }
-
-    storage_ = ConsecutiveStringStorage{};
-    writtenStrings_ = 0;
-    return result;
-  }
+  inline ConsecutiveStringStorage generateStorage(bool optimize = false);
 
   /// \return whether the table is empty.
-  bool empty() const {
-    return strings_.empty();
-  }
+  inline bool empty() const;
 };
+
+inline uint32_t UniquingStringLiteralTable::addString(
+    llvm::StringRef str,
+    bool isIdentifier) {
+  assert(strings_.size() == isIdentifier_.size());
+  auto id = strings_.insert(str);
+  if (id == isIdentifier_.size()) {
+    isIdentifier_.push_back(isIdentifier);
+  } else if (isIdentifier) {
+    isIdentifier_[id] = true;
+  }
+
+  return id;
+}
+
+inline uint32_t UniquingStringLiteralTable::getExistingStringId(
+    llvm::StringRef str) const {
+  auto iter = strings_.find(str);
+  assert(iter != strings_.end() && "str should exist in string table.");
+  return std::distance(strings_.begin(), iter);
+}
+
+inline uint32_t UniquingStringLiteralTable::getExistingIdentifierId(
+    llvm::StringRef str) const {
+  auto idx = getExistingStringId(str);
+  assert(isIdentifier_[idx] && "str is not an identifier.");
+  return idx;
+}
+
+inline ConsecutiveStringStorage UniquingStringLiteralTable::generateStorage(
+    bool optimize) {
+  // Flush unwritten strings to our existing storage and then transfer it
+  // to the caller. Mark our written strings as zero since our storage will
+  // now be empty.
+  flushUnwrittenStringsToStorage(optimize);
+  ConsecutiveStringStorage result = std::move(storage_);
+
+  for (uint32_t idx = 0; idx < isIdentifier_.size(); ++idx) {
+    if (isIdentifier_[idx]) {
+      result.markEntryAsIdentifier(idx);
+    }
+  }
+
+  storage_ = ConsecutiveStringStorage{};
+  writtenStrings_ = 0;
+  return result;
+}
+
+inline bool UniquingStringLiteralTable::empty() const {
+  return strings_.empty();
+}
 
 } // namespace hbc
 } // namespace hermes
