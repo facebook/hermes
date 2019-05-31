3494a4abb310c3f04e6b4f67ce5f6fa84e687604
Riley Dulin <dulinr@fb.com>
2019-05-31 12:40:12 -0700

    Remove contiguousBackingStorage experiment option
    Summary:
    Now that the experiment for a contiguous address space has completed,
    remove the option from `RuntimeConfig`. It will now be decided by a build mode only,
    and not at runtime.
    
    Differential Revision: D15577808
    
    fbshipit-source-id: 2062d245c2bfe048747005a59a490b911f6a0e10

--- public/hermes/Public/RuntimeConfig.h
+++ public/hermes/Public/RuntimeConfig.h
@@ -19,50 +19,46 @@ namespace vm {
 class PinnedHermesValue;
 
 // Parameters for Runtime initialisation.  Check documentation in README.md
-#define RUNTIME_FIELDS(F)                                                \
-  /* Parameters to be passed on to the GC. */                            \
-  F(vm::GCConfig, GCConfig)                                              \
-                                                                         \
-  /* Pre-allocated Register Stack */                                     \
-  F(PinnedHermesValue *, RegisterStack, nullptr)                         \
-                                                                         \
-  /* Register Stack Size */                                              \
-  F(unsigned, MaxNumRegisters, 1024 * 1024)                              \
-                                                                         \
-  /* Whether or not the JIT is enabled */                                \
-  F(bool, EnableJIT, false)                                              \
-                                                                         \
-  /* Whether to allow eval and Function ctor */                          \
-  F(bool, EnableEval, true)                                              \
-                                                                         \
-  /* Support for ES6 Symbol. */                                          \
-  F(bool, ES6Symbol, true)                                               \
-                                                                         \
-  /* Whether to use a contiguous backing storage for segments (true), */ \
-  /* or a non-contiguous backing storage that is requested on-demand. */ \
-  F(bool, ContiguousBackingStorage, false)                               \
-                                                                         \
-  /* Enable sampling certain statistics. */                              \
-  F(bool, EnableSampledStats, false)                                     \
-                                                                         \
-  /* Whether to enable sampling profiler */                              \
-  F(bool, EnableSampleProfiling, false)                                  \
-                                                                         \
-  /* Whether to randomize stack placement etc. */                        \
-  F(bool, RandomizeMemoryLayout, false)                                  \
-                                                                         \
-  /* Eagerly read bytecode into page cache. */                           \
-  F(unsigned, BytecodeWarmupPercent, 0)                                  \
-                                                                         \
-  /* Signal-based I/O tracking. Slows down execution. If enabled, */     \
-  /* all bytecode buffers > 64 kB passed to Hermes must be mmap:ed. */   \
-  F(bool, TrackIO, false)                                                \
-                                                                         \
-  /* An interface for managing crashes. */                               \
-  F(std::shared_ptr<CrashManager>, CrashMgr, new NopCrashManager)        \
-                                                                         \
-  /* The flags passed from a VM experiment */                            \
-  F(uint32_t, VMExperimentFlags, 0)                                      \
+#define RUNTIME_FIELDS(F)                                              \
+  /* Parameters to be passed on to the GC. */                          \
+  F(vm::GCConfig, GCConfig)                                            \
+                                                                       \
+  /* Pre-allocated Register Stack */                                   \
+  F(PinnedHermesValue *, RegisterStack, nullptr)                       \
+                                                                       \
+  /* Register Stack Size */                                            \
+  F(unsigned, MaxNumRegisters, 1024 * 1024)                            \
+                                                                       \
+  /* Whether or not the JIT is enabled */                              \
+  F(bool, EnableJIT, false)                                            \
+                                                                       \
+  /* Whether to allow eval and Function ctor */                        \
+  F(bool, EnableEval, true)                                            \
+                                                                       \
+  /* Support for ES6 Symbol. */                                        \
+  F(bool, ES6Symbol, true)                                             \
+                                                                       \
+  /* Enable sampling certain statistics. */                            \
+  F(bool, EnableSampledStats, false)                                   \
+                                                                       \
+  /* Whether to enable sampling profiler */                            \
+  F(bool, EnableSampleProfiling, false)                                \
+                                                                       \
+  /* Whether to randomize stack placement etc. */                      \
+  F(bool, RandomizeMemoryLayout, false)                                \
+                                                                       \
+  /* Eagerly read bytecode into page cache. */                         \
+  F(unsigned, BytecodeWarmupPercent, 0)                                \
+                                                                       \
+  /* Signal-based I/O tracking. Slows down execution. If enabled, */   \
+  /* all bytecode buffers > 64 kB passed to Hermes must be mmap:ed. */ \
+  F(bool, TrackIO, false)                                              \
+                                                                       \
+  /* An interface for managing crashes. */                             \
+  F(std::shared_ptr<CrashManager>, CrashMgr, new NopCrashManager)      \
+                                                                       \
+  /* The flags passed from a VM experiment */                          \
+  F(uint32_t, VMExperimentFlags, 0)                                    \
   /* RUNTIME_FIELDS END */
 
 _HERMES_CTORCONFIG_STRUCT(RuntimeConfig, RUNTIME_FIELDS, {});
