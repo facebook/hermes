5204738030af9e8e7f5666d65c83a7c244adc5a5
Lun Liu <lunliu@fb.com>
2019-06-27 08:06:10 -0700

    Update Array JSLib functions pop() to use toLength() instead of toUInt32_RJS()
    Summary:
    ES6+ requires toLength() to be used on the length property which returns an uint64_t. Also change
    the type of len used in the helper function because of it. Tests for pop() are removed
    from blacklist.
    
    Reviewed By: ridiculousfish
    
    Differential Revision: D15944408
    
    fbshipit-source-id: f5b804218017aa32054f4f8d78f8daf1487034c8

--- lib/VM/JSLib/Array.cpp
+++ lib/VM/JSLib/Array.cpp
@@ -1211,11 +1211,11 @@ arrayPrototypePop(void *, Runtime *runtime, NativeArgs args) {
   if (LLVM_UNLIKELY(propRes == ExecutionStatus::EXCEPTION)) {
     return ExecutionStatus::EXCEPTION;
   }
-  auto intRes = toUInt32_RJS(runtime, runtime->makeHandle(*propRes));
+  auto intRes = toLength(runtime, runtime->makeHandle(*propRes));
   if (LLVM_UNLIKELY(intRes == ExecutionStatus::EXCEPTION)) {
     return ExecutionStatus::EXCEPTION;
   }
-  uint32_t len = intRes->getNumber();
+  uint64_t len = intRes->getNumber();
 
   if (len == 0) {
     if (LLVM_UNLIKELY(
