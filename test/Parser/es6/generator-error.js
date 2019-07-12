4667a8c05edd7c40b9b52a5e97222ae66014a946
Xun Li <xun@fb.com>
2019-07-12 12:46:56 -0700

    Add missing copyright headers to a few files
    Summary: A few files are missing the proper copyright headers. Add them.
    
    Reviewed By: avp
    
    Differential Revision: D16225293
    
    fbshipit-source-id: 09f1ee2a8e413cfd6ba29f09b29f305ba14b0b79

--- test/Parser/es6/generator-error.js
+++ test/Parser/es6/generator-error.js
@@ -1,11 +1,16 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the LICENSE
+// file in the root directory of this source tree.
+//
 // RUN: (! %hermesc -dump-ast -pretty-json %s 2>&1 ) | %FileCheck --match-full-lines %s
 
 function *foo(yield) {}
-// CHECK:{{.*}}:3:15: error: Unexpected usage of 'yield' as an identifier
+// CHECK:{{.*}}:{{.*}}:15: error: Unexpected usage of 'yield' as an identifier
 // CHECK-NEXT:function *foo(yield) {}
 // CHECK-NEXT:              ^~~~~
 
 function *bar() { var yield = 1; }
-// CHECK:{{.*}}:8:23: error: Unexpected usage of 'yield' as an identifier
+// CHECK:{{.*}}:{{.*}}:23: error: Unexpected usage of 'yield' as an identifier
 // CHECK-NEXT:function *bar() { var yield = 1; }
 // CHECK-NEXT:                      ^~~~~
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 const {baseUrlPattern} = require('./utils');
 
 const isBuilding = process.env.npm_lifecycle_script
