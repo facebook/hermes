d2eabd61a2b91199ec0686535fd445172f0b2fcb
Riley Dulin <dulinr@fb.com>
2019-07-19 16:05:46 -0700

    Add chrome heap snapshot parser script
    Summary:
    This script will turn a Chrome heap snapshot filled with 1-D arrays of numbers into a pretty
    list of JSON tuples.
    Before:
    ```
    "nodes":[9,1,1,0,11,0
    ,9,2,3,0,24,0
    ,9,3,5,0,1,0
    ,9,4,7,0,460,0
    ,9,5,9,0,539,0
    ,9,6,11,0,54,0
    ...
    ```
    After:
    ```
    [
      {
        "type": "synthetic",
        "name": "",
        "id": 1,
        "self_size": 0,
        "edges": [
          {
            "type": "element",
            "name_or_index": 1,
            "to_node": 1
          },
          {
            "type": "shortcut",
            "name_or_index": 7345,
            "to_node": 2164
          },
          {
            "type": "shortcut",
            "name_or_index": 7346,
            "to_node": 2168
          },
          ...
        ],
        "trace_node_id": 0,
      },
      ...
    ]
    ```
    Which makes it a lot easier to explore the format and find bugs in its output.
    
    Reviewed By: willholen
    
    Differential Revision: D16369235
    
    fbshipit-source-id: 68d973c12e24649b245f17a35ff590b23a2eb79d

---
+++ utils/chrome_snapshot_parser.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the LICENSE
+# file in the root directory of this source tree.
+
+import argparse
+import json
+
+
+"""
+Takes in a Chrome heap snapshot and makes the output into more explorable JSON
+"""
+
+
+# The order of all these lists must match the order of fields that v8 outputs.
+EDGE_FIELDS = ["type", "name_or_index", "to_node"]
+EDGE_TYPES = [
+    "context",
+    "element",
+    "property",
+    "internal",
+    "hidden",
+    "shortcut",
+    "weak",
+]
+NODE_FIELDS = ["type", "name", "id", "self_size", "edge_count", "trace_node_id"]
+NODE_TYPES = [
+    "hidden",
+    "array",
+    "string",
+    "object",
+    "code",
+    "closure",
+    "regexp",
+    "number",
+    "native",
+    "synthetic",
+    "concatenated string",
+    "sliced string",
+    "symbol",
+    "bigint",
+]
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("heapsnapshot")
+    parser.add_argument("out")
+    args = parser.parse_args()
+    with open(args.heapsnapshot, "r") as f:
+        root = json.load(f)
+    curr_node = 0
+    curr_edge = 0
+    nodes = []
+    while curr_node < len(root["nodes"]):
+        raw_type, name, id, self_size, edge_count, trace_node_id = root["nodes"][
+            curr_node : curr_node + len(NODE_FIELDS)
+        ]
+        edges = []
+        end_edge = curr_edge + edge_count * len(EDGE_FIELDS)
+        while curr_edge < end_edge:
+            raw_edge_type, name_or_index, to_node = root["edges"][
+                curr_edge : curr_edge + len(EDGE_FIELDS)
+            ]
+            real_type = EDGE_TYPES[raw_edge_type]
+            edges.append(
+                {
+                    "type": real_type,
+                    "name_or_index": root["strings"][name_or_index]
+                    if real_type
+                    in ("context", "property", "internal", "shortcut", "weak")
+                    else name_or_index,
+                    "to_node": to_node // len(NODE_FIELDS),
+                }
+            )
+            curr_edge += len(EDGE_FIELDS)
+        nodes.append(
+            {
+                "type": NODE_TYPES[raw_type],
+                "name": root["strings"][name],
+                "id": id,
+                "self_size": self_size,
+                "edges": edges,
+                "trace_node_id": trace_node_id,
+            }
+        )
+        curr_node += len(NODE_FIELDS)
+
+    with open(args.out, "w") as f:
+        json.dump(nodes, f, indent=2)
+
+
+if __name__ == "__main__":
+    main()
