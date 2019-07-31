efa839394ad306b8bfcc4c69b025b446c223caaa
Kudo Chien <ckchien@gmail.com>
2019-07-31 20:58:04 +0800

    Remove libc++_shared.so from AAR and add hermes-cppruntime.aar [2/2]
    Summary:
        1. Add cppruntime subproject which will generate
           hermes-cppruntime-release.aar included libc++_shared.so.
    
        2. To get libc++_shared.so, although we could have a gradle
           custom task to copy the file from NDK, the drawback is that the
           path might change between different NDK versions.
           I decided to have a stub cpp and use formal cmake build to
           generate libc++_shared.so as usual.
           During the time to generate AAR,
           using packagingOptions.exclude "**/libstub.so"
           to keep only libc++_shared.so.

---
+++ android/cppruntime/src/main/cpp/stub.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the LICENSE
+ * file in the root directory of this source tree.
+ */
+#include <jni.h>
+
+extern "C" JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved) {
+  return JNI_VERSION_1_6;
+}
