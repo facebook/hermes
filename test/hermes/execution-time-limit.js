5ff2706a5d62ad7ae1bb1e7cd25012299a2f5900
Will Holen <willholen@fb.com>
2019-09-05 16:59:16 -0700

    Add missing copyright headers
    Summary: Two files were missing copyright headers.
    
    Reviewed By: dulinriley
    
    Differential Revision: D17211365
    
    fbshipit-source-id: 50f0fe8a754faa60c23362e30adc83063cb8301d

--- test/hermes/execution-time-limit.js
+++ test/hermes/execution-time-limit.js
@@ -1,3 +1,8 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the LICENSE
+// file in the root directory of this source tree.
+//
 // RUN: (! %hermes -emit-async-break-check -time-limit=1000 %s 2>&1 ) | %FileCheck --match-full-lines %s
 
 function entryPoint() {
@@ -14,6 +19,6 @@ function helper() {
 entryPoint();
 
 //CHECK: Error: Javascript execution has timed out.
-//CHECK: at helper ({{.*/execution-time-limit.js}}:10:5)
-//CHECK-NEXT: at entryPoint ({{.*/execution-time-limit.js}}:4:9)
-//CHECK-NEXT: at global ({{.*/execution-time-limit.js}}:14:11)
+//CHECK: at helper ({{.*/execution-time-limit.js}}:15:5)
+//CHECK-NEXT: at entryPoint ({{.*/execution-time-limit.js}}:9:9)
+//CHECK-NEXT: at global ({{.*/execution-time-limit.js}}:19:11)
