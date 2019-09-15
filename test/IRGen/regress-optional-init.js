67decb1a4af9680dbe82b0cee824602ded52d8bd
Tzvetan Mikov <tmikov@fb.com>
2019-09-15 10:46:23 -0700

    add missing copyright header in test
    Summary: .
    
    Reviewed By: kodafb
    
    Differential Revision: D17390241
    
    fbshipit-source-id: 07c16ced195d54af07a0c55d727ac5e607dc7253

--- test/IRGen/regress-optional-init.js
+++ test/IRGen/regress-optional-init.js
@@ -1,3 +1,8 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the LICENSE
+// file in the root directory of this source tree.
+//
 // RUN: %hermesc -dump-ir %s | %FileCheck --match-full-lines %s
 //
 // Make sure that the optional initialization generates a correct Phi node, when the initialization
