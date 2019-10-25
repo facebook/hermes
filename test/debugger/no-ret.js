21a823dfa859038d4f75a75f1a66135a2aea740f
Riley Dulin <dulinr@fb.com>
2019-10-25 16:16:01 -0700

    Disable leak checks at exit for a debugger test
    Summary:
    The debugger uses `exit(0)` in some test cases, and LSAN does not count this as a safe way to
    release memory, so it causes a crash and a reported leak (of memory allocated by GC objects).
    
    Sadly, `__lsan_do_leak_check` doesn't fix this, because it is still called while a Runtime is on the heap and not cleaned up.
    Tell this test to not install an atexit handler to do the leak check.
    
    Reviewed By: avp
    
    Differential Revision: D18118300
    
    fbshipit-source-id: 1ffa687c71bcea4e97d897eb4bd3fa1882b47648

--- test/debugger/no-ret.js
+++ test/debugger/no-ret.js
@@ -5,11 +5,13 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-// RUN: %hdb --break-at-start %s < %s.debug | %FileCheck --match-full-lines %s
+// RUN: ASAN_OPTIONS=leak_check_at_exit=0 %hdb --break-at-start %s < %s.debug | %FileCheck --match-full-lines %s
 // REQUIRES: debugger
+
+// Turn off leak checks at exit because hdb calls exit while a Runtime is still alive.
 
  for (;;);
 
-// CHECK: Break on script load in global: {{.*}}:11:2
-// CHECK: Stepped to global: {{.*}}:11:2
-// CHECK: Stepped to global: {{.*}}:11:2
+// CHECK: Break on script load in global: {{.*}}:13:2
+// CHECK: Stepped to global: {{.*}}:13:2
+// CHECK: Stepped to global: {{.*}}:13:2
