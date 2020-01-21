3178be1093d113a2b317d4b08dcbdfd939671943
Riley Dulin <dulinr@fb.com>
2020-01-21 12:40:22 -0800

    Use fbandroid/mode/asan on Linux sandcastle machines
    Summary:
    There's now a way to run ASAN and UBSAN on devservers, using `fbandroid/mode/asan_ubsan`.
    Use this on Linux jobs to get some extra ASAN and UBSAN coverage.
    
    #utd-hermes
    
    Reviewed By: avp
    
    Differential Revision: D18123730
    
    fbshipit-source-id: c0d74c698b2edbede87cfbba129e1b4b3d045874

--- unittests/VMRuntime/Instrumentation/ProcessStatsTest.cpp
+++ unittests/VMRuntime/Instrumentation/ProcessStatsTest.cpp
@@ -8,6 +8,7 @@
 #ifndef _WINDOWS
 #include "hermes/VM/instrumentation/ProcessStats.h"
 
+#include "hermes/Support/Compiler.h"
 #include "hermes/Support/OSCompat.h"
 
 #include "gtest/gtest.h"
@@ -77,6 +78,7 @@ TEST(ProcessStatsTest, Test) {
 }
 
 void ProcessStatsTest(InfoAssertion assertionImpl) {
+#if !LLVM_ADDRESS_SANITIZER_BUILD
   const size_t PS = hermes::oscompat::page_size();
   const size_t PSkB = PS / 1024;
 
@@ -160,6 +162,11 @@ void ProcessStatsTest(InfoAssertion assertionImpl) {
   EXPECT_INFO_DELTA(afterUnmap, afterNoop2, 0, 0);
 
 #undef EXPECT_INFO_DELTA
+#else
+  // Otherwise unused functions.
+  (void)flushRSSEvents;
+  (void)touchPages;
+#endif
 }
 
 void infoAssertionImpl(
