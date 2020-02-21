57b0ea758f5b32c0eee494a0c407bc449aedbe25
Riley Dulin <dulinr@fb.com>
2020-02-21 11:40:31 -0800

    Migrate asl_log to os_log for MacOS logging
    Summary:
    There's a build warning when using `asl_log` and building for MacOS;
    it says that `asl_log` is deprecated and to use `os_log` instead.
    
    Unfortunately `os_log` only works with varargs, and does *not* work with
    a `va_list`. There's a private MacOS API that does work with `va_list`, but
    it's not exposed.
    
    So on MacOS, do a local `vsnprintf` to format the args into a string, and then
    pass only that string to `os_log`. This is actually better for Hermes's use case,
    so that we only ever use valid C format strings.
    
    Reviewed By: tmikov
    
    Differential Revision: D19839635
    
    fbshipit-source-id: e9074b30d8d9272b2b58c24ce267697bc7b5ef96

--- lib/Platform/Logging.cpp
+++ lib/Platform/Logging.cpp
@@ -7,19 +7,16 @@
 
 #include "hermes/Platform/Logging.h"
 
-#ifdef __APPLE__
-#include "TargetConditionals.h"
-#endif
-
 #ifdef __ANDROID__
 #include <android/log.h>
 #elif defined(__APPLE__)
-#include <asl.h>
+#include <os/log.h>
 #else
 #include <cstdio>
 #endif
 
 #include <cstdarg>
+#include <memory>
 
 namespace hermes {
 
@@ -29,7 +26,22 @@ void hermesLog(const char *componentName, const char *fmt, ...) {
 #ifdef __ANDROID__
   __android_log_vprint(ANDROID_LOG_INFO, componentName, fmt, args);
 #elif defined(__APPLE__)
-  asl_vlog(NULL, NULL, ASL_LEVEL_NOTICE, fmt, args);
+  static os_log_t hermesLogger = os_log_create("com.hermes", "Default");
+  // Need to make a copy in order to do the vsprintf trick.
+  va_list argsCopy;
+  va_copy(argsCopy, args);
+  int numChars = vsnprintf(nullptr, 0, fmt, argsCopy);
+  va_end(argsCopy);
+  std::unique_ptr<char[]> buffer{new char[numChars + 1]};
+  int numCharsWritten = vsnprintf(buffer.get(), numChars + 1, fmt, args);
+  (void)numCharsWritten;
+  // Log all messages as public here. Hermes will never log private information.
+  os_log_with_type(
+      hermesLogger,
+      OS_LOG_TYPE_INFO,
+      "%{public}s: %{public}s",
+      componentName,
+      buffer.get());
 #else
   fprintf(stderr, "%s: ", componentName);
   vfprintf(stderr, fmt, args);
