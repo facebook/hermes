9b28f5a3e845f1c2ca3df515fc9b2e33b052aa74
Neil Dhar <neildhar@fb.com>
2020-05-21 14:19:04 -0700

    Add support for OSS-Fuzz build
    Summary:
    OSS-Fuzz is a fuzzing platform for open source software provided by
    Google. This diff makes the following changes:
    
    1. Add a fuzz target that uses libfuzzer
    2. Add a cmake target for the fuzzer
    3. Edit our UBSAN configuration to be more precise, since OSS-Fuzz
    enables unsigned integer overflow detection
    
    Reviewed By: dulinriley
    
    Differential Revision: D21191934
    
    fbshipit-source-id: 0c36c2140ce89ebf81d62ae8a689fdc0e48df1aa

---
+++ tools/fuzzers/fuzzer-jsi-entry.cpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <hermes/hermes.h>
+#include <jsi/jsi.h>
+
+using facebook::hermes::HermesRuntime;
+using facebook::hermes::makeHermesRuntime;
+using facebook::jsi::JSIException;
+using facebook::jsi::StringBuffer;
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  if (!size) {
+    // Hermes can't handle empty input, and it would have a performance cost
+    // to do so.
+    return 0;
+  }
+  // Hermes bytecode is assumed to be trusted, and there are often no checks
+  // for validity (for performance purposes).
+  // Discard inputs that would be interpreted as bytecode to avoid reporting
+  // those as errors.
+  if (HermesRuntime::isHermesBytecode(data, size)) {
+    return 0;
+  }
+
+  std::string s(reinterpret_cast<const char *>(data), size);
+  s.append("\0");
+  auto runtime = makeHermesRuntime();
+
+  try {
+    runtime->evaluateJavaScript(std::make_unique<StringBuffer>(s), "");
+  } catch (const JSIException &e) {
+    // Swallow JS-based exceptions.
+    // The fuzzer will generate a lot of invalid JS, and if this causes an
+    // exception to be thrown evaluating it, that's alright.
+    // The fuzzer is more interested in inputs that cause the sort of crash that
+    // doesn't throw an exception.
+  }
+  // Other types of exceptions are not ok to occur, and should be reported as
+  // bugs.
+  // Hermes throws no exceptions during normal operation, with the exception of
+  // any that escape JS.
+  return 0;
+}
