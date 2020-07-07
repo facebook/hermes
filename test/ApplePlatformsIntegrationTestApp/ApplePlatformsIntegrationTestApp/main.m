419f3362143a72f6a759db34dd5f316ebe78c0b5
Eloy Durán <eloy.de.enige@gmail.com>
2020-07-07 22:17:31 +0200

    [test] Remove boilerplate code

--- test/ApplePlatformsIntegrationTestApp/ApplePlatformsIntegrationTestApp/main.m
+++ test/ApplePlatformsIntegrationTestApp/ApplePlatformsIntegrationTestApp/main.m
@@ -1,8 +1,5 @@
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, const char * argv[]) {
-  @autoreleasepool {
-      // Setup code that might create autoreleased objects goes here.
-  }
   return NSApplicationMain(argc, argv);
 }
