08b09715432602e142dee603b9aa0fecee0e6d73
Will Holen <willholen@fb.com>
2020-07-24 17:20:15 -0700

    Run e2e tests against RN master. (#294)
    Summary:
    Pull Request resolved: https://github.com/facebook/hermes/pull/294
    
    Pull Request resolved: https://github.com/facebook/hermes/pull/287
    
    We should be able to run against RN master.
    
    Reviewed By: mhorowitz
    
    Differential Revision: D22440288
    
    fbshipit-source-id: 90dcf5cee7b0b632650e7703515ec85ac020d1c8

--- test/circleci-e2e/run.sh
+++ test/circleci-e2e/run.sh
@@ -7,14 +7,12 @@
 # Parse command-line arguments
 use_react_native_master=
 hermes_npm_package=
-artifact_postfix=
 project_dir=awesome1
 
 while (( "$#" )); do
   case $1 in
     --react-native-master)
       use_react_native_master=1
-      #artifact_postfix=rn-master
       #project_dir=awesome2
       ;;
     --hermes-npm-package)
@@ -31,6 +29,14 @@ while (( "$#" )); do
    esac
    shift
 done
+
+# Use md5 instead of md5sum if the latter can't be found
+if ! command -v md5sum > /dev/null
+then
+  md5sum() {
+    md5 "$@"
+  }
+fi
 
 # Perform GNU sed operation confirming file changed
 function sed_op() {
@@ -49,22 +55,39 @@ function sed_op() {
   [[ "$(md5sum "$file")" != "$hash_before" ]]
 }
 
+project_path="$PWD/$project_dir"
+commit() {
+  ( cd "$project_path" && git commit -a -m "$1" )
+}
+
 npx react-native init "$project_dir"
+
+# Install new NPM. This must be done before we start messing with
+# node_modules/react-native
+if [[ -n "$hermes_npm_package" ]]
+then
+  ( cd "$project_path" && npm install "$hermes_npm_package" )
+fi
+
+( cd "$project_path" && git init && git add . )
+commit "Initial project"
 
 # Enable Hermes
 # https://reactnative.dev/docs/hermes
 sed_op 's/enableHermes: false/enableHermes: true/' "$project_dir"/android/app/build.gradle
+commit "Enable hermes"
 
 # Patch in master React Native checkout.
 # https://github.com/facebook/react-native/wiki/Building-from-source
 if [[ -n "$use_react_native_master" ]] ; then
   pushd "$project_dir"/node_modules/
   mv react-native react-native-
-  git clone https://github.com/facebook/react-native
+  git clone --depth=1 https://github.com/facebook/react-native
   cd react-native
   npm install
+  [[ -n "$hermes_npm_package" ]] && npm install "$hermes_npm_package"
   popd
-  sed_op '/classpath("com.android.tools.build:gradle:3.4.2")/aclasspath("de.undercouch:gradle-download-task:4.0.0")' "$project_dir"/android/build.gradle
+  sed_op '/classpath("com.android.tools.build:gradle:.*")/aclasspath("de.undercouch:gradle-download-task:4.0.0")' "$project_dir"/android/build.gradle
   cat >> "$project_dir"/android/settings.gradle <<EOT
 include ':ReactAndroid'
 project(':ReactAndroid').projectDir = new File(
@@ -72,23 +95,24 @@ project(':ReactAndroid').projectDir = new File(
 EOT
   sed_op 's/implementation "com.facebook.react:react-native:.*/implementation project(":ReactAndroid")/' "$project_dir"/android/app/build.gradle
 fi
+commit "Run RN from source"
 
-# Add newly built Hermes package
-if [[ -n "$hermes_npm_package" ]] ; then
-  ( cd "$project_dir"/node_modules/react-native && npm install "$hermes_npm_package" )
-fi
 
 # Modify app to print a log message at start-up
 echo "console.log('Using Hermes: ' + (global.HermesInternal != null));" >> "$project_dir"/index.js
+commit "Add smoke test message"
+
+# Build release app
+( cd "$project_dir"/android/ && ./gradlew packageRelease )
 
 # Wait for emulator to boot
 adb wait-for-device
 
-# Build APK + install onto emulator
+# Install onto emulator
 ( cd "$project_dir"/android/ && ./gradlew installRelease )
 
 # Stash the APK
-cp "$project_dir"/android/app/build/outputs/apk/release/app-release.apk /tmp/app-release-"$artifact_postfix".apk
+cp "$project_dir"/android/app/build/outputs/apk/release/app-release.apk /tmp/hermes/output/app-release.apk
 
 # Start screen recording for debug
 adb shell screenrecord /sdcard/screencap.mp4 &
@@ -97,7 +121,7 @@ function stashScreenCapture() {
   local exit_code=$?
   adb shell killall -s INT screenrecord
   wait $adb_screen_record_pid || true
-  adb pull /sdcard/screencap.mp4 /tmp/screencap-"$artifact_postfix".mp4
+  adb pull /sdcard/screencap.mp4 /tmp/hermes/output/screencap.mp4
   exit "$exit_code"
 }
 trap stashScreenCapture EXIT
@@ -109,6 +133,8 @@ SECONDS=0
 until grep 'Using Hermes: true' <(adb logcat -d -s ReactNativeJS:I) ; do
   if ((SECONDS > 30 )) ; then
     echo "Did not see correct log message within 30s"
+    adb logcat -d > /tmp/hermes/output/adblog.txt
+    tar cfz /tmp/hermes/output/project.tar.gz "$project_path"
     exit 1
   fi
   sleep 2
