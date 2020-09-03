5b548ffd1b049b336c82431763015d547bd827da
tudorms <tudor.mihai@microsoft.com>
2020-09-03 15:52:07 -0700

    Fix UWP builds

--- cmake/modules/Hermes.cmake
+++ cmake/modules/Hermes.cmake
@@ -28,10 +28,12 @@ if (EMSCRIPTEN AND EMSCRIPTEN_FASTCOMP)
   set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -s BINARYEN_TRAP_MODE=clamp")
 endif()
 
-# For compatibility, CMake adds /EHsc by default for MSVC. We want to set that
-# flag per target, so remove it.
+# For compatibility, CMake adds /EHsc, /GR and /DUNICODE by default for MSVC. We want to set those
+# flags per target, so remove them.
 if (MSVC)
   string(REPLACE "/EHsc" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+  string(REPLACE "/GR" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+  string(REPLACE "/DUNICODE" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
 endif (MSVC)
 
 # set stack reserved size to ~10MB
