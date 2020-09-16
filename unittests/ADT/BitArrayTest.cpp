66c482287fa73fcbd203092783743bea0a7a74ab
Neil Dhar <neildhar@fb.com>
2020-09-16 15:40:56 -0700

    Expand BitArray tests
    Summary: Add BitArray tests to cover edge cases.
    
    Reviewed By: dulinriley
    
    Differential Revision: D23709014
    
    fbshipit-source-id: 22f4d8f9aa5c3669e7fc65c5cea3900103fb2c94

--- unittests/ADT/BitArrayTest.cpp
+++ unittests/ADT/BitArrayTest.cpp
@@ -18,20 +18,29 @@ using namespace hermes;
 template <typename SIZE>
 struct BitArrayTest : public ::testing::Test {
  protected:
-  std::deque<size_t> getIndices() {
+  std::vector<std::deque<size_t>> getTestIndices() {
+    std::vector<std::deque<size_t>> testIndices;
     // Use a mix of word-aligned and non-word-aligned values. Set the first and
     // last bits to check that those edge cases are handled correctly.
-    return {0,
-            1,
-            2,
-            128,
-            234,
-            436,
-            789,
-            1099,
-            SIZE::value - 65,
-            SIZE::value - 2,
-            SIZE::value - 1};
+    testIndices.push_back({0,
+                           1,
+                           2,
+                           128,
+                           234,
+                           436,
+                           789,
+                           1099,
+                           SIZE::value - 65,
+                           SIZE::value - 2,
+                           SIZE::value - 1});
+
+    // Test the case where there are no set bits at the start/end.
+    testIndices.push_back({128, 234, 436, 789, 1099, SIZE::value - 100});
+
+    // Test the case where set bits occur at word boundaries.
+    testIndices.push_back({63, 127, 1023, 1024, SIZE::value - 1});
+
+    return testIndices;
   }
 };
 
@@ -46,74 +55,85 @@ TYPED_TEST_CASE(BitArrayTest, TestSizes);
 TYPED_TEST(BitArrayTest, NextSetBit) {
   constexpr size_t N = TypeParam::value;
   BitArray<N> ba;
-  auto indices = this->getIndices();
-  // Empty case: No marked bits
-  EXPECT_EQ(N, ba.findNextSetBitFrom(0));
-  for (size_t idx : indices) {
-    ba.set(idx, true);
+  auto testIndices = this->getTestIndices();
+  for (auto &indices : testIndices) {
+    ba.reset();
+    // Empty case: No marked bits
+    EXPECT_EQ(N, ba.findNextSetBitFrom(0));
+    for (size_t idx : indices) {
+      ba.set(idx, true);
+    }
+    // Use the same style of loop we use elsewhere for scanning the array.
+    for (size_t from = ba.findNextSetBitFrom(0); from < N;
+         from = ba.findNextSetBitFrom(from + 1)) {
+      EXPECT_EQ(indices.front(), from);
+      indices.pop_front();
+    }
+    EXPECT_EQ(0, indices.size());
   }
-  // Use the same style of loop we use elsewhere for scanning the array.
-  for (size_t from = ba.findNextSetBitFrom(0); from < N;
-       from = ba.findNextSetBitFrom(from + 1)) {
-    EXPECT_EQ(indices.front(), from);
-    indices.pop_front();
-  }
-  EXPECT_EQ(0, indices.size());
 }
 
 TYPED_TEST(BitArrayTest, NextZeroBit) {
   constexpr size_t N = TypeParam::value;
   BitArray<N> ba;
-  auto indices = this->getIndices();
-  ba.set();
-  // Full case: No unmarked bits
-  EXPECT_EQ(N, ba.findNextZeroBitFrom(0));
-  for (auto idx : indices) {
-    ba.set(idx, false);
-  }
+  auto testIndices = this->getTestIndices();
+  for (auto &indices : testIndices) {
+    ba.set();
+    // Full case: No unmarked bits
+    EXPECT_EQ(N, ba.findNextZeroBitFrom(0));
+    for (auto idx : indices) {
+      ba.set(idx, false);
+    }
 
-  // Use the same style of loop we use elsewhere for scanning the array.
-  for (size_t from = ba.findNextZeroBitFrom(0); from < N;
-       from = ba.findNextZeroBitFrom(from + 1)) {
-    EXPECT_EQ(indices.front(), from);
-    indices.pop_front();
+    // Use the same style of loop we use elsewhere for scanning the array.
+    for (size_t from = ba.findNextZeroBitFrom(0); from < N;
+         from = ba.findNextZeroBitFrom(from + 1)) {
+      EXPECT_EQ(indices.front(), from);
+      indices.pop_front();
+    }
+    EXPECT_EQ(0, indices.size());
   }
-  EXPECT_EQ(0, indices.size());
 }
 
 TYPED_TEST(BitArrayTest, PrevSetBit) {
   constexpr size_t N = TypeParam::value;
   BitArray<N> ba;
-  auto indices = this->getIndices();
-  // Empty case: No marked bits
-  EXPECT_EQ(N, ba.findPrevSetBitFrom(N));
-  for (auto idx : indices) {
-    ba.set(idx, true);
+
+  auto testIndices = this->getTestIndices();
+  for (auto &indices : testIndices) {
+    ba.reset();
+    // Empty case: No marked bits
+    EXPECT_EQ(N, ba.findPrevSetBitFrom(N));
+    for (auto idx : indices) {
+      ba.set(idx, true);
+    }
+    for (size_t from = ba.findPrevSetBitFrom(N); from != N;
+         from = ba.findPrevSetBitFrom(from)) {
+      EXPECT_EQ(indices.back(), from);
+      indices.pop_back();
+    }
+    EXPECT_EQ(0, indices.size());
   }
-  for (size_t from = ba.findPrevSetBitFrom(N); from != N;
-       from = ba.findPrevSetBitFrom(from)) {
-    EXPECT_EQ(indices.back(), from);
-    indices.pop_back();
-  }
-  EXPECT_EQ(0, indices.size());
 }
 
 TYPED_TEST(BitArrayTest, PrevZeroBit) {
   constexpr size_t N = TypeParam::value;
   BitArray<N> ba;
-  auto indices = this->getIndices();
-  ba.set();
-  // Full case: No unmarked bits
-  EXPECT_EQ(N, ba.findPrevZeroBitFrom(N));
-  for (auto idx : indices) {
-    ba.set(idx, false);
+  auto testIndices = this->getTestIndices();
+  for (auto &indices : testIndices) {
+    ba.set();
+    // Full case: No unmarked bits
+    EXPECT_EQ(N, ba.findPrevZeroBitFrom(N));
+    for (auto idx : indices) {
+      ba.set(idx, false);
+    }
+    for (size_t from = ba.findPrevZeroBitFrom(N); from != N;
+         from = ba.findPrevZeroBitFrom(from)) {
+      EXPECT_EQ(indices.back(), from);
+      indices.pop_back();
+    }
+    EXPECT_EQ(0, indices.size());
   }
-  for (size_t from = ba.findPrevZeroBitFrom(N); from != N;
-       from = ba.findPrevZeroBitFrom(from)) {
-    EXPECT_EQ(indices.back(), from);
-    indices.pop_back();
-  }
-  EXPECT_EQ(0, indices.size());
 }
 
 } // namespace
