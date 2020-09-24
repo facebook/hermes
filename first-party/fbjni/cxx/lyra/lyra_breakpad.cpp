c16239b3e51ab100d517636481e73ab9aab54857
David Reiss <dreiss@fb.com>
2020-09-23 23:01:20 -0700

    Tiny formatting fix in lyra_breakpad
    Reviewed By: passy
    
    Differential Revision: D23820250
    
    fbshipit-source-id: b0ef4c0b09a39c1fe619d2fbd0d327bcc245ed2c

--- first-party/fbjni/cxx/lyra/lyra_breakpad.cpp
+++ first-party/fbjni/cxx/lyra/lyra_breakpad.cpp
@@ -22,7 +22,7 @@ namespace lyra {
 /**
  * This can be overridden by an implementation capable of looking up
  * the breakpad id for logging purposes.
-*/
+ */
 __attribute__((weak))
 std::string getBreakpadId(const std::string& library) {
   return "<unimplemented>";
