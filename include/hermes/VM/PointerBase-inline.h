9eb08fc55a1b46cedafe636b2a240ba1225fec5e
Neil Dhar <neildhar@fb.com>
2020-09-28 19:39:00 -0700

    Fix circular dependencies in PointerBase
    Summary:
    `PointerBase.h` cannot include `AlignedHeapSegment.h` directly
    because `AlignedHeapSegment.h` includes other files that in turn
    include `PointerBase.h`. For instance, `GCCell.h` indirectly includes
    `PointerBase.h`, and simultaneously, `AlignedHeapSegment.h` includes
    `GCCell.h`. Since `PointerBase.h` only depends on a small subset of
    `AlignedHeapSegment.h`, we can remove this circularity by extracting
    `SegmentInfo` into a separate file that is included by both
    PointerBase and AlignedHeapSegment. This allows us to simplify the
    PointerBase code in a subsequent change.
    
    Reviewed By: dulinriley
    
    Differential Revision: D23968489
    
    fbshipit-source-id: 1d75c7ad3460c689213019d595af6dbae6c37ec8

--- include/hermes/VM/PointerBase-inline.h
+++ include/hermes/VM/PointerBase-inline.h
@@ -8,9 +8,9 @@
 #ifndef HERMES_VM_POINTERBASE_INLINE_H
 #define HERMES_VM_POINTERBASE_INLINE_H
 
-#include "hermes/VM/AlignedHeapSegment.h"
 #include "hermes/VM/AlignedStorage.h"
 #include "hermes/VM/PointerBase.h"
+#include "hermes/VM/SegmentInfo.h"
 
 #include <cassert>
 #include <cstdint>
@@ -53,7 +53,7 @@ inline uint32_t BasedPointer::computeSegmentAndOffset(const void *heapAddr) {
   uintptr_t offset = addrAsInt - reinterpret_cast<uintptr_t>(segStart);
   /// Now get the segment index, and shift it so that it's bits do not
   /// overlap with those of offset.
-  return (AlignedHeapSegment::segmentIndexFromStart(segStart)
+  return (SegmentInfo::segmentIndexFromStart(segStart)
           << AlignedStorage::kLogSize) |
       offset;
 }
@@ -87,7 +87,7 @@ inline void PointerBase::setSegment(unsigned idx, void *segStart) {
   char *bias =
       reinterpret_cast<char *>(segStart) - (idx << AlignedStorage::kLogSize);
   segmentMap_[idx] = bias;
-  AlignedHeapSegment::setSegmentIndexFromStart(segStart, idx);
+  SegmentInfo::setSegmentIndexFromStart(segStart, idx);
 }
 
 #else
