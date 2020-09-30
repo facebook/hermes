f8841e3995ccfc047433f4f8bf743a560a28f2b5
Neil Dhar <neildhar@fb.com>
2020-09-30 15:43:04 -0700

    Aggressively inline BasedPointer code
    Summary:
    Make it easier for the compiler to inline `BasedPointer` code by
    placing all of it in `PointerBase.h` and deleting
    `PointerBase-inline.h`.  This allows the compiler to always see the
    inline definitions, instead of relying on `PointerBase-inline.h` also
    being included.
    
    Note that this change is now possible because there are no circular
    dependencies involving `PointerBase.h`
    
    Reviewed By: dulinriley
    
    Differential Revision: D23922958
    
    fbshipit-source-id: 378355ae9e9cc9adb9868a07e911ccf5b690f6ce

--- include/hermes/VM/SegmentInfo.h
+++ include/hermes/VM/SegmentInfo.h
@@ -10,6 +10,16 @@
 
 #include "hermes/VM/AlignedStorage.h"
 
+/// Define this macro here because SegmentInfo is necessarily a dependency of
+/// anything using compressed pointers.
+#if defined(HERMESVM_ALLOW_COMPRESSED_POINTERS) && LLVM_PTR_SIZE == 8 && \
+    (defined(HERMESVM_GC_NONCONTIG_GENERATIONAL) ||                      \
+     defined(HERMESVM_GC_HADES))
+/// \macro HERMESVM_COMPRESSED_POINTERS
+/// \brief If defined, store pointers as 32 bits in GC-managed Hermes objects.
+#define HERMESVM_COMPRESSED_POINTERS
+#endif
+
 namespace hermes {
 namespace vm {
 
