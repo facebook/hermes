136bfee929003b73359066fffc5d60d4f26913e7
Hans Halverson <hanshalverson@fb.com>
2020-10-28 12:45:22 -0700

    Custom UTF8 decoder to address bugs when converting to JS strings
    Summary:
    There are two main bugs currently present in the Hermes parser compiled to WASM's handling of unicode strings.
    
    First of all JS strings can contain null bytes in the middle of the string, however emscripten's `UTF8ToString` decoding utility is only built for null terminated strings. This means that if a string contains a null byte in the middle, `UTF8ToString` will drop all bytes after the null byte.
    
    Secondly, `UTF8ToString` produces the replacement character `0xfffd` when encountering invalid UTF-8. This includes when decoding code points that are in the surrogate blocks, as code points in the surrogate blocks are not technically valid UTF-8. However Hermes has already done the initial unicode validation when parsing, and when it encounters a code point that cannot be represented in a single UTF-16 code unit it encodes it as a surrogate pair where each code unit is encoded using UTF-8. This means that Hermes produces technically invalid UTF-8 strings since they may contain UTF-8 encoded surrogate code points.
    
    To fix both these issues, let's introduce our own UTF-8 decoder utility to be used when reading strings out of the WASM heap from JS. This new utility, called `HermesDecodeUTF8String`, is based off emscripten's [UTF8ToString](https://github.com/emscripten-core/emscripten/blob/master/src/runtime_strings.js#L25) with modifications to fix the issues mentioned above. First of all the new `HermesDecodeUTF8String` always decodes exactly `size` bytes, instead of stopping at the first null byte. Secondly, `HermesDecodeUTF8String` does not check for invalid code point ranges when decoding (e.g. code points in the surrogate blocks), as Hermes has already done unicode validation when parsing and is intentionally producing these.
    
    To add utility functions outside the JS library itself we must supply another js file using the `--pre-js` option to `emcc`, so let's add support for this option to the `hermes_wasm_binary` definition.
    
    Reviewed By: gkz
    
    Differential Revision: D24521465
    
    fbshipit-source-id: f562da7612198533282eae75aa9485e451370c4e

--- tools/hermes-parser/js/__tests__/HermesParser-test.js
+++ tools/hermes-parser/js/__tests__/HermesParser-test.js
@@ -1136,3 +1136,68 @@ test('Import expression', () => {
     },
   });
 });
+
+test('Unicode strings and identifiers', () => {
+  const source = `
+    // Null byte in middle of string
+    'foo \0 bar';
+    // Unicode directly in text
+    const 𩸽 = '𩸽';
+    // Unicode literals
+    const a\u033f = '\u033f';
+    // Surrogate pairs (invalid individual UTF-8 code points)
+    '\ud83d\udea6';
+  `;
+
+  expect(parse(source)).toMatchObject({
+    type: 'Program',
+    body: [
+      {
+        type: 'ExpressionStatement',
+        expression: {
+          type: 'Literal',
+          value: 'foo \0 bar',
+        },
+      },
+      {
+        type: 'VariableDeclaration',
+        declarations: [
+          {
+            type: 'VariableDeclarator',
+            id: {
+              type: 'Identifier',
+              name: '𩸽',
+            },
+            init: {
+              type: 'Literal',
+              value: '𩸽',
+            },
+          },
+        ],
+      },
+      {
+        type: 'VariableDeclaration',
+        declarations: [
+          {
+            type: 'VariableDeclarator',
+            id: {
+              type: 'Identifier',
+              name: 'a\u033f',
+            },
+            init: {
+              type: 'Literal',
+              value: '\u033f',
+            },
+          },
+        ],
+      },
+      {
+        type: 'ExpressionStatement',
+        expression: {
+          type: 'Literal',
+          value: '\ud83d\udea6',
+        },
+      },
+    ],
+  });
+});
