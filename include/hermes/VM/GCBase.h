5f22ad0461dfdc4605f329937730e328df92b4d2
Neil Dhar <neildhar@fb.com>
2020-10-29 21:47:27 -0700

    Clean up Runtime::alloc and JSObjectAlloc
    Summary: Use new GC::makeA API.
    
    Reviewed By: dulinriley
    
    Differential Revision: D24527669
    
    fbshipit-source-id: e74a02c63f18a89c251d5bb652c060c21d0039f0

--- include/hermes/VM/GCBase.h
+++ include/hermes/VM/GCBase.h
@@ -67,20 +67,13 @@ class Deserializer;
 // A specific GC class extend GCBase, and override its virtual functions.
 // In addition, it must implement the following methods:
 
-/// Allocate a new cell of the specified type \p vt and size \p size. If
-/// necessary perform a GC cycle, which may potentially move allocated
-/// objects. \p fixedSize should indicate whether the allocation is
-/// for a fixed-size, small object; some GCs may allow optimizations
-/// on this bases.  \p hasFinalizer must be \p HasFinalizer::Yes if
-/// cells of the given type require a finalizer to be called.
-///
-///   template <bool fixedSize = true,
-///             HasFinalizer hasFinalizer = HasFinalizer::No>
-///   CallResult<GCCell *> alloc(const VTable *vt, uint32_t size);
-///
-/// Allocate a new cell of type \p T and size \p size using the APIs above.
-/// Instantiate an object of type \p T in the newly allocated cell, using
-/// \p args as the arguments to its constructor.
+/// Allocate a new cell of type \p T and size \p size. Instantiate an object of
+/// type \p T in the newly allocated cell, using \p args as the arguments to its
+/// constructor. If necessary perform a GC cycle, which may potentially move
+/// allocated objects. \p fixedSize should indicate whether the allocation is
+/// for a fixed-size, small object; some GCs may allow optimizations on this
+/// basis. \p hasFinalizer must be \p HasFinalizer::Yes if cells of the given
+/// type require a finalizer to be called.
 ///
 ///   template <
 ///     typename T,
