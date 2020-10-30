6058e2297f04c4eb9b3ccc594ce5266a308f3687
Will Holen <willholen@fb.com>
2020-10-29 21:58:34 -0700

    Respect compileFlags.debug even if debugger is not compiled in
    Summary:
    Currently, we ignore compileFlags.debug if the debugger is compiled out.
    
    This is a problem for `emhermesc`, which sets the flag and serializes
    the bundle. If debugger support is not built in, the resulting bundle
    does not contain complete debug info. When loaded in a different
    Hermes instance that *does* have debugger built in, it can't inspect
    local variables due to missing lexical debug data.
    
    Reviewed By: dulinriley
    
    Differential Revision: D24524133
    
    fbshipit-source-id: bf6320740881b4922ea0116951247582a997bde8

--- lib/BCGen/HBC/BytecodeProviderFromSrc.cpp
+++ lib/BCGen/HBC/BytecodeProviderFromSrc.cpp
@@ -174,12 +174,8 @@ BCProviderFromSrc::createBCProviderFromSrc(
   context->setAllowFunctionToStringWithRuntimeSource(
       compileFlags.allowFunctionToStringWithRuntimeSource);
   context->setGeneratorEnabled(compileFlags.enableGenerator);
-#ifdef HERMES_ENABLE_DEBUGGER
   context->setDebugInfoSetting(
       compileFlags.debug ? DebugInfoSetting::ALL : DebugInfoSetting::THROWING);
-#else
-  context->setDebugInfoSetting(DebugInfoSetting::THROWING);
-#endif
   context->setEmitAsyncBreakCheck(compileFlags.emitAsyncBreakCheck);
 
   // Populate the declFileList.
