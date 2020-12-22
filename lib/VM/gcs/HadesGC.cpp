6a9afca6f8236b46ef940936cbaef1916be28e1b
Neil Dhar <neildhar@fb.com>
2020-12-22 04:46:27 -0800

    Use target heap size for compaction
    Summary:
    Start a compaction to shrink the heap whenever the target heap size
    falls below the current heap size. This means that Hades will track
    the occupancy target and current size, regardless of whether it goes
    up or down.
    
    Reviewed By: dulinriley
    
    Differential Revision: D25158654
    
    fbshipit-source-id: 8b3f44242a43a0f30847a4f0b116428e486be404

--- lib/VM/gcs/HadesGC.cpp
+++ lib/VM/gcs/HadesGC.cpp
@@ -1499,20 +1499,24 @@ void HadesGC::prepareCompactee() {
 
   llvh::Optional<size_t> compacteeIdx;
 #ifndef HERMESVM_SANITIZE_HANDLES
-  // Only compact a segment if it was at least 75% occupied at some point
-  // and is now at most at 1/3 of its peak. If multiple segments meet these
-  // criteria, then select the one with the fewest allocated bytes, to
-  // minimise scanning and copying.
+  // There are two possible criteria for compacting a segment:
+  // 1. If any segment was at least 75% occupied at some point
+  // and is now at most at 1/3 of its peak.
+  // 2. If the target size of the heap has fallen below its actual size.
   constexpr size_t kMinPeakBytesForCompaction =
       (HeapSegment::maxSize() * 3) / 4;
   constexpr size_t kMinPeakRatioForCompaction = 3;
   uint64_t minBytes = HeapSegment::maxSize();
+  const bool resizeCheck = oldGen_.targetSizeBytes() < oldGen_.size();
   for (size_t i = 0; i < oldGen_.numSegments(); ++i) {
     const size_t curBytes = oldGen_.allocatedBytes(i);
     const size_t peakBytes = oldGen_.peakAllocatedBytes(i);
-    if (peakBytes > kMinPeakBytesForCompaction &&
-        curBytes < peakBytes / kMinPeakRatioForCompaction &&
-        curBytes < minBytes) {
+    const bool ratioCheck = peakBytes > kMinPeakBytesForCompaction &&
+        curBytes < peakBytes / kMinPeakRatioForCompaction;
+
+    // Select the one with the fewest allocated bytes, to
+    // minimise scanning and copying.
+    if ((ratioCheck || resizeCheck) && curBytes < minBytes) {
       compacteeIdx = i;
       minBytes = curBytes;
     }
