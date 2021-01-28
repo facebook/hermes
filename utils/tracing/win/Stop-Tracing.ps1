5cb02234cc287ca829c950e1e8c5b94dc5f896f6
Anandraj <mganandraj@outlook.com>
2021-01-28 15:49:59 -0800

    [Inc. Feedbacks] Refactoring PS script, adding patch file for llvm changes

--- utils/tracing/win/Stop-Tracing.ps1
+++ utils/tracing/win/Stop-Tracing.ps1
@@ -1,44 +1,78 @@
-param(
-    [bool]$NoAnalysis = $False
+<#
+  .SYNOPSIS
+  Stops a Trace recording session previously started by running Start-Tracing.ps1, and optionally start analysis.
+
+  .DESCRIPTION
+  Stops a Trace recording session previously started by running Start-Tracing.ps1, and optionally start analysis.
+
+  .PARAMETER NoAnalysis
+  Include this switch if the script should only stop the session. This can be useful when including this call in other scripts, or to defer analysis to a later time.
+
+  .PARAMETER OutputPath
+  None.
+
+  .INPUTS
+  None. You cannot pipe objects to this script.
+
+  .OUTPUTS
+  None. This script does not generate any output.
+
+  .EXAMPLE
+  PS> .\utils\tracing\win\Start-Tracing.ps1 -IncludeGeneralTrace
+
+  .EXAMPLE
+  PS> .\utils\tracing\win\Start-Tracing.ps1
+#>
+
+
+[CmdletBinding()]
+Param(
+    [Parameter(ParameterSetName='Default', Position=0, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+    [switch]$NoAnalysis
 )
 
-$mypath = split-path -parent $MyInvocation.MyCommand.Definition
-$timestamp = [DateTime]::Now.ToString("yyyyMMdd_HHmmss") 
-$fileName = "rnw_" + $timestamp + ".etl"
-$etlPath = Join-Path -Path $mypath -ChildPath $fileName
+begin {
+    Set-StrictMode -Version Latest
 
-if (!(Get-Command "wpr.exe" -ErrorAction SilentlyContinue)) { 
-    throw "
-   ################################################################
-   wpr.exe (Windows Performance Recorder) unavailable ! 
-   Ensure that WPT (Windows Performance Toolkit) is installed and its tools are added to the path 
-   ################################################################"
-}
- 
-$wprArgs = "-stop", $etlPath
-
-Write-Host "wpr.exe $wprArgs"
-& Start-Process -Verb RunAs "wpr.exe" -ArgumentList $wprArgs -PassThru -Wait
-
-if (!(Get-Command "wpa.exe" -ErrorAction SilentlyContinue)) { 
-    Write-Host "wpa.exe (Windows Performance Analyzer) unavailable !"
-    Write-Host "Ensure that WPT (Windows Performance Toolkit) is installed and its tools are added to the path "
+    $scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+    $timeStamp = [DateTime]::Now.ToString("yyyyMMdd_HHmmss") 
+    $etlFileName = "rnw_" + $timeStamp + ".etl"
+    $etlFilePath = Join-Path -Path $scriptPath -ChildPath $etlFileName
+    $wprArgs = "-stop", $etlFilePath
 }
 
-if ($NoAnalysis) {
-    Write-Host "To analys this trace, install WPT and run: 'wpa $etlPath'"
-}
-else {
-
-    if (!(Get-Command "wpa.exe" -ErrorAction SilentlyContinue)) { 
+process{
+    if (!(Get-Command "wpr.exe" -ErrorAction SilentlyContinue)) { 
         throw "
-        ################################################################
-        wpa.exe (Windows Performance Analyzer) unavailable ! 
-        Ensure that WPT (Windows Performance Toolkit) is installed and its tools are added to the path 
-        ################################################################"
+    ################################################################
+    wpr.exe (Windows Performance Recorder) unavailable ! 
+    Ensure that WPT (Windows Performance Toolkit) is installed and its tools are added to the path 
+    ################################################################"
     }
 
-    $wpaArgs = $etlPath
-    Write-Host "wpa.exe $wpaArgs"
-    & Start-Process -Verb RunAs "wpa.exe" -ArgumentList $wpaArgs
+    Write-Host "wpr.exe $wprArgs"
+    & Start-Process -Verb RunAs "wpr.exe" -ArgumentList $wprArgs -PassThru -Wait
+
+    if (!(Get-Command "wpa.exe" -ErrorAction SilentlyContinue)) { 
+        Write-Host "wpa.exe (Windows Performance Analyzer) unavailable !"
+        Write-Host "Ensure that WPT (Windows Performance Toolkit) is installed and its tools are added to the path "
+    }
+
+    if ($NoAnalysis.IsPresent) {
+        Write-Host "To analys this trace, install WPT and run: 'wpa $etlFilePath'"
+    }
+    else {
+
+        if (!(Get-Command "wpa.exe" -ErrorAction SilentlyContinue)) { 
+            throw "
+            ################################################################
+            wpa.exe (Windows Performance Analyzer) unavailable ! 
+            Ensure that WPT (Windows Performance Toolkit) is installed and its tools are added to the path 
+            ################################################################"
+        }
+
+        $wpaArgs = $etlFilePath
+        Write-Host "wpa.exe $wpaArgs"
+        & Start-Process -Verb RunAs "wpa.exe" -ArgumentList $wpaArgs
+    }
 }
