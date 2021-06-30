b3ecdb5a00498fd064c640be8e83034e7d497231
Arkkeeper <arkkeeper@yandex.ru>
2021-06-30 00:34:42 -0700

    arm64 simulators & catalyst support (#475)
    Summary:
    Edits build scripts to allow merging architectures into .xcframework instead of using lipo for building fat framework.
    Adds compatibility with arm64 iOS Simulators on Apple M1 Macs and allows using Hermes with RN apps built for Mac Catalyst.
    
    Fixes https://github.com/facebook/hermes/issues/460 and https://github.com/facebook/hermes/issues/468
    
    Pull Request resolved: https://github.com/facebook/hermes/pull/475
    
    Differential Revision: D29465045
    
    Pulled By: Huxpro
    
    fbshipit-source-id: 6291aac9ee47db83d6f78822767019ec0277a899

--- utils/build-apple-framework.sh
+++ utils/build-apple-framework.sh
@@ -46,7 +46,8 @@ function build_host_hermesc {
 # Utility function to configure an Apple framework
 function configure_apple_framework {
   local build_cli_tools enable_bitcode
-  if [[ $1 == iphoneos ]]; then
+
+  if [[ $1 == iphoneos || $1 == catalyst ]]; then
     enable_bitcode="true"
   else
     enable_bitcode="false"
@@ -98,22 +99,21 @@ function create_universal_framework {
   cd ./destroot/Library/Frameworks || exit 1
 
   local platforms=("$@")
+  local args=""
 
   echo "Creating universal framework for platforms: ${platforms[*]}"
 
   for i in "${!platforms[@]}"; do
-    platforms[$i]="${platforms[$i]}/hermes.framework/hermes"
+    args+="-framework ${platforms[$i]}/hermes.framework "
   done
 
-  lipo -create -output "${platforms[0]}" "${platforms[@]}"
+  xcodebuild -create-xcframework $args -output "${platforms[0]}/hermes.xcframework"
 
   # Once all was linked into a single framework, clean destroot
   # from unused frameworks
   for platform in "${@:2}"; do
     rm -r "$platform"
   done
-
-  lipo -info "${platforms[0]}"
 
   cd - || exit 1
 }
