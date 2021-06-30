0db13f7e63adc2c12913f85cfd27a298213c9541
Raghav Rao <rrao@fb.com>
2021-06-30 00:51:07 -0700

    Fix JNI leaks
    Summary:
    Oacr(assistant platform) creates native threads on every start and these thread make calls to glog ex. https://fburl.com/diffusion/pzx0nsai
    Here is full backtrace P426317875
    Glog internally would internally call AndroidLogSink::send which does the attach but there is no detach.
    These threads are not aware that the attach happened. This diffs add an attach/detach in scope so that there is no memory leak.
    
    We are seeing thousands of virtual memory leaks for Portal voice app https://fburl.com/everpaste/1bp5hwsj
    This exception P426037302 is present in logview for portal voice assistant app and other apps like alohausers and is happening here in the code where mmap is failing https://fburl.com/diffusion/h1kqd68k. Error = 12 is 'no memory' and in this context it virtual memory.
    
    When we checked the memory map(pmap) we  found thousands of IndirectReference mapping and growing steadily and ultimately it leads to virtual memory exhaustion.
    
    0000000053f48000     16K rw---    [anon:dalvik-indirect ref table]
    0000000053f54000      8K rw---    [anon:dalvik-indirect ref table]
    0000000053f6b000     24K rw---    [anon:dalvik-indirect ref table]
    0000000054040000     16K rw---    [anon:dalvik-indirect ref table]
    00000000540d2000      8K rw---    [anon:dalvik-indirect ref table]
    
    allocated from here https://fburl.com/diffusion/ru1gd35e
    
    **logview**
    
    {F628796337}
    
    Reviewed By: JunqiWang
    
    Differential Revision: D29433693
    
    fbshipit-source-id: cfabc9c6bc0e44d1032ee5eba1e32d43c815bae8

--- first-party/fbjni/cxx/fbjni/detail/Environment.cpp
+++ first-party/fbjni/cxx/fbjni/detail/Environment.cpp
@@ -322,6 +322,11 @@ JNIEnv* Environment::ensureCurrentThreadIsAttached() {
 }
 
 /* static */
+void Environment::ensureCurrentThreadAttachDetachInScope(std::function<void()>&& runnable) {
+  ThreadScope::WithClassLoader(std::move(runnable));
+}
+
+/* static */
 bool Environment::isGlobalJvmAvailable() {
   return g_vm != nullptr;
 }
