2d46aa85c0bfa98b6c38f66c5054eb4abe939cf3
Adithi Rao <adithirao@fb.com>
2021-07-30 14:59:10 -0700

    Console error test case
    Summary: Example use case of console.error/console.warn.
    
    Reviewed By: avp
    
    Differential Revision: D29943190
    
    fbshipit-source-id: 8c11f9a272d7165366337ea63c9b199ea7bafc0e

---
+++ test/node-hermes/console-error.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+// RUN: %node-hermes %s 2>&1 | %FileCheck --match-full-lines %s
+// REQUIRES: node-hermes
+
+print('Console error functionality');
+// CHECK-LABEL: Console error functionality
+
+var name = 'Will Robinson';
+console.warn(`Danger ${name}! Danger!`);
+// CHECK: Danger Will Robinson! Danger!
+
+var code = 5;
+console.error('error #%d', code);
+// CHECK-NEXT: error #5
