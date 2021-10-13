118c8849b7d69a205f97a83d90ff1760df6e7882
Sarah Fluck <sarahjf2047@gmail.com>
2021-10-13 14:09:40 +0100

    Working through NS errors

--- lib/Platform/Intl/PlatformIntlApple.mm
+++ lib/Platform/Intl/PlatformIntlApple.mm
@@ -19,7 +19,7 @@ vm::CallResult<std::vector<std::u16string>> getCanonicalLocales(
     // 1. If locales is undefined, then
     // Return a new empty List.
     if (locales.size() == 0) {
-      return std::vector<std::u16string>();
+        return std::vector<std::u16string>();
     }
     // Note:: Some other major input validation occurs closer to VM in 'normalizeLocales' in
     // JSLib/Intl.cpp
@@ -39,181 +39,149 @@ vm::CallResult<std::vector<std::u16string>> getCanonicalLocales(
     // 7. Repeat, while k < len
     
     for(std::u16string locale : locales) {
-        // We don't have steps for 7a. 7b. 7c. i-iv  .. as we only allow string arrays here..
+    {   // We don't have steps for 7a. 7b. 7c. i-iv  .. as we only allow string arrays here..
         // Smoke validation.
         // Throw RangeError if input locale string is (1) empty (2) non-ASCII string.
-        if (local == nil) {
-            // Ask how to throw exception
+//        if (locale == nil) {
+//            // Ask how to throw exception
+//            return runtime->raiseRangeError("Incorrect locale information provided");
+//        }
+        if (locale.empty()) {
             return runtime->raiseRangeError("Incorrect locale information provided");
-        }
-        if (locale->empty()) {
-            return runtime->raiseRangeError("Incorrect locale information provided");
-        }
-        for(char const &c: *locale) {
-            if (static_cast<unsigned char>(c) > 127) {
-              return runtime->raiseRangeError("Incorrect locale information provided");
-            }
         }
         // 7.c.v & 7.c.vi
         // Check for tag later
-        std::u16string canonicalizedTag = [NSLocale canonicalLocaleIdentifierFromString:locale];
-        
+        //std::u16string canonicalizedTag = [NSLocale canonicalLocaleIdentifierFromString:locale];
+        std::u16string canonicalizedTag = locale;
         // 7.c.vii
-        // 'error: invalid argument type 'std::__wrap_iter<std::basic_string<char16_t> *>' to unary expression' with '&& !std::find(seen.begin(), seen.end(), canonicalizedTag)'
-        if(!canonicalizedTag.empty() 
-            && !std::find(seen.begin(), seen.end(), canonicalizedTag) != seen.end()) {
+        if(!canonicalizedTag.empty()
+            && std::find(seen.begin(), seen.end(), canonicalizedTag) != seen.end()) {
             seen.push_back(canonicalizedTag);
         }
+    }}
+        return seen;
     }
-    return seen;
-}
-
-// Implementer note: This method corresponds roughly to
-// https://tc39.es/ecma402/#sec-canonicalizelocalelist
-//
-// Also see the implementer notes on DateTimeFormat#DateTimeFormat()
-// for more discussion of locales and CanonicalizeLocaleList.
-
-//1. Let O be RequireObjectCoercible(this value).
-//2. Let S be ? ToString(O).
-vm::CallResult<std::u16string> toLocaleLowerCase(
-    vm::Runtime *runtime,
-    const std::vector<std::u16string> &locales,
-    const std::u16string &str) {
-  return std::u16string(u"lowered");
-}
-
-
-vm::CallResult<std::u16string> toLocaleUpperCase(
-    vm::Runtime *runtime,
-    const std::vector<std::u16string> &locales,
-    const std::u16string &str) {
-  return std::u16string(u"uppered");
-}
-
-struct Collator::Impl {
-  std::u16string locale;
-};
-
-Collator::Collator() : impl_(std::make_unique<Impl>()) {}
-Collator::~Collator() {}
-
-vm::CallResult<std::vector<std::u16string>> Collator::supportedLocalesOf(
-    vm::Runtime *runtime,
-    const std::vector<std::u16string> &locales,
-    const Options &options) noexcept {
-  return std::vector<std::u16string>{u"en-CA", u"de-DE"};
-}
-
-vm::ExecutionStatus Collator::initialize(
-    vm::Runtime *runtime,
-    const std::vector<std::u16string> &locales,
-    const Options &options) noexcept {
-  impl_->locale = u"en-US";
-  return vm::ExecutionStatus::RETURNED;
-}
-
-Options Collator::resolvedOptions() noexcept {
-  Options options;
-  options.emplace(u"locale", Option(impl_->locale));
-  options.emplace(u"numeric", Option(false));
-  return options;
-}
-
-double Collator::compare(
-    const std::u16string &x,
-    const std::u16string &y) noexcept {
-  return x.compare(y);
-}
-
-struct DateTimeFormat::Impl {
-  std::u16string locale;
-};
-
-DateTimeFormat::DateTimeFormat() : impl_(std::make_unique<Impl>()) {}
-DateTimeFormat::~DateTimeFormat() {}
-
-vm::CallResult<std::vector<std::u16string>> DateTimeFormat::supportedLocalesOf(
-    vm::Runtime *runtime,
-    const std::vector<std::u16string> &locales,
-    const Options &options) noexcept {
-  return std::vector<std::u16string>{u"en-CA", u"de-DE"};
-}
-
-vm::ExecutionStatus DateTimeFormat::initialize(
-    vm::Runtime *runtime,
-    const std::vector<std::u16string> &locales,
-    const Options &options) noexcept {
-  impl_->locale = u"en-US";
-  return vm::ExecutionStatus::RETURNED;
-}
-
-Options DateTimeFormat::resolvedOptions() noexcept {
-  Options options;
-  options.emplace(u"locale", Option(impl_->locale));
-  options.emplace(u"numeric", Option(false));
-  return options;
-}
-
-std::u16string DateTimeFormat::format(double jsTimeValue) noexcept {
-  auto s = std::to_string(jsTimeValue);
-  return std::u16string(s.begin(), s.end());
-}
-
-std::vector<std::unordered_map<std::u16string, std::u16string>>
-DateTimeFormat::formatToParts(double jsTimeValue) noexcept {
-  std::unordered_map<std::u16string, std::u16string> part;
-  part[u"type"] = u"integer";
-  // This isn't right, but I didn't want to do more work for a stub.
-  std::string s = std::to_string(jsTimeValue);
-  part[u"value"] = {s.begin(), s.end()};
-  return std::vector<std::unordered_map<std::u16string, std::u16string>>{part};
-}
-
-struct NumberFormat::Impl {
-  std::u16string locale;
-};
-
-NumberFormat::NumberFormat() : impl_(std::make_unique<Impl>()) {}
-NumberFormat::~NumberFormat() {}
-
-vm::CallResult<std::vector<std::u16string>> NumberFormat::supportedLocalesOf(
-    vm::Runtime *runtime,
-    const std::vector<std::u16string> &locales,
-    const Options &options) noexcept {
-  return std::vector<std::u16string>{u"en-CA", u"de-DE"};
-}
-
-vm::ExecutionStatus NumberFormat::initialize(
-    vm::Runtime *runtime,
-    const std::vector<std::u16string> &locales,
-    const Options &options) noexcept {
-  impl_->locale = u"en-US";
-  return vm::ExecutionStatus::RETURNED;
-}
-
-Options NumberFormat::resolvedOptions() noexcept {
-  Options options;
-  options.emplace(u"locale", Option(impl_->locale));
-  options.emplace(u"numeric", Option(false));
-  return options;
-}
-
-std::u16string NumberFormat::format(double number) noexcept {
-  auto s = std::to_string(number);
-  return std::u16string(s.begin(), s.end());
-}
-
-std::vector<std::unordered_map<std::u16string, std::u16string>>
-NumberFormat::formatToParts(double number) noexcept {
-  std::unordered_map<std::u16string, std::u16string> part;
-  part[u"type"] = u"integer";
-  // This isn't right, but I didn't want to do more work for a stub.
-  std::string s = std::to_string(number);
-  part[u"value"] = {s.begin(), s.end()};
-  return std::vector<std::unordered_map<std::u16string, std::u16string>>{part};
-}
-
-} // namespace platform_intl
-} // namespace hermes
-
+    // Implementer note: This method corresponds roughly to
+    // https://tc39.es/ecma402/#sec-canonicalizelocalelist
+    //
+    // Also see the implementer notes on DateTimeFormat#DateTimeFormat()
+    // for more discussion of locales and CanonicalizeLocaleList.
+    //1. Let O be RequireObjectCoercible(this value).
+    //2. Let S be ? ToString(O).
+    vm::CallResult<std::u16string> toLocaleLowerCase(
+        vm::Runtime *runtime,
+        const std::vector<std::u16string> &locales,
+        const std::u16string &str) {
+      return std::u16string(u"lowered");
+    }
+    vm::CallResult<std::u16string> toLocaleUpperCase(
+        vm::Runtime *runtime,
+        const std::vector<std::u16string> &locales,
+        const std::u16string &str) {
+      return std::u16string(u"uppered");
+    }
+    struct Collator::Impl {
+      std::u16string locale;
+    };
+    Collator::Collator() : impl_(std::make_unique<Impl>()) {}
+    Collator::~Collator() {}
+    vm::CallResult<std::vector<std::u16string>> Collator::supportedLocalesOf(
+        vm::Runtime *runtime,
+        const std::vector<std::u16string> &locales,
+        const Options &options) noexcept {
+      return std::vector<std::u16string>{u"en-CA", u"de-DE"};
+    }
+    vm::ExecutionStatus Collator::initialize(
+        vm::Runtime *runtime,
+        const std::vector<std::u16string> &locales,
+        const Options &options) noexcept {
+      impl_->locale = u"en-US";
+      return vm::ExecutionStatus::RETURNED;
+    }
+    Options Collator::resolvedOptions() noexcept {
+      Options options;
+      options.emplace(u"locale", Option(impl_->locale));
+      options.emplace(u"numeric", Option(false));
+      return options;
+    }
+    double Collator::compare(
+        const std::u16string &x,
+        const std::u16string &y) noexcept {
+      return x.compare(y);
+    }
+    struct DateTimeFormat::Impl {
+      std::u16string locale;
+    };
+    DateTimeFormat::DateTimeFormat() : impl_(std::make_unique<Impl>()) {}
+    DateTimeFormat::~DateTimeFormat() {}
+    vm::CallResult<std::vector<std::u16string>> DateTimeFormat::supportedLocalesOf(
+        vm::Runtime *runtime,
+        const std::vector<std::u16string> &locales,
+        const Options &options) noexcept {
+      return std::vector<std::u16string>{u"en-CA", u"de-DE"};
+    }
+    vm::ExecutionStatus DateTimeFormat::initialize(
+        vm::Runtime *runtime,
+        const std::vector<std::u16string> &locales,
+        const Options &options) noexcept {
+      impl_->locale = u"en-US";
+      return vm::ExecutionStatus::RETURNED;
+    }
+    Options DateTimeFormat::resolvedOptions() noexcept {
+      Options options;
+      options.emplace(u"locale", Option(impl_->locale));
+      options.emplace(u"numeric", Option(false));
+      return options;
+    }
+    std::u16string DateTimeFormat::format(double jsTimeValue) noexcept {
+      auto s = std::to_string(jsTimeValue);
+      return std::u16string(s.begin(), s.end());
+    }
+    std::vector<std::unordered_map<std::u16string, std::u16string>>
+    DateTimeFormat::formatToParts(double jsTimeValue) noexcept {
+      std::unordered_map<std::u16string, std::u16string> part;
+      part[u"type"] = u"integer";
+      // This isn't right, but I didn't want to do more work for a stub.
+      std::string s = std::to_string(jsTimeValue);
+      part[u"value"] = {s.begin(), s.end()};
+      return std::vector<std::unordered_map<std::u16string, std::u16string>>{part};
+    }
+    struct NumberFormat::Impl {
+      std::u16string locale;
+    };
+    NumberFormat::NumberFormat() : impl_(std::make_unique<Impl>()) {}
+    NumberFormat::~NumberFormat() {}
+    vm::CallResult<std::vector<std::u16string>> NumberFormat::supportedLocalesOf(
+        vm::Runtime *runtime,
+        const std::vector<std::u16string> &locales,
+        const Options &options) noexcept {
+      return std::vector<std::u16string>{u"en-CA", u"de-DE"};
+    }
+    vm::ExecutionStatus NumberFormat::initialize(
+        vm::Runtime *runtime,
+        const std::vector<std::u16string> &locales,
+        const Options &options) noexcept {
+      impl_->locale = u"en-US";
+      return vm::ExecutionStatus::RETURNED;
+    }
+    Options NumberFormat::resolvedOptions() noexcept {
+      Options options;
+      options.emplace(u"locale", Option(impl_->locale));
+      options.emplace(u"numeric", Option(false));
+      return options;
+    }
+    std::u16string NumberFormat::format(double number) noexcept {
+      auto s = std::to_string(number);
+      return std::u16string(s.begin(), s.end());
+    }
+    std::vector<std::unordered_map<std::u16string, std::u16string>>
+    NumberFormat::formatToParts(double number) noexcept {
+      std::unordered_map<std::u16string, std::u16string> part;
+      part[u"type"] = u"integer";
+      // This isn't right, but I didn't want to do more work for a stub.
+      std::string s = std::to_string(number);
+      part[u"value"] = {s.begin(), s.end()};
+      return std::vector<std::unordered_map<std::u16string, std::u16string>>{part};
+    }
+    } // namespace platform_intl
+    } // namespace hermes
