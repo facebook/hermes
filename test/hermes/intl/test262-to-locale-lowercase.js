2cf333cad72047509d803bad99d2f69d73212f91
Sarah Fluck <sarahjf2047@gmail.com>
2021-11-03 13:28:43 +0000

    Add copyright notice

--- test/hermes/intl/test262-to-locale-lowercase.js
+++ test/hermes/intl/test262-to-locale-lowercase.js
@@ -4,6 +4,18 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+/* This code is adapted from Test262, which has the following copyright notice:
+ *
+ * The << Software identified by reference to the Ecma Standard* ("Software)">>  is protected by copyright and is being 
+ * made available under the  "BSD License", included below. This Software may be subject to third party rights (rights 
+ * from parties other than Ecma International), including patent rights, and no licenses under such third party rights 
+ * are granted under this license even if the third party concerned is a member of Ecma International.  SEE THE ECMA 
+ * CODE OF CONDUCT IN PATENT MATTERS AVAILABLE AT http://www.ecma-international.org/memento/codeofconduct.htm FOR 
+ *INFORMATION REGARDING THE LICENSING OF PATENT CLAIMS THAT ARE REQUIRED TO IMPLEMENT ECMA INTERNATIONAL STANDARDS*.
+ *
+ * Copyright (C) 2012-2013 Ecma International
+ * All rights reserved.
+*/
 
 // RUN: %hermes -O -target=HBC %s | %FileCheck --match-full-lines %s
 // REQUIRES: intl
