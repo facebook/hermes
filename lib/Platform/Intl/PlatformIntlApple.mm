652a700df12bcf677c4d957a899bb73805cb2d43
Sarah Fluck <sarahjf2047@gmail.com>
2021-11-25 22:34:31 +0000

    Initial commit

--- lib/Platform/Intl/PlatformIntlApple.mm
+++ lib/Platform/Intl/PlatformIntlApple.mm
@@ -272,7 +272,8 @@ DateTimeFormat::formatToParts(double jsTimeValue) noexcept {
 }
 
 struct NumberFormat::Impl {
-  std::u16string locale;
+  std::u16string mResolvedStyle, mResolvedCurrency, mResolvedCurrencyDisplay, mResolvedCurrencySign, mResolvedUnit, mResolvedUnitDisplay, mRoundingType, locale;
+  uint8_t mResolvedMinimumIntegerDigits, mResolvedMinimumFractionDigits, mResolvedMaximumFractionDigits;
 };
 
 NumberFormat::NumberFormat() : impl_(std::make_unique<Impl>()) {}
@@ -284,6 +285,186 @@ vm::CallResult<std::vector<std::u16string>> NumberFormat::supportedLocalesOf(
     const Options &options) noexcept {
   return std::vector<std::u16string>{u"en-CA", u"de-DE"};
 }
+
+// https://tc39.es/ecma402/#sec-defaultnumberoption
+vm::CallResult<std::uint8_t> defaultNumberOption(const std::uint8_t value,
+  const std::uint8_t minimum, const std::uint8_t maximum,
+  const std::uint8_t fallback) {
+//  1. If value is undefined, return fallback.
+  if (!value) {
+    return fallback;
+  }
+//  2. Set value to ? ToNumber(value).
+//  3. If value is NaN or less than minimum or greater than maximum, throw a RangeError exception.
+  if (value == NAN || value < minimum || value > maximum) {
+    return vm::ExecutionStatus::EXCEPTION;
+  }
+//  4. Return floor(value).
+  return std::floor(value);
+}
+// TODO: isWellFormedUnitIdentifier
+
+// https://tc39.es/ecma402/#sec-case-sensitivity-and-case-mapping
+// Note that we should convert only upper case translation in ASCII range.
+std::u16string normalizeCurrencyCode(std::u16string currencyCode) {
+  std::u16string normalized;
+  std::uint8_t offset = 'a' - 'A';
+  for (std::uint8_t idx = 0; idx < currencyCode.length(); idx++) {
+    unsigned char c = currencyCode[idx];
+    if (c >= 'a' && c <= 'z') {
+      normalized.push_back((char) c - offset);
+    }
+    else {
+      normalized.push_back(c);
+    }
+    }
+  return normalized;
+}
+
+// https://tc39.es/ecma402/#sec-iswellformedcurrencycode
+bool isWellFormedCurrencyCode(std::u16string currencyCode) {
+//  1. Let normalized be the result of mapping currency to upper case as described in 6.1.
+  std::u16string normalized = normalizeCurrencyCode(currencyCode);
+//  2. If the number of elements in normalized is not 3, return false.
+  if (normalized.size() != 3) {
+    return false;
+  }
+//  3. If normalized contains any character that is not in the range "A" to "Z" (U+0041 to U+005A), return false.
+  if (!std::all_of(std::begin(normalized), std::end(normalized),
+                   [](char c){ return std::isupper(c); })) {
+    return false;
+  }
+//  4. Return true.
+  return true;
+}
+// https://tc39.es/ecma402/#sec-setnumberformatunitoptions
+vm::CallResult<Options> setNumberFormatUnitOptions(const Options &options) {
+//  1. Assert: Type(intlObj) is Object.
+//  2. Assert: Type(options) is Object.
+//  3. Let style be ? GetOption(options, "style", "string", « "decimal", "percent", "currency", "unit" », "decimal").
+//  4. Set intlObj.[[Style]] to style.
+  const std::vector<std::u16string> styleVector = {u"decimal", u"percent", u"currency", u"unit"};
+  auto style = getOption(options, u"style", u"string", styleVector, u"decimal");
+  // Want to use impl_ but it can't be accessed here?
+  impl_->mResolvedStyle = style;
+//  Let currency be ? GetOption(options, "currency", "string", undefined, undefined).
+//  TODO: undefined fallbacks
+  auto currency = getOption(options, u"currency", u"string", undefined, undefined);
+//  6. If currency is undefined, then
+  if (currency->getString() == u"") {
+//  a. If style is "currency", throw a TypeError exception.
+    if (impl_->mResolvedStyle == u"currency") {
+      // Throw typeError through initialize?
+      return vm::ExecutionStatus::EXCEPTION;
+    }
+    //  7. Else,
+    else {
+    //  a. If the result of IsWellFormedCurrencyCode(currency) is false, throw a RangeError exception.
+      if (!isWellFormedCurrencyCode(currency)) {
+        return vm::ExecutionStatus::EXCEPTION;
+      }
+    }
+  }
+//  8. Let currencyDisplay be ? GetOption(options, "currencyDisplay", "string", « "code", "symbol", "narrowSymbol", "name" », "symbol").
+  std::vector<std::u16string> currencyDisplayVector = {u"code", u"symbol", u"narrowSymbol", u"name"};
+  auto currencyDisplay = getOption(options, u"currencyDisplay", u"string", currencyDisplayVector, u"symbol");
+//  9. Let currencySign be ? GetOption(options, "currencySign", "string", « "standard", "accounting" », "standard").
+  std::vector<std::u16string> currencySignVector = {u"standard", u"accounting"};
+  auto currencySign = getOption(options, u"currencySign", u"string", currencySignVector, u"standard");
+//  10. Let unit be ? GetOption(options, "unit", "string", undefined, undefined).
+  auto unit = getOption(options, u"unit", u"string", undefined, undefined);
+//  11. If unit is undefined, then
+  if (unit->getString() == u"") {
+//  a. If style is "unit", throw a TypeError exception.
+    if (impl_->mResolvedStyle == u"unit") {
+      return runtime->raiseTypeError("Expected unit !");
+    }
+//  12. Else,
+    else {
+//  a. If the result of IsWellFormedUnitIdentifier(unit) is false, throw a RangeError exception.
+      if (!isWellFormedCurrencyCode(unit)) {
+        return runtime->raiseRangeError("Expected unit !");
+      }
+    }
+  }
+//  13. Let unitDisplay be ? GetOption(options, "unitDisplay", "string", « "short", "narrow", "long" », "short").
+  auto unitDisplay = getOption(options, u"unitDisplay", u"string", {u"short", u"narrow", u"long"}, u"short");
+//  14. If style is "currency", then
+  if (impl_->mResolvedStyle == u"currency") {
+//  a. Let currency be the result of converting currency to upper case as specified in 6.1.
+//  b. Set intlObj.[[Currency]] to currency.
+//  c. Set intlObj.[[CurrencyDisplay]] to currencyDisplay.
+//  d. Set intlObj.[[CurrencySign]] to currencySign.
+    impl_->mResolvedCurrency = normalizeCurrencyCode(currency);
+    impl_->mCurrencyDisplay = currencyDisplay;
+    impl_->mCurrencySign = currencySign;
+  }
+  if (impl_->mResolvedStyle == u"unit") {
+//  15. If style is "unit", then
+//  a. Set intlObj.[[Unit]] to unit.
+//  b. Set intlObj.[[UnitDisplay]] to unitDisplay.
+    impl_->mResolvedUnit = unit->getString();
+    impl_->mResolvedUnitDisplay = unitDisplay->getString();
+  }
+  return vm::ExecutionStatus::RETURNED;
+}
+
+// https://tc39.es/ecma402/#sec-setnfdigitoptions
+vm::CallResult<Options> setNumberFormatDigitOptions(const Options &options) {
+//  1. Let mnid be ? GetNumberOption(options, "minimumIntegerDigits,", 1, 21, 1).
+  auto mnid = getOptionNumber(options, u"minimumIntegerDigits", 1, 21, 1);
+//  2. Let mnfd be ? Get(options, "minimumFractionDigits").
+  auto mnfd = options.find(u"minimumFractionDigits");
+//  3. Let mxfd be ? Get(options, "maximumFractionDigits").
+  auto mxfd = options.find(u"maximumFractionDigits");
+//  4. Let mnsd be ? Get(options, "minimumSignificantDigits").
+  auto mnsd = options.find(u"minimumSignificantDigits");
+//  5. Let mxsd be ? Get(options, "maximumSignificantDigits").
+  auto mxsd = options.find(u"maximumSignificantDigits");
+//  6. Set intlObj.[[MinimumIntegerDigits]] to mnid.
+//  May need to be rounded?
+  impl_->mResolvedMinimumIntegerDigits = mnid.getNumber();
+  bool hasSd, hasFd, needSd;
+//  If mnsd is not undefined or mxsd is not undefined, then
+  if (mnsd != u"" || mxsd != u"") {
+    // mRoundingType = SIGNIFICANT_DIGITS?
+    mnsd = defaultNumberOption(mnsd->second.getNumber(), 1, 21, 1);
+    mxsd = defaultNumberOption(mxsd->second.getNumber(), mnsd->second.getNumber(), 21, 21);
+    impl_->mResolvedMinimumSignificantDigits = std::floor(mnsd);
+    impl_-> mResolvedMaximumSignificantDigits = std::floor(mxsd);
+  } else if (mnfd != u"" || mxfd != u"") {
+    // mRoundingType = FRACTION_DIGITS?
+    // TODO: Find out what mnfdDefault is
+    mnfd = defaultNumberOption(mnfd->second.getNumber(), 0, 20, mnfdDefault);
+    //  Object mxfdActualDefault = JSObjects.newNumber(Math.max(JSObjects.getJavaDouble(mnfd), JSObjects.getJavaDouble(mxfdDefault)));
+    mxfd = defaultNumberOption(mxfd, mnfd, 20, mxfdActualDefault);
+    impl_->mResolvedMinimumFractionDigits = std::floor(mnfd);
+    impl_->mResolvedMaximumFractionDigits = std::floor(mxfd);
+  } else if (mResolvedNotation == COMPACT) {
+    impl_->mRoundingType = COMPACT_ROUNDING;
+  } else if (mResolvedNotation == ENGINEERING) {
+// The default setting for engineering notation.
+// This is not based on the spec, but is required by our implementation of engineering
+// notation.
+// From
+// https://unicode-org.github.io/icu-docs/apidoc/released/icu4c/classicu_1_1DecimalFormat.html
+// If areSignificantDigitsUsed() returns false, then the minimum number of significant digits
+// shown is one,
+// and the maximum number of significant digits shown is the sum of the minimum integer and
+// maximum fraction digits,
+// and is unaffected by the maximum integer digits.
+//
+// In short, the minimum integer will be set to 1 and hence to achieve maximum default
+// fraction digits of "3" (as in spec), we should set the maximum fraction digits to "5"
+    impl_->mRoundingType = FRACTION_DIGITS;
+    mResolvedMaximumFractionDigits = 5;
+  } else {
+    impl_->mRoundingType = FRACTION_DIGITS;
+    impl_->mResolvedMinimumFractionDigits = std::floor(mnfdDefault);
+    impl_->mResolvedMaximumFractionDigits = std::floor(mxfdDefault);
+  }
+}
+// TODO: Add isLocaleIdType
 
 vm::ExecutionStatus NumberFormat::initialize(
     vm::Runtime *runtime,
