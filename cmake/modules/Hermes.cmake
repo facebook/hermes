ade0f9e506ccfda05362e8ebcee7c63010d1b5ca
Neil Dhar <neildhar@fb.com>
2022-01-04 21:19:03 -0800

    Disable -fno-strict-float-cast-overflow flag
    Summary:
    This breaks the open source iOS build, back it out temporarily since it
    doesn't have the intended effect anyway.
    
    Reviewed By: mhorowitz
    
    Differential Revision: D33290406
    
    fbshipit-source-id: b5dbfa93a1a04e1e1b53d48b857cba867393eaaf

--- cmake/modules/Hermes.cmake
+++ cmake/modules/Hermes.cmake
@@ -420,7 +420,9 @@ if (GCC_COMPATIBLE)
   add_flag_if_supported("-Wdelete-non-virtual-dtor" DELETE_NON_VIRTUAL_DTOR_FLAG)
 
   # Avoid triggering arbitrary UB when converting doubles to ints.
-  add_flag_if_supported("-fno-strict-float-cast-overflow" NO_STRICT_FLOAT_CAST_OVERFLOW_FLAG)
+  # TODO(T108716033) Evaluate adding this flag back in once a new clang is
+  # released or XCode is fixed.
+  # add_flag_if_supported("-fno-strict-float-cast-overflow" NO_STRICT_FLOAT_CAST_OVERFLOW_FLAG)
 
   # Disable range loop analysis warnings.
   check_cxx_compiler_flag("-Wrange-loop-analysis" RANGE_ANALYSIS_FLAG)
