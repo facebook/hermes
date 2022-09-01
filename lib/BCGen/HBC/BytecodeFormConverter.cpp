46ecbb3a24603487f516d8004cb7adae50c9294b
Vladimir Morozov <vmorozov@microsoft.com>
2022-09-01 13:18:12 -0700

    Fix runtime error for the wrong indirection on destruction

--- lib/BCGen/HBC/BytecodeFormConverter.cpp
+++ lib/BCGen/HBC/BytecodeFormConverter.cpp
@@ -165,7 +165,7 @@ class BytecodeFormConverter {
       BytecodeForm sourceForm)
       : bytes_(bytes), fields_(fields) {
     auto res = hbc::BCProviderFromBuffer::createBCProviderFromBuffer(
-        std::make_unique<Buffer>(bytes.data(), bytes.size()), sourceForm);
+        std::make_unique<const Buffer>(bytes.data(), bytes.size()), sourceForm);
     if (!res.first) {
       hermes_fatal(res.second.c_str());
     }
