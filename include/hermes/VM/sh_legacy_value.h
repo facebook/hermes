1f21755fa3a4f6add64002b2f29eedf79ab5648c
Tzvetan Mikov <tmikov@fb.com>
2022-09-14 13:30:36 -0700

    Define SHLegacyValue
    Summary:
    SHlegacyValue is a C struct that defines the format of HV64. Move all
    constants and type definitions from HermesValue.h to the new header.
    
    HermesValue publicly inherits from SHLegacyValue, so they have a
    compatible layout. This is intended to avoid UB when pointers are cast
    between them.
    
    For additional certainty, strict aliasing will be disabled in the
    project.
    
    Reviewed By: avp
    
    Differential Revision: D39161676
    
    fbshipit-source-id: 84c551fcf9953ac0fa2bb7397982ca3ed2520365

---
+++ include/hermes/VM/sh_legacy_value.h
@@ -0,0 +1,207 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+//===----------------------------------------------------------------------===//
+/// \file
+/// This header defines the "NaN-boxing" encoding format used to represent
+/// values in Hermes.
+///
+/// NaN-boxing relies on the representation of doubles in IEEE-754:
+///
+/// \pre
+/// s   eeeeeee,eeee mmmm,mmmmmmmm,mmmmmmmm,mmmmmmmm,mmmmmmmm,mmmmmmmm,mmmmmmmm
+/// |   |            |
+/// 63  62           51                                                       0
+///
+/// s: 1-bit sign
+/// e: 11-bit exponent
+/// m: 52-bit mantissa
+/// \endpre
+///
+/// An exponent of all 1-s (0x7ff) and a non-zero mantissa is used to encode
+/// NaN. So, as long as the top 12 bits are 0x7ff or 0xfff and the bottom 52
+/// bits are not 0, we can store any bit pattern in the bottom bits and it will
+/// be interpreted as NaN.
+///
+/// This is what the "canonical quiet NaN" looks like:
+/// \pre
+/// 0   1111111,1111 1000,00000000,00000000,00000000,00000000,00000000,00000000
+/// |   |            |
+/// 63  62           51                                                       0
+/// \endpre
+///
+/// Note that the sign bit can have any value.
+/// We have chosen to set the sign bit as 1 and encode out 3-bit type tag in
+/// bits 50-48. The type tag cannot be zero because it's reserved for the
+/// "canonical quiet NaN". Thus our type tags range between:
+/// \pre
+/// 1   1111111,1111 1111,xxxxxxxx,xxxxxxxx,xxxxxxxx,xxxxxxxx,xxxxxxxx,xxxxxxxx
+///   and
+/// 1   1111111,1111 1001,xxxxxxxx,xxxxxxxx,xxxxxxxx,xxxxxxxx,xxxxxxxx,xxxxxxxx
+/// \endpre
+///
+/// Masking only the top 16 bits, our tag range is [0xffff .. 0xfff9].
+/// Anything lower than 0xfff8 represents a real number, specifically 0xfff8
+/// covers the "canonical quiet NaN".
+///
+/// Extended Tags
+/// =============
+/// This leaves us with 48 data bits and only 7 tags. In some cases we don't
+/// need the full 48 bits, allowing us to extend the width of the tag. We have
+/// chosen to reserve the last 3 tags (0xfffd, 0xfffe, 0xffff) for full width
+/// 48-bit data, and to extend the first 4 tags with one bit to the right.
+/// We call the resulting 4-bit tag an "extended tag".
+///
+/// Heap Pointer Encoding
+/// ================
+/// On 32-bit platforms clearly we have enough bits to encode any pointer in
+/// the low word.
+///
+/// On 64-bit platforms, we could theoretically arrange our own heap to fit
+/// within the available 48-bits. In practice however that is not necessary
+/// (yet) because current 64-bit platforms use at most 48 bits of virtual
+/// address space. x86-64 requires bit 47 to be sign extended into the top 16
+/// bits (leaving effectively 47 address bits), while Linux ARM64 simply
+/// requires the top N bits (depending on configuration, but at least 16) be all
+/// 0 or all 1. In both cases negative values are used for kernel addresses
+/// (top bit 1).
+///
+/// Since in our case we never need to represent a kernel address - all
+/// addresses are within our own heap - we know that the top bit is 0 and we
+/// don't need to store it leaving us with exactly 48-bits.
+///
+/// Should the OS requirements change in the distant future, we can "squeeze" 3
+/// more bits by relying on the 8-byte alignment of all our allocations and
+/// shifting the values to the right. That is still not needed however.
+///
+/// Native Pointer Encoding
+/// ================
+/// We also have limited support for storing a native pointer in a HermesValue.
+/// When doing so, we do not associate a tag with the native pointer and instead
+/// require that the pointer is a valid non-NaN double bit-for-bit. It is the
+/// caller's responsibility to keep track of where these native pointers are
+/// stored.
+///
+/// Native pointers cannot be NaN-boxed because on platforms where the ARM
+/// memory tagging extension is enabled, the top byte may also have bits set
+/// in it. On Android, these tags are added in the 56th to 59th bits of pointers
+/// allocated in the native heap. However, as long as it is only the top byte
+/// and the bottom 48 bits that have non-zero values, we are guaranteed that the
+/// value will not be a NaN.
+///
+/// Fortunately, since the native pointers will appear as doubles to anything
+/// other than the code that created them, anything that scans HermesValues
+/// (e.g. the GC or heap snapshots), will simply ignore them.
+
+#ifndef HERMES_SH_LEGACY_VALUE_H
+#define HERMES_SH_LEGACY_VALUE_H
+
+#include <assert.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct HermesValueBase {
+  union {
+    // 64 raw bits stored and reinterpreted as necessary.
+    uint64_t raw;
+    double f64;
+  };
+};
+
+typedef struct HermesValueBase SHLegacyValue;
+
+typedef intptr_t HVTagType;
+
+/// Tags are defined as 16-bit values positioned at the high bits of a 64-bit
+/// word.
+/// If tag < FirstTag, the encoded value is a double.
+enum HVTag {
+  HVTag_First = (HVTagType)(int8_t)0xf9,
+  HVTag_EmptyInvalid = HVTag_First,
+  HVTag_UndefinedNull,
+  HVTag_BoolSymbol,
+  HVTag_NativeValue,
+  /// Pointer tags start here.
+  HVTag_FirstPointer,
+  HVTag_Str = HVTag_FirstPointer,
+  HVTag_BigInt,
+  HVTag_Object,
+  HVTag_Last = (int8_t)0xff,
+};
+
+static_assert(HVTag_Object <= HVTag_Last, "Tags overflow");
+
+/// An "extended tag", occupying one extra bit.
+enum HVETag {
+  HVETag_Empty = HVTag_EmptyInvalid * 2,
+#ifdef HERMES_SLOW_DEBUG
+  /// An invalid hermes value is one that should never exist in normal
+  /// operation, it can be used as a sigil to indicate a programming failure.
+  HVETag_Invalid = HVTag_EmptyInvalid * 2 + 1,
+#endif
+  HVETag_Undefined = HVTag_UndefinedNull * 2,
+  HVETag_Null = HVTag_UndefinedNull * 2 + 1,
+  HVETag_Bool = HVTag_BoolSymbol * 2,
+  HVETag_Symbol = HVTag_BoolSymbol * 2 + 1,
+  HVETag_Native1 = HVTag_NativeValue * 2,
+  HVETag_Native2 = HVTag_NativeValue * 2 + 1,
+  HVETag_Str1 = HVTag_Str * 2,
+  HVETag_Str2 = HVTag_Str * 2 + 1,
+  HVETag_BigInt1 = HVTag_BigInt * 2,
+  HVETag_BigInt2 = HVTag_BigInt * 2 + 1,
+  HVETag_Object1 = HVTag_Object * 2,
+  HVETag_Object2 = HVTag_Object * 2 + 1,
+
+  HVETag_FirstPointer = HVETag_Str1,
+};
+
+/// Number of bits used in the high part to encode the sign, exponent and tag.
+static const unsigned kHV_NumTagExpBits = 16;
+/// Number of bits available for data storage.
+static const unsigned kHV_NumDataBits = (64 - kHV_NumTagExpBits);
+
+/// Width of a tag in bits. The tag is aligned to the right of the top bits.
+static const unsigned kHV_TagWidth = 3;
+static const unsigned kHV_TagMask = (1 << kHV_TagWidth) - 1;
+/// Mask to extract the data from the whole 64-bit word.
+static const uint64_t kHV_DataMask = (1ull << kHV_NumDataBits) - 1;
+
+static const unsigned kHV_ETagWidth = 4;
+static const unsigned kHV_ETagMask = (1 << kHV_ETagWidth) - 1;
+
+static inline SHLegacyValue _sh_ljs_encode_raw_tag(
+    uint64_t val,
+    enum HVTag tag) {
+  return (SHLegacyValue){val | ((uint64_t)tag << kHV_NumDataBits)};
+}
+static inline SHLegacyValue _sh_ljs_encode_raw_etag(
+    uint64_t val,
+    enum HVETag etag) {
+  return (SHLegacyValue){val | ((uint64_t)etag << (kHV_NumDataBits - 1))};
+}
+
+static inline SHLegacyValue _sh_ljs_double(double v) {
+  union {
+    double d;
+    uint64_t i;
+  } u;
+  u.d = v;
+  return (SHLegacyValue){u.i};
+}
+
+static inline SHLegacyValue _sh_ljs_undefined() {
+  return _sh_ljs_encode_raw_etag(0, HVETag_Undefined);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // HERMES_SH_LEGACY_VALUE_H
