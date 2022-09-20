6670fc81406f0e1a38e190c8bf9e1fd83a93ef44
Tzvetan Mikov <tmikov@fb.com>
2022-09-19 18:47:58 -0700

    add test target check-shermes
    Summary:
    A test target for testing Static Hermes.
    
    For now, before we have full parity, SH tests are placed in the
    test/shermes directory.
    
    Reviewed By: neildhar
    
    Differential Revision: D39625797
    
    fbshipit-source-id: acc5949c58ef35a06404ccedf8e352546c563b36

---
+++ test/shermes/global-var-no-clear.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// RUN: %shermes -exec %s | %FileCheck --match-full-lines %s
+
+var globalEval = eval;
+
+var q = true;
+globalEval('var q;');
+print(q);
+// CHECK: true
+
+var s = 'if (x === undefined) { var x; x = 0; }; x += 1;';
+globalEval(s);
+globalEval(s);
+globalEval(s);
+print(x);
+// CHECK: 3
---
+++ test/shermes/string-table-overflow.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// RUN: %shermes -exec %s | %FileCheck --match-full-lines %s
+
+// This is designed to elicit an overflow string table entry in the bytecode file.
+
+var shortString = "yyyyyyyyyy";
+var longString = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+var both = longString + shortString;
+print(both.length);
+
+// CHECK: 760
