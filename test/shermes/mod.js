20687a84280f8df2e6651d7af81834c642183508
Tzvetan Mikov <tmikov@meta.com>
2022-09-20 13:48:18 -0700

    add more passing tests
    Summary: .
    
    Reviewed By: neildhar
    
    Differential Revision: D39666567
    
    fbshipit-source-id: 24d9d97f639f51d1e6e4b2041481b1760fc2a4b9

---
+++ test/shermes/mod.js
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// RUN: %shermes -exec %s | %FileCheck --match-full-lines %s
+
+print(10 % 2);
+//CHECK: 0
+
+print(10 % 3);
+//CHECK: 1
+
+print(10 % 0);
+//CHECK: NaN
+
+print(10 % Infinity);
+//CHECK: 10
+
+print(10 % NaN);
+//CHECK: NaN
+
+print(0 % 10);
+//CHECK: 0
+
+print(Infinity % 10);
+//CHECK: NaN
+
+print(NaN % 10);
+//CHECK: NaN
+
+print(5.5 % 1.5);
+//CHECK: 1
+
+print(5.5 % -1.5);
+//CHECK: 1
+
+print(-5.5 % 1.5);
+//CHECK: -1
+
+print(-5.5 % -1.5);
+//CHECK: -1
+
+print(5.5 % 2.5);
+//CHECK: 0.5
+
+print(5.5 % -2.5);
+//CHECK: 0.5
+
+print(-5.5 % 2.5);
+//CHECK: -0.5
+
+print(-5.5 % -2.5);
+//CHECK: -0.5
+
