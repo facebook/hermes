dfe7d890a38ca0f602ace921f0410536eb1f8b11
Aakash Patel <avp@fb.com>
2022-10-05 17:15:33 -0700

    Add new SemanticResolver
    Summary:
    Implement a new SemanticResolver which extracts all the resolution
    information into `SemContext`.
    
    This is based directly on Juno's implementation with the same
    structure except for the fact that we use RAII classes instead
    of the callback structure. There's nothing preventing the callback
    structure from being used here, and helper methods for using callbacks
    can be easily added in the future, it's just that RAII seemed fairly
    straightforward and simple for this implementation.
    
    With the exception of the Unresolver pass,
    everything in the Juno implementation is contained in this diff.
    
    The Hermes implementation also does some manipulation of the AST,
    including the try/catch transform, which will be in future diffs.
    
    Differential Revision: D39985175
    
    fbshipit-source-id: 61b4a7f82d98e028e0884563c49a4293beb7664c

---
+++ lib/AST/ScopedFunctionPromoter.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#ifndef HERMES_AST_SCOPEDFUNCTIONPROMOTER_H
+#define HERMES_AST_SCOPEDFUNCTIONPROMOTER_H
+
+#include "hermes/AST/ESTree.h"
+
+namespace hermes {
+
+namespace sem {
+class Keywords;
+}
+
+namespace sema {
+
+class SemanticResolver;
+
+/// This function checks whether it is safe to promote block-scoped function
+/// declarations to function scope. i.e. whether it is safe to replace one with
+/// "var" without creating a conflict.
+///
+/// A conflict exists if a let-like declaration is visible in the declaration
+/// scope. The checker starts with a list of all block scoped function
+/// declarations. Then it visits all scopes recursively, maintaining a scoped
+/// table of let-like declarations with matching names. When it encounters a
+/// block-scoped function declaration, it checks whether a matching let-like
+/// declaration is visible. If not, it is safe to promote.
+///
+/// The input is a list of block-scoped function function declarations. The
+/// ones that can be promoted are deleted from their own scope and added to the
+/// function scope.
+void promoteScopedFuncDecls(
+    SemanticResolver &resolver,
+    ESTree::FunctionLikeNode *funcNode);
+
+} // namespace sema
+} // namespace hermes
+
+#endif
