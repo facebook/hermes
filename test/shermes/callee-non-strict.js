208131447cffad937bc9808f454a1220d8383890
Tzvetan Mikov <tmikov@meta.com>
2022-10-13 21:08:51 -0700

    Add more passing tests
    Summary: .
    
    Reviewed By: neildhar
    
    Differential Revision: D39718824
    
    fbshipit-source-id: 6a3ee46c0a54f1da2e790a95e52ac18c219e53cc

---
+++ test/shermes/callee-non-strict.js
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// RUN: %shermes -exec %s | %FileCheck --match-full-lines %s
+
+print('callee');
+// CHECK-LABEL: callee
+
+function getCallee() {
+  return arguments.callee;
+}
+print(getCallee() === getCallee);
+// CHECK-LABEL: true
+
+function checkCallee() {
+  print(arguments.callee === checkCallee);
+}
+checkCallee();
+// CHECK-LABEL: true
+
+function leak() {
+  return arguments;
+}
+function leak2() {
+  return leak();
+}
+var a = leak();
+print(a.callee === leak);
+// CHECK-NEXT: true
+var a = leak2();
+print(a.callee === leak);
+// CHECK-NEXT: true
