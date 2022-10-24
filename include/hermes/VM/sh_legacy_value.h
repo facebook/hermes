f0a067d45f46994f358067c5dc8b176136cf5508
Aakash Patel <avp@meta.com>
2022-10-24 10:27:51 -0700

    Add simple type-specific operations (#843)
    Summary:
    Pull Request resolved: https://github.com/facebook/hermes/pull/843
    
    Add checks for the types of operands to unary, binary, conditional,
    and branch instructions to speed up execution.
    
    This is a straightforward set of checks in SH instruction selection,
    with some extra code and state needed because we're emitting C and we
    need to make sure the parentheses line up.
    
    This implementation is partial and there are still more specializations
    which can be added without too much extra effort.
    
    Reviewed By: tmikov
    
    Differential Revision: D40281679
    
    fbshipit-source-id: 48dc92b0e2afee3082f4a155d1af0c17d2fc5b8f

--- include/hermes/VM/sh_legacy_value.h
+++ include/hermes/VM/sh_legacy_value.h
@@ -222,8 +222,23 @@ static inline bool _sh_ljs_is_undefined(SHLegacyValue v) {
 static inline bool _sh_ljs_is_null(SHLegacyValue v) {
   return _sh_ljs_get_etag(v) == HVETag_Null;
 }
+static inline bool _sh_ljs_is_bool(SHLegacyValue v) {
+  return _sh_ljs_get_etag(v) == HVETag_Bool;
+}
 static inline bool _sh_ljs_is_object(SHLegacyValue v) {
   return _sh_ljs_get_tag(v) == HVTag_Object;
+}
+static inline bool _sh_ljs_is_double(SHLegacyValue v) {
+  // C enums are int, so to compare we have to use unsigned.
+  return (unsigned int)_sh_ljs_get_tag(v) < (unsigned int)HVTag_First;
+}
+
+static inline bool _sh_ljs_get_bool(SHLegacyValue v) {
+  // Clear the ETag and return the raw values that are left.
+  return (bool)(v.raw & ~_sh_ljs_get_etag(v));
+}
+static inline double _sh_ljs_get_double(SHLegacyValue v) {
+  return v.f64;
 }
 
 #ifdef __cplusplus
---
+++ test/shermes/double-ops.js
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// RUN: %shermes -exec %s | %FileCheck %s --match-full-lines
+
+print('doubles');
+// CHECK-LABEL: doubles
+
+function math(a, b) {
+  a = +a;
+  b = +b;
+
+  print(a + b);
+  print(a - b);
+  print(a * b);
+  print(a / b);
+  print(a % b);
+}
+
+math(1, 2);
+// CHECK-NEXT: 3
+// CHECK-NEXT: -1
+// CHECK-NEXT: 2
+// CHECK-NEXT: 0.5
+// CHECK-NEXT: 1
+
+function compare(a, b) {
+  a = +a;
+  b = +b;
+
+  print(a < b);
+  print(a > b);
+  print(a <= b);
+  print(a >= b);
+  print(a === b);
+  print(a !== b);
+  print(a == b);
+  print(a != b);
+}
+
+compare(1, 2);
+// CHECK-NEXT: true
+// CHECK-NEXT: false
+// CHECK-NEXT: true
+// CHECK-NEXT: false
+// CHECK-NEXT: false
+// CHECK-NEXT: true
+// CHECK-NEXT: false
+// CHECK-NEXT: true
+
+function branch(a, b) {
+  a = +a;
+  b = +b;
+
+  if(a < b)
+    print('less');
+  if(a > b)
+    print('greater');
+  if(a <= b)
+    print('less_equal');
+  if(a >= b)
+    print('greater_equal');
+  if(a === b)
+    print('strict_equal');
+  if(a !== b)
+    print('strict_not_equal');
+  if(a == b)
+    print('equal');
+  if(a != b)
+    print('not_equal');
+}
+
+branch(1, 2);
+// CHECK-NEXT: less
+// CHECK-NEXT: less_equal
+// CHECK-NEXT: strict_not_equal
+// CHECK-NEXT: not_equal
+
+print('done');
+// CHECK-NEXT: done
