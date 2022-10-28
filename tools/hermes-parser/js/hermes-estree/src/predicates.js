5bfdb32a81588ed172bd3ca6efe782fe0a032a5f
Pieter Vanderwerff <pieterv@meta.com>
2022-10-28 15:47:57 -0700

    Add isExpression and isStatement predicate functions
    Summary: Add `isExpression` and `isStatement` predicate functions. These are useful helpers for checking node collection types.
    
    Reviewed By: bradzacher
    
    Differential Revision: D40781896
    
    fbshipit-source-id: 6f257c5bca933bf78213e2c7cdabb45993b78057

--- tools/hermes-parser/js/hermes-estree/src/predicates.js
+++ tools/hermes-parser/js/hermes-estree/src/predicates.js
@@ -13,18 +13,71 @@
 import type {ESNode, Token} from './types';
 
 import {
+  isArrayExpression,
   isArrowFunctionExpression,
+  isAssignmentExpression,
+  isAwaitExpression,
+  isBinaryExpression,
   isBlockComment,
+  isBlockStatement,
+  isBreakStatement,
+  isCallExpression,
+  isChainExpression,
   isClassDeclaration,
   isClassExpression,
+  isConditionalExpression,
+  isContinueStatement,
+  isDebuggerStatement,
+  isDeclareClass,
+  isDeclareFunction,
+  isDeclareInterface,
+  isDeclareModule,
+  isDeclareOpaqueType,
+  isDeclareTypeAlias,
+  isDeclareVariable,
+  isDoWhileStatement,
+  isEmptyStatement,
+  isEnumDeclaration,
+  isExpressionStatement,
+  isForInStatement,
+  isForOfStatement,
+  isForStatement,
   isFunctionDeclaration,
   isFunctionExpression,
+  isIdentifier,
+  isIfStatement,
+  isImportExpression,
+  isInterfaceDeclaration,
+  isJSXElement,
+  isJSXFragment,
+  isLabeledStatement,
   isLineComment,
   isLiteral,
+  isLogicalExpression,
   isMemberExpression,
+  isMetaProperty,
   isMethodDefinition,
-  isPropertyDefinition,
+  isNewExpression,
+  isObjectExpression,
+  isOpaqueType,
   isProperty,
+  isPropertyDefinition,
+  isReturnStatement,
+  isSequenceExpression,
+  isSwitchStatement,
+  isTaggedTemplateExpression,
+  isTemplateLiteral,
+  isThisExpression,
+  isThrowStatement,
+  isTryStatement,
+  isTypeAlias,
+  isTypeCastExpression,
+  isUnaryExpression,
+  isUpdateExpression,
+  isVariableDeclaration,
+  isWhileStatement,
+  isWithStatement,
+  isYieldExpression,
 } from './generated/predicates';
 
 export * from './generated/predicates';
@@ -111,4 +164,74 @@ export function isRegExpLiteral(node: ESNode): boolean %checks {
 
 export function isStringLiteral(node: ESNode): boolean %checks {
   return isLiteral(node) && node.literalType === 'string';
+}
+
+export function isExpression(node: ESNode): boolean %checks {
+  return (
+    isThisExpression(node) ||
+    isArrayExpression(node) ||
+    isObjectExpression(node) ||
+    isFunctionExpression(node) ||
+    isArrowFunctionExpression(node) ||
+    isYieldExpression(node) ||
+    isLiteral(node) ||
+    isUnaryExpression(node) ||
+    isUpdateExpression(node) ||
+    isBinaryExpression(node) ||
+    isAssignmentExpression(node) ||
+    isLogicalExpression(node) ||
+    isMemberExpression(node) ||
+    isConditionalExpression(node) ||
+    isCallExpression(node) ||
+    isNewExpression(node) ||
+    isSequenceExpression(node) ||
+    isTemplateLiteral(node) ||
+    isTaggedTemplateExpression(node) ||
+    isClassExpression(node) ||
+    isMetaProperty(node) ||
+    isIdentifier(node) ||
+    isAwaitExpression(node) ||
+    isImportExpression(node) ||
+    isChainExpression(node) ||
+    isTypeCastExpression(node) ||
+    isJSXFragment(node) ||
+    isJSXElement(node)
+  );
+}
+
+export function isStatement(node: ESNode): boolean %checks {
+  return (
+    isBlockStatement(node) ||
+    isBreakStatement(node) ||
+    isClassDeclaration(node) ||
+    isContinueStatement(node) ||
+    isDebuggerStatement(node) ||
+    isDeclareClass(node) ||
+    isDeclareVariable(node) ||
+    isDeclareFunction(node) ||
+    isDeclareInterface(node) ||
+    isDeclareModule(node) ||
+    isDeclareOpaqueType(node) ||
+    isDeclareTypeAlias(node) ||
+    isDoWhileStatement(node) ||
+    isEmptyStatement(node) ||
+    isEnumDeclaration(node) ||
+    isExpressionStatement(node) ||
+    isForInStatement(node) ||
+    isForOfStatement(node) ||
+    isForStatement(node) ||
+    isFunctionDeclaration(node) ||
+    isIfStatement(node) ||
+    isInterfaceDeclaration(node) ||
+    isLabeledStatement(node) ||
+    isOpaqueType(node) ||
+    isReturnStatement(node) ||
+    isSwitchStatement(node) ||
+    isThrowStatement(node) ||
+    isTryStatement(node) ||
+    isTypeAlias(node) ||
+    isVariableDeclaration(node) ||
+    isWhileStatement(node) ||
+    isWithStatement(node)
+  );
 }
