c1498cae9ac73027c77e4dcd841bade22a1b16a5
Aakash Patel <avp@meta.com>
2022-11-18 10:01:07 -0800

    Fix compilation for Windows ARM64 (#808) (#808)
    Summary:
    Original Author: vmorozov@microsoft.com
    Original Git: 4316463b0cf33c3df5073a917010a24a270fab20
    
    Currently code in `OSCompatWindows.cpp` causes a compilation error for ARM64 platform.
    The issue is that the `__rdtsc()` intrinsic function is defined only for x86 and x64 CPUs and it is not available for ARM64.
    The PR addresses this issue and also fixes incorrect `return` statement in the `#else` branch that is used for ARM64.
    
    Pull Request resolved: https://github.com/facebook/hermes/pull/808
    
    Original Reviewed By: mattbfb
    
    Original Revision: D39412176
    
    Reviewed By: neildhar
    
    Differential Revision: D41240782
    
    fbshipit-source-id: 5372c32af06c5f20311f32504ed28c3c1c57ac4c

--- lib/Support/OSCompatWindows.cpp
+++ lib/Support/OSCompatWindows.cpp
@@ -423,14 +423,14 @@ int sched_getcpu() {
 }
 
 uint64_t cpu_cycle_counter() {
-#if defined(_MSC_VER)
+#if defined(_MSC_VER) && (defined(_M_IX86) || defined(_M_X64))
   return __rdtsc();
 #elif __has_builtin(__builtin_readcyclecounter)
   return __builtin_readcyclecounter();
 #else
   LARGE_INTEGER cnt;
   QueryPerformanceCounter(&cnt);
-  return cnt;
+  return static_cast<uint64_t>(cnt.QuadPart);
 #endif
 }
 
