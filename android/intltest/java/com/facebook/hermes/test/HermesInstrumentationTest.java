7c32726ee1bc8d11a16d831b83514e88235e33a0
Aakash Patel <avp@meta.com>
2022-11-18 10:01:07 -0800

    Use Locale.US
    Summary:
    Original Author: jpporto@fb.com
    Original Git: bb015dc6d6732d6d74f6e7262756f6501a6541af
    
    This change modifies the ```HermesInstrumentationTest.testDateFormat``` to use
    Locale.US as the default locale instead of creating a new Locale("en-US");
    
    Original Reviewed By: fbmal7
    
    Original Revision: D40430827
    
    Reviewed By: tmikov
    
    Differential Revision: D41240803
    
    fbshipit-source-id: 30a5b318e5e709ff6239cade71b2eeb67e3cd2e3

--- android/intltest/java/com/facebook/hermes/test/HermesInstrumentationTest.java
+++ android/intltest/java/com/facebook/hermes/test/HermesInstrumentationTest.java
@@ -51,7 +51,7 @@ public class HermesInstrumentationTest extends InstrumentationTestCase {
   @Test
   public void testDateFormat() {
     Locale defaultLocale = Locale.getDefault();
-    Locale.setDefault(new Locale("en-US"));
+    Locale.setDefault(Locale.US);
 
     TimeZone defaultZone = TimeZone.getDefault();
     TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
