9c0f3b30f961c5cb66a428d7b964a07d666b6bf9
Neil Dhar <neildhar@meta.com>
2022-12-07 16:32:57 -0800

    Generalize snapshot infra
    Summary:
    Move the snapshot infra to a test utils file so new translate passes can use it
    
    Original Author: pieterv@meta.com
    Original Git: 7c68c0cea2fb0937db156b4c1bb81a8e448fd2de
    
    Original Reviewed By: bradzacher
    
    Original Revision: D41325250
    
    Reviewed By: avp
    
    Differential Revision: D41625256
    
    fbshipit-source-id: 9651f4654d0fcf62c7c036e4b0fa026df89b9d92

--- tools/hermes-parser/js/hermes-translate/__tests__/flowDefToTSDef/flowDefToTSDef-test.js
+++ tools/hermes-parser/js/hermes-translate/__tests__/flowDefToTSDef/flowDefToTSDef-test.js
@@ -11,63 +11,16 @@
 // $FlowExpectedError[cannot-resolve-module]
 import prettierConfig from '../../../.prettierrc.json';
 import {translateFlowDefToTSDef} from '../../src';
-import {ExpectedTranslationError} from '../../src/utils/ErrorUtils';
-import fs from 'fs';
-import glob from 'glob';
 import path from 'path';
-import {} from 'hermes-parser';
-
-import 'jest-specific-snapshot';
-
-const FIXTURES_DIR = path.resolve(__dirname, 'fixtures');
-const FIXTURES = glob
-  .sync(`${FIXTURES_DIR}/**/spec.js`)
-  .map(file => {
-    const parsed = path.parse(file);
-    const contents = fs.readFileSync(file, 'utf8');
-    const fixtureName = path.relative(FIXTURES_DIR, parsed.dir);
-    return {
-      contents,
-      name: fixtureName,
-      fixturePath: file,
-      translateSnapshotPath: path.join(parsed.dir, `translate-result.shot`),
-    };
-  })
-  .filter(Boolean);
+import {testFixtures} from '../utils/snapshotTestUtils';
 
 // set this to the path of the test to only run that fixture
 const ONLY = '';
 
 describe('flowDefToTSDef', () => {
-  if (FIXTURES.length === 0) {
-    it.skip('no fixtures found', () => {});
-  }
-
-  for (const fixture of FIXTURES) {
-    (ONLY === fixture.name ? it.only : it)(fixture.name, () => {
-      // ensure we don't accidentally have an empty fixture
-      expect(fixture.contents).not.toBe('');
-
-      try {
-        const printedResult = translateFlowDefToTSDef(
-          fixture.contents,
-          prettierConfig,
-        );
-        expect(printedResult).toMatchSpecificSnapshot(
-          fixture.translateSnapshotPath,
-        );
-
-        // TODO - parse the printed result back with TS and assert there aren't any errors
-      } catch (e) {
-        if (e instanceof ExpectedTranslationError) {
-          // errors of type TranslationError are expected, allowed and should be logged
-          expect(e).toMatchSpecificSnapshot(fixture.translateSnapshotPath);
-        } else {
-          // unexpected errors should crash the test
-          // this ensures we don't accidentally write a snapshot for a test that's unexpectedly crashing
-          throw e;
-        }
-      }
-    });
-  }
+  testFixtures(
+    path.resolve(__dirname, 'fixtures'),
+    contents => translateFlowDefToTSDef(contents, prettierConfig),
+    ONLY,
+  );
 });
