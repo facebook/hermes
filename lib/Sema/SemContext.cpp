cebb027e15298235dfa80290bf732eec8fb32816
Tzvetan Mikov <tmikov@meta.com>
2023-01-23 10:29:09 -0800

    SemContext: extract dumping into a separate class
    Summary:
    Instead of per-class `dump()` methods, move all implementations into a
    separate class `SemContextDumper`. The dumper class supports an optional
    annotator callback for printing additional information associated with
    instances of `sema::Decl` in side tables.
    
    Also, while we are here, remove one unnecessary constructor of
    sema::Decl.
    
    Reviewed By: avp
    
    Differential Revision: D42492197
    
    fbshipit-source-id: 75aee52190cbda98f076345bbe49e54194b81e13

--- lib/Sema/SemContext.cpp
+++ lib/Sema/SemContext.cpp
@@ -10,123 +10,8 @@
 namespace hermes {
 namespace sema {
 
-#ifndef NDEBUG
 static llvh::FormattedString ind(unsigned level) {
   return llvh::left_justify("", level * 4);
-}
-#endif
-
-void Decl::dump(unsigned level) const {
-#ifndef NDEBUG
-  llvh::outs() << ind(level) << "Decl '" << name << "' ";
-  const char *s;
-#define CASE(x) \
-  case Kind::x: \
-    s = #x;     \
-    break;
-  switch (kind) {
-    CASE(Let)
-    CASE(Const)
-    CASE(Class)
-    CASE(Import)
-    CASE(ES5Catch)
-    CASE(FunctionExprName)
-    CASE(ClassExprName)
-    CASE(ScopedFunction)
-    CASE(Var)
-    CASE(Parameter)
-    CASE(GlobalProperty)
-    CASE(UndeclaredGlobalProperty)
-  }
-  llvh::outs() << s;
-#undef CASE
-#define CASE(x)    \
-  case Special::x: \
-    s = #x;        \
-    break;
-  switch (special) {
-    CASE(NotSpecial)
-    CASE(Arguments)
-    CASE(Eval)
-  }
-#undef CASE
-  llvh::outs() << "\n";
-#endif
-}
-
-void LexicalScope::dump(const SemContext *sd, unsigned int level) const {
-#ifndef NDEBUG
-  llvh::outs() << ind(level) << "Scope " << llvh::format("%p", this) << "\n";
-  for (const auto &decl : decls) {
-    decl->dump(level + 1);
-  }
-  for (const auto *fd : hoistedFunctions) {
-    llvh::outs() << ind(level + 1) << "hoistedFunction "
-                 << llvh::cast<ESTree::IdentifierNode>(fd->_id)->_name->str()
-                 << "\n";
-  }
-#endif
-}
-
-void FunctionInfo::dump(const SemContext *sd, unsigned level) const {
-#ifndef NDEBUG
-  llvh::outs() << ind(level) << "Func " << (this->strict ? "strict" : "loose")
-               << "\n";
-  std::map<const LexicalScope *, llvh::SmallVector<const LexicalScope *, 2>>
-      children;
-
-  for (const auto *sc : scopes) {
-    if (sc == scopes[0])
-      continue;
-    children[sc->parentScope].push_back(sc);
-  }
-
-  unsigned processedCount = 0;
-  std::function<void(const LexicalScope *, unsigned)> dumpScope =
-      [&dumpScope, &children, &processedCount, sd](
-          const LexicalScope *sc, unsigned level) {
-        sc->dump(sd, level);
-        ++processedCount;
-        auto it = children.find(sc);
-        if (it == children.end())
-          return;
-        for (auto *childScope : it->second)
-          dumpScope(childScope, level + 1);
-      };
-
-  dumpScope(scopes[0], level + 1);
-  assert(processedCount == scopes.size() && "not all scopes were visited");
-#endif
-}
-
-void SemContext::dump() const {
-#ifndef NDEBUG
-  llvh::outs() << "SemContext\n";
-  std::map<const FunctionInfo *, llvh::SmallVector<const FunctionInfo *, 2>>
-      children;
-
-  for (const auto &F : functions_) {
-    if (&F == &functions_[0])
-      continue;
-    children[F.parentFunction].push_back(&F);
-  }
-
-  unsigned processedCount = 0;
-  std::function<void(const FunctionInfo *, unsigned)> dumpFunction =
-      [&dumpFunction, &children, &processedCount, this](
-          const FunctionInfo *F, unsigned level) {
-        F->dump(this, level);
-        ++processedCount;
-        auto it = children.find(F);
-        if (it == children.end())
-          return;
-        for (auto *childFunc : it->second)
-          dumpFunction(childFunc, level + 1);
-      };
-
-  dumpFunction(&functions_[0], 0);
-  assert(processedCount == functions_.size() && "not all scopes were visited");
-#endif
 }
 
 SemContext::SemContext(Context &ctx) {
@@ -213,6 +98,148 @@ Decl *SemContext::funcArgumentsDecl(
   argumentsFunc->argumentsDecl = decl;
 
   return decl;
+}
+
+void SemContextDumper::printSemContext(
+    llvh::raw_ostream &os,
+    const SemContext &semCtx) {
+  os << "SemContext\n";
+  std::map<const FunctionInfo *, llvh::SmallVector<const FunctionInfo *, 2>>
+      children;
+
+  for (const auto &F : semCtx.functions_) {
+    if (&F == &semCtx.functions_[0])
+      continue;
+    children[F.parentFunction].push_back(&F);
+  }
+
+  unsigned processedCount = 0;
+  std::function<void(const FunctionInfo *, unsigned)> dumpFunction =
+      [&dumpFunction, &children, &processedCount, &os, this](
+          const FunctionInfo *F, unsigned level) {
+        printFunction(os, *F, level);
+        ++processedCount;
+        auto it = children.find(F);
+        if (it == children.end())
+          return;
+        for (auto *childFunc : it->second)
+          dumpFunction(childFunc, level + 1);
+      };
+
+  dumpFunction(&semCtx.functions_[0], 0);
+  assert(
+      processedCount == semCtx.functions_.size() &&
+      "not all scopes were visited");
+}
+
+void SemContextDumper::printFunction(
+    llvh::raw_ostream &os,
+    const FunctionInfo &f,
+    unsigned level) {
+  os << ind(level) << "Func " << (f.strict ? "strict" : "loose") << "\n";
+  std::map<const LexicalScope *, llvh::SmallVector<const LexicalScope *, 2>>
+      children;
+
+  for (const auto *sc : f.scopes) {
+    if (sc == f.scopes[0])
+      continue;
+    children[sc->parentScope].push_back(sc);
+  }
+
+  unsigned processedCount = 0;
+  std::function<void(const LexicalScope *, unsigned)> dumpScope =
+      [&dumpScope, &children, &processedCount, &os, this](
+          const LexicalScope *sc, unsigned level) {
+        printScope(os, sc, level);
+        ++processedCount;
+        auto it = children.find(sc);
+        if (it == children.end())
+          return;
+        for (auto *childScope : it->second)
+          dumpScope(childScope, level + 1);
+      };
+
+  dumpScope(f.scopes[0], level + 1);
+  assert(processedCount == f.scopes.size() && "not all scopes were visited");
+}
+
+void SemContextDumper::printScope(
+    llvh::raw_ostream &os,
+    const LexicalScope *s,
+    unsigned int level) {
+  os << ind(level) << "Scope %s." << scopeNumbers_.getNumber(s) << '\n';
+  for (const auto decl : s->decls) {
+    os << ind(level + 1);
+    printDecl(os, decl);
+    os << '\n';
+  }
+  for (const auto *fd : s->hoistedFunctions) {
+    os << ind(level + 1) << "hoistedFunction "
+       << llvh::cast<ESTree::IdentifierNode>(fd->_id)->_name->str() << "\n";
+  }
+}
+
+void SemContextDumper::printScopeRef(
+    llvh::raw_ostream &os,
+    const LexicalScope *s) {
+  os << "Scope %s." << scopeNumbers_.getNumber(s);
+}
+
+void SemContextDumper::printDecl(llvh::raw_ostream &os, const Decl *d) {
+  os << "Decl %d." << declNumbers_.getNumber(d) << " '" << d->name << "' ";
+  const char *s;
+#define CASE(x)       \
+  case Decl::Kind::x: \
+    s = #x;           \
+    break;
+  switch (d->kind) {
+    CASE(Let)
+    CASE(Const)
+    CASE(Class)
+    CASE(Import)
+    CASE(ES5Catch)
+    CASE(FunctionExprName)
+    CASE(ClassExprName)
+    CASE(ScopedFunction)
+    CASE(Var)
+    CASE(Parameter)
+    CASE(GlobalProperty)
+    CASE(UndeclaredGlobalProperty)
+  }
+  os << s;
+#undef CASE
+  if (d->special != Decl::Special::NotSpecial) {
+#define CASE(x)          \
+  case Decl::Special::x: \
+    s = #x;              \
+    break;
+
+    switch (d->special) {
+      CASE(Arguments)
+      CASE(Eval)
+      case Decl::Special::NotSpecial:
+        break;
+    }
+    os << ' ' << s;
+
+#undef CASE
+  }
+
+  if (annotateDecl_)
+    annotateDecl_(os, d);
+}
+
+void SemContextDumper::printDeclRef(llvh::raw_ostream &os, const Decl *d) {
+  os << "%d." << declNumbers_.getNumber(d);
+  if (d->name.isValid())
+    os << " '" << d->name << '\'';
+}
+
+size_t SemContextDumper::PtrNumberingImpl::getNumberImpl(const void *ptr) {
+  auto [it, inserted] = numbers_.try_emplace(ptr, nextNumber_);
+  if (inserted)
+    ++nextNumber_;
+  return it->second;
 }
 
 } // namespace sema
