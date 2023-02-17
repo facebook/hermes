41a07d7721cdf7791ec7a45af9d96a95077c14be
radex <radexpl@gmail.com>
2023-02-17 13:34:52 +0100

    simdjson: demonstrate basic simdjson parsing

--- lib/VM/JSLib/RuntimeFastJSONUtils.cpp
+++ lib/VM/JSLib/RuntimeFastJSONUtils.cpp
@@ -22,16 +22,51 @@
 namespace hermes {
 namespace vm {
 
+CallResult<HermesValue> parseValue(Runtime &rt) {
+  using namespace simdjson;
+  ondemand::parser parser;
+  simdjson::error_code error;
+
+  // auto json = "\"Hello from JSON\""_padded;
+  auto json = "2137"_padded;
+  ondemand::document doc;
+  error = parser.iterate(json).get(doc);
+  auto &value = doc;
+
+  ondemand::json_type type;
+  error = value.type().get(type);
+  switch (type) {
+  //   case ondemand::json_type::array:
+  //   case ondemand::json_type::object:
+    case ondemand::json_type::number:
+      double doubleValue;
+      error = value.get(doubleValue);
+      return HermesValue::encodeDoubleValue(doubleValue);
+  //   case ondemand::json_type::string:
+  //     std::string_view stringView = value;
+    case ondemand::json_type::boolean:
+      bool boolValue;
+      error = value.get(boolValue);
+      return HermesValue::encodeBoolValue(boolValue);
+    case ondemand::json_type::null:
+      return HermesValue::encodeNullValue();
+    default:
+      return ExecutionStatus::EXCEPTION;
+  }
+}
+
 CallResult<HermesValue> runtimeFastJSONParse(
     Runtime &runtime,
     Handle<StringPrimitive> jsonString,
     Handle<Callable> reviver) {
-  MutableHandle<> returnValue{runtime};
+  // MutableHandle<> returnValue{runtime};
 
-  auto string = StringPrimitive::create(runtime, createASCIIRef("Hello world!"));
-  returnValue = HermesValue::encodeStringValue(string->getString());
+  // auto string = StringPrimitive::create(runtime, createASCIIRef("Hello world!"));
+  // returnValue = HermesValue::encodeStringValue(string->getString());
 
-  return returnValue.getHermesValue();
+  return parseValue(runtime);
+
+  // return returnValue.getHermesValue();
 }
 
 } // namespace vm
