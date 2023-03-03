417190297242f14fed540378390d28926c2a7f7e
Pieter Vanderwerff <pieterv@meta.com>
2023-03-02 17:46:46 -0800

    Support React.AbstractComponent TS conversion
    Summary:
    Support translating `React.AbstractComponent` into the appropriate TS version. In this case i have chosen to convert to `React.ForwardRefExoticComponent`, e.g.
    - `React.AbstractComponent<Config>` -> `React.ForwardRefExoticComponent<Config>`
    - `React.AbstractComponent<Config, Instance>` -> `React.ForwardRefExoticComponent<Config & React.RefAttributes<Instance>>`
    
    I chose `ForwardRefExoticComponent` even in the non `Instance` case as its the most flexible of the TS `ExoticComponent` API's. I'm interested how this works in real code, we may need to change it. TS React API's: https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/react/v17/index.d.ts#L349-L363
    
    Reviewed By: evanyeung
    
    Differential Revision: D43523533
    
    fbshipit-source-id: 79845a2ec883c2fa4eea5c23007e142aa21c1a4a

--- tools/hermes-parser/js/hermes-translate/__tests__/flowDefToTSDef/fixtures/types/react/spec.js
+++ tools/hermes-parser/js/hermes-translate/__tests__/flowDefToTSDef/fixtures/types/react/spec.js
@@ -10,9 +10,11 @@
 
 import * as React from 'react';
 
-type T2 = React.Node; // React.ReactNode
-type T1 = React.MixedElement; // JSX.Element
-type T2 = React.Element<typeof Component>; // React.ReactElement<typeof Component>
+type T1 = React.Node; // React.ReactNode
+type T2 = React.MixedElement; // JSX.Element
+type T3 = React.Element<typeof Component>; // React.ReactElement<typeof Component>
+type T4 = React.AbstractComponent<Props>; // React.ForwardRefExoticComponent<Props>
+type T4 = React.AbstractComponent<Props, HTMLElement>; // React.ForwardRefExoticComponent<Props & React.RefAttributes<HTMLElement>>
 
 type Props = {A: string};
 declare function Component(props: Props): React.Node;
