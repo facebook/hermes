49f8711e251ac9228aa0bac4b9b01565d90451f4
Noah Lemen <noahlemen@meta.com>
2023-03-22 12:25:56 -0700

    handle alternate React type names
    Summary:
    - Adds checks for Flow's global React types (`React$*`)
    - Makes unhandled React types throw
    - Adds handling for some common React types
    
    Reviewed By: pieterv
    
    Differential Revision: D44108579
    
    fbshipit-source-id: 1d5bc3e5b0321a48fd6081a8e08894e4d71e37ec

---
+++ tools/hermes-parser/js/flow-api-translator/__tests__/flowDefToTSDef/fixtures/types/react/global/spec.js
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ */
+
+type T1 = React$Node; // React.ReactNode
+type T2 = React$MixedElement; // JSX.Element
+type T3 = React$Element<typeof Component>; // React.ReactElement<typeof Component>
+type T4 = React$ElementRef<typeof Component>; // React.ElementRef<typeof Component>
+type T5 = React$AbstractComponent<Props, HTMLElement>; // React.ForwardRefExoticComponent<Props & React.RefAttributes<HTMLElement>>
+type T6 = React$Context<Foo>; // React.Context<Foo>
+type T7 = React$ComponentType<Props>; // React.ForwardRefExoticComponent<Props & React.RefAttributes<unknown>>;
+type T8 = React$FragmentType; // React.Fragment
+
+type Props = {A: string};
+declare function Component(props: Props): React$Node;
