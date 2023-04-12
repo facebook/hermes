787d29fb6cf780f341c3d59793867043060a1f1a
Neil Dhar <neildhar@meta.com>
2023-04-11 17:00:16 -0700

    Implement new side effect model
    Summary:
    The current side effect model has several limitations:
    
    1. Reading and writing are not treated as orthogonal operations.
    (writing is considered a superset of reading)
    2. Accesses to the frame, heap, and stack are not disambiguated. This
    is especially important for the stack, since there are meaningful
    restrictions on how it may be accessed.
    3. It does not communicate other information about how an instruction
    may be reordered. For instance, whether it must be placed at the start
    of a basic block.
    
    Reviewed By: avp
    
    Differential Revision: D43572449
    
    fbshipit-source-id: c7dd68a7818a217dc985312c933fe1015b675a77

--- include/hermes/IR/IR.h
+++ include/hermes/IR/IR.h
@@ -355,6 +355,147 @@ enum class SideEffectKind {
   Unknown,
 };
 
+/// Describes the potential side effects of an instruction. The side effects are
+/// described by a series of bits, each of which specifies a particular way in
+/// which this instruction may observe/modify the state of the world, or
+/// otherwise restrict its movement/deletion.
+class SideEffect {
+ public:
+  SideEffect() {
+    memset(this, 0, sizeof(SideEffect));
+  }
+
+  /// Define each of the fields in a SideEffect.
+  /// \c ReadStack and \c WriteStack imply that an instruction may read/write to
+  /// a stack location. Such an instruction must take an \c AllocStackInst
+  /// operand which it can read/write to.
+  /// \c ReadFrame and \c WriteFrame imply that an instruction may read/write
+  /// variables in the frame.
+  /// \c ReadHeap and \c WriteHeap imply that an instruction may read/write
+  /// objects and other values on the heap.
+  /// \c Throw implies that an instruction may throw an exception.
+  /// \c ExecuteJS implies that an instruction may execute arbitrary user
+  /// provided JS. An instruction with \c ExecuteJS set must also have bits set
+  /// for throwing and accessing the frame and heap.
+  /// \c FirstInBlock implies that an instruction must be at the start of a
+  /// basic block. An instruction with \c FirstInBlock set must precede all
+  /// instructions that do not have it set.
+  /// \c Idempotent implies that repeated execution of an instruction will not
+  /// affect the state of the world or the result of the instruction. For
+  /// example, two identical instructions that have \p Idempotent set may be
+  /// merged if they are only separated by instructions that have no side
+  /// effects.
+
+#define SIDE_EFFECT_FIELDS \
+  FIELD(ReadStack)         \
+  FIELD(WriteStack)        \
+  FIELD(ReadFrame)         \
+  FIELD(WriteFrame)        \
+  FIELD(ReadHeap)          \
+  FIELD(WriteHeap)         \
+  FIELD(Throw)             \
+  FIELD(ExecuteJS)         \
+  FIELD(FirstInBlock)      \
+  FIELD(Idempotent)
+
+#define FIELD(field)        \
+  bool get##field() const { \
+    return f##field##_;     \
+  }
+  SIDE_EFFECT_FIELDS
+#undef FIELD
+
+#define FIELD(field)         \
+  SideEffect &set##field() { \
+    f##field##_ = true;      \
+    return *this;            \
+  }
+  SIDE_EFFECT_FIELDS
+#undef FIELD
+
+  /// Create side effects for an instruction that may execute user JS. Executing
+  /// JS implies several other side-effects, so this helps populate them.
+  static SideEffect createExecute() {
+    return SideEffect{}
+        .setReadFrame()
+        .setReadHeap()
+        .setWriteFrame()
+        .setWriteHeap()
+        .setExecuteJS()
+        .setThrow();
+  }
+
+  /// Create side effects for an instruction that may have arbitrary side
+  /// effects, and should not be deleted or reordered. For now this is just an
+  /// alias for createExecute.
+  static SideEffect createUnknown() {
+    return createExecute();
+  }
+
+  /// TODO: Migrate all instructions to explicitly construct their SideEffect,
+  /// and delete this function.
+  static SideEffect fromSideEffectKind(SideEffectKind kind) {
+    switch (kind) {
+      case SideEffectKind::None:
+        return SideEffect{};
+      case SideEffectKind::MayRead:
+        return SideEffect{}.setReadStack().setReadFrame().setReadHeap();
+      case SideEffectKind::MayWrite:
+        return SideEffect{}
+            .setReadStack()
+            .setReadFrame()
+            .setReadHeap()
+            .setWriteStack()
+            .setWriteFrame()
+            .setWriteHeap();
+      case SideEffectKind::Unknown:
+        return SideEffect{}
+            .setReadStack()
+            .setReadFrame()
+            .setReadHeap()
+            .setWriteStack()
+            .setWriteFrame()
+            .setWriteHeap()
+            .setExecuteJS()
+            .setThrow();
+    }
+    llvm_unreachable("No other SideEffectKind.");
+  }
+
+  /// Helper functions to expose the SideEffectKind interface.
+  /// TODO: Delete these once all instructions are migrated off SideEffectKind.
+  bool mayExecute() const {
+    return getThrow() || getExecuteJS();
+  }
+  bool mayWriteOrWorse() const {
+    return getWriteStack() || getWriteFrame() || getWriteHeap() || mayExecute();
+  }
+  bool mayReadOrWorse() const {
+    return getReadStack() || getReadFrame() || getReadHeap() ||
+        mayWriteOrWorse();
+  }
+
+  /// Check if these side effects are well formed. For now, this just checks
+  /// that we correctly set/unset bits when an instruction is marked as being
+  /// able to execute.
+  bool isWellFormed() const {
+    if (getExecuteJS()) {
+      // If an instruction may execute, it cannot be idempotent, and could throw
+      // or access the frame/heap.
+      if (getIdempotent() || !getReadFrame() || !getReadHeap() ||
+          !getWriteFrame() || !getWriteHeap() || !getThrow())
+        return false;
+    }
+    return true;
+  }
+
+ private:
+#define FIELD(field) bool f##field##_ : 1;
+  SIDE_EFFECT_FIELDS
+#undef FIELD
+#undef SIDE_EFFECT_FIELDS
+};
+
 enum class ValueKind : uint8_t {
   First_ValueKind,
 #define INCLUDE_ALL_INSTRS
