98b6c05930cc52a23a2715265d7d4a45b0443acd
Neil Dhar <neildhar@meta.com>
2023-04-13 10:17:22 -0700

    Add more passing tests
    Reviewed By: avp
    
    Differential Revision: D44944980
    
    fbshipit-source-id: d96b022bf33f605a0883024ad29984e302821375

---
+++ test/shermes/optional-catch-binding.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// RUN: %shermes -exec %s | %FileCheck --match-full-lines %s
+
+try {
+  throw Error('hello');
+} catch {
+  print('caught the error');
+}
+// CHECK: caught the error
---
+++ test/shermes/phi_regress1.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// RUN: %shermes -exec %s | %FileCheck %s --match-full-lines
+
+// Using a Phi in a successor of a Phi predecessor block:
+// B0:
+//   ...
+// B1:
+//   %p = PhiInst %a, B0, %b, B2
+//   ...
+// B2:
+//   CondBranchInst ..., B1, B3
+// B3:
+//   Use %p
+
+var glob = null;
+
+function bad(param1, param2) {
+    for (;;) {
+        if (param2)
+            param2.foo = 0;
+        if (!param2) {
+            glob = param1;
+            return null;
+        }
+        param1 = param2;
+    }
+    return null;
+}
+
+bad("foo", null);
+print("phi");
+//CHECK: phi
+print(glob);
+//CHECK-NEXT: foo
