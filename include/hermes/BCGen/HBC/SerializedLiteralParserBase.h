9d71e50c3254b7360619eba9ffa7c21b5acb2e38
Neil Dhar <neildhar@meta.com>
2023-04-13 10:31:22 -0700

    Add a visitor for parsing serialised literals
    Summary:
    Add the ability to visit serialised literals with a visitor passed in
    as a template parameter. This results in a simpler API, since the
    parsing can remain completely separate from VM details.
    
    More importantly, this can also perform better, since the logic for
    each user can be written to deal directly with the serialised value.
    For example, if some code wants to produce `SmallHermesValue`s, it is
    currently forced to first produce a `HermesValue`, and then convert it.
    With the new API, it can directly produce a `SmallHermesValue`.
    
    Reviewed By: avp
    
    Differential Revision: D44262703
    
    fbshipit-source-id: e78d7e710afef689a7ad585c04a8a76256dc237c

--- include/hermes/BCGen/HBC/SerializedLiteralParserBase.h
+++ include/hermes/BCGen/HBC/SerializedLiteralParserBase.h
@@ -8,8 +8,10 @@
 #ifndef HERMES_BCGEN_HBC_SERIALIZEDLITERALPARSERBASE_H
 #define HERMES_BCGEN_HBC_SERIALIZEDLITERALPARSERBASE_H
 
+#include "hermes/BCGen/HBC/SerializedLiteralGenerator.h"
 #include "hermes/Support/Conversions.h"
 #include "llvh/ADT/ArrayRef.h"
+#include "llvh/Support/Endian.h"
 
 namespace hermes {
 namespace hbc {
@@ -58,6 +60,91 @@ class SerializedLiteralParserBase {
  public:
   bool hasNext() const {
     return elemsLeft_ != 0;
+  }
+
+  /// Visit \p totalLen elements serialized in the buffer \p buf with the given
+  /// visitor \p v.
+  /// The visitor should define the following methods, which will be invoked
+  /// when an element of the corresponding type is encountered.
+  ///   void visitStringID(StringID);
+  ///   void visitNumber(double);
+  ///   void visitNull();
+  ///   void visitBool(bool);
+  ///
+  /// TODO: Migrate all places that parse serialized literal to this method, and
+  /// remove the iterator style API from this class.
+  template <typename Visitor>
+  static void parse(CharArray buf, unsigned int totalLen, Visitor &v) {
+    size_t bufIdx = 0;
+
+    while (totalLen) {
+      auto tag = buf[bufIdx++];
+      // If the first bit in the tag is set to 1, it is an indication that the
+      // sequence is longer than 15 elements, and that the tag is two bytes
+      // long.
+      size_t leftInSeq;
+      if (tag & 0x80)
+        leftInSeq = ((tag & 0x0f) << 8) | (buf[bufIdx++]);
+      else
+        leftInSeq = tag & 0x0f;
+
+      SerializedLiteralGenerator::TagType type =
+          tag & SerializedLiteralGenerator::TagMask;
+      switch (type) {
+        case SerializedLiteralGenerator::ByteStringTag:
+          while (leftInSeq-- && totalLen--) {
+            uint8_t val = llvh::support::endian::read<uint8_t, 1>(
+                &buf[bufIdx], llvh::support::endianness::little);
+            v.visitStringID(val);
+            bufIdx += 1;
+          }
+          break;
+        case SerializedLiteralGenerator::ShortStringTag:
+          while (leftInSeq-- && totalLen--) {
+            uint16_t val = llvh::support::endian::read<uint16_t, 1>(
+                &buf[bufIdx], llvh::support::endianness::little);
+            v.visitStringID(val);
+            bufIdx += 2;
+          }
+          break;
+        case SerializedLiteralGenerator::LongStringTag:
+          while (leftInSeq-- && totalLen--) {
+            uint32_t val = llvh::support::endian::read<uint32_t, 1>(
+                &buf[bufIdx], llvh::support::endianness::little);
+            v.visitStringID(val);
+            bufIdx += 4;
+          }
+          break;
+        case SerializedLiteralGenerator::NumberTag:
+          while (leftInSeq-- && totalLen--) {
+            double val = llvh::support::endian::read<double, 1>(
+                &buf[bufIdx], llvh::support::endianness::little);
+            v.visitNumber(val);
+            bufIdx += 8;
+          }
+          break;
+        case SerializedLiteralGenerator::IntegerTag:
+          while (leftInSeq-- && totalLen--) {
+            int32_t val = llvh::support::endian::read<int32_t, 1>(
+                &buf[bufIdx], llvh::support::endianness::little);
+            v.visitNumber(val);
+            bufIdx += 4;
+          }
+          break;
+        case SerializedLiteralGenerator::NullTag:
+          while (leftInSeq-- && totalLen--)
+            v.visitNull();
+          break;
+        case SerializedLiteralGenerator::TrueTag:
+          while (leftInSeq-- && totalLen--)
+            v.visitBool(true);
+          break;
+        case SerializedLiteralGenerator::FalseTag:
+          while (leftInSeq-- && totalLen--)
+            v.visitBool(false);
+          break;
+      }
+    }
   }
 };
 
