f709b6f87d24ebe52ca173dbcf99d1bd445a99f1
Neil Dhar <neildhar@meta.com>
2023-05-26 17:13:57 -0700

    Use a class in FrameLoadStoreOpts
    Summary:
    Use a class to wrap the persistent state for each function in
    `FrameLoadStoreOpts`. This simplifies subsequent diffs which add
    heavier analysis to this pas.
    
    Reviewed By: avp
    
    Differential Revision: D46176642
    
    fbshipit-source-id: 002841ff7b50992767f95c0340b19c676f93351a

--- lib/Optimizer/Scalar/FrameLoadStoreOpts.cpp
+++ lib/Optimizer/Scalar/FrameLoadStoreOpts.cpp
@@ -44,192 +44,206 @@ class CapturedVariables {
   llvh::DenseSet<Variable *> stores;
 };
 
-/// Add the variables owned by \p src that the function \p F is capturing into
-/// \p cv. Notice that the function F may have sub-closures that capture
-/// variables. This method does a recursive scan and collects all captured
-/// variables.
-void collectCapturedVariables(
-    CapturedVariables &cv,
-    Function *F,
-    Function *src) {
-  assert(F != src && "Cannot collect captured variables from src itself.");
-  // For all instructions in the function:
-  for (auto &BB : *F) {
-    for (auto &I : BB) {
-      // Recursively check capturing functions by inspecting the created
-      // closure.
-      if (auto *CF = llvh::dyn_cast<BaseCreateLexicalChildInst>(&I)) {
-        collectCapturedVariables(cv, CF->getFunctionCode(), src);
-        continue;
-      }
+class FunctionLoadStoreOptimizer {
+  /// The function being optimized.
+  Function *const F_;
 
-      if (auto *LF = llvh::dyn_cast<LoadFrameInst>(&I)) {
-        if (LF->getLoadVariable()->getParent()->getFunction() == src)
-          cv.loads.insert(LF->getLoadVariable());
-        continue;
-      }
+  /// Describes whether a variable has been captured anywhere in the program.
+  const CapturedVariables &globalCV_;
 
+  /// Add the variables owned by \p src that the function \p F is capturing into
+  /// \p cv. Notice that the function F may have sub-closures that capture
+  /// variables. This method does a recursive scan and collects all captured
+  /// variables.
+  void collectCapturedVariables(CapturedVariables &cv, Function *inner) {
+    assert(inner != F_ && "Cannot collect captured variables from src itself.");
+    // For all instructions in the function:
+    for (auto &BB : *inner) {
+      for (auto &I : BB) {
+        // Recursively check capturing functions by inspecting the created
+        // closure.
+        if (auto *CF = llvh::dyn_cast<BaseCreateLexicalChildInst>(&I)) {
+          collectCapturedVariables(cv, CF->getFunctionCode());
+          continue;
+        }
+
+        if (auto *LF = llvh::dyn_cast<LoadFrameInst>(&I)) {
+          if (LF->getLoadVariable()->getParent()->getFunction() == F_)
+            cv.loads.insert(LF->getLoadVariable());
+          continue;
+        }
+
+        if (auto *SF = llvh::dyn_cast<StoreFrameInst>(&I)) {
+          if (SF->getVariable()->getParent()->getFunction() == F_)
+            cv.stores.insert(SF->getVariable());
+          continue;
+        }
+      }
+    }
+  }
+
+  /// Attempts to replace loads from the frame in \p BB with values from a
+  /// previous load or store to the same variable. Uses capture information from
+  /// globalCV_ to determine whether intervening operations may store to the
+  /// variable and prevent forwarding.
+  bool eliminateLoads(BasicBlock *BB) {
+    // In the entry block, we can perform more precise analysis by tracking
+    // exactly when variables owned by F_ are actually captured. Create an empty
+    // CapturedVariables that will be incrementally updated as we progress
+    // through the entry block. This is set to None in all subsequent blocks.
+    // TODO: Factor out analysis of owned variables, and analyse them across the
+    // entire function.
+    llvh::Optional<CapturedVariables> entryCV;
+    if (BB == &*F_->begin())
+      entryCV.emplace();
+
+    // Map from a Variable to its current known value that can be forwarded to a
+    // load. If no entry exists, the value is unknown and must be loaded from
+    // the frame.
+    llvh::DenseMap<Variable *, Value *> knownValues;
+    IRBuilder::InstructionDestroyer destroyer;
+
+    bool changed = false;
+
+    for (Instruction &I : *BB) {
       if (auto *SF = llvh::dyn_cast<StoreFrameInst>(&I)) {
-        if (SF->getVariable()->getParent()->getFunction() == src)
-          cv.stores.insert(SF->getVariable());
+        // Record the value stored to the frame:
+        knownValues[SF->getVariable()] = SF->getValue();
         continue;
       }
-    }
-  }
-}
 
-/// Attempts to replace loads from the frame in \p BB with values from a
-/// previous load or store to the same variable. Uses capture information from
-/// \p globalCV to determine whether intervening operations may store to the
-/// variable and prevent forwarding.
-bool eliminateLoads(BasicBlock *BB, const CapturedVariables &globalCV) {
-  Function *F = BB->getParent();
-
-  // In the entry block, we can perform more precise analysis by tracking
-  // exactly when variables owned by F are actually captured. Create an empty
-  // CapturedVariables that will be incrementally updated as we progress through
-  // the entry block. This is set to None in all subsequent blocks.
-  // TODO: Factor out analysis of owned variables, and analyse them across the
-  // entire function.
-  llvh::Optional<CapturedVariables> entryCV;
-  if (BB == &*F->begin())
-    entryCV.emplace();
-
-  // Map from a Variable to its current known value that can be forwarded to a
-  // load. If no entry exists, the value is unknown and must be loaded from the
-  // frame.
-  llvh::DenseMap<Variable *, Value *> knownValues;
-  IRBuilder::InstructionDestroyer destroyer;
-
-  bool changed = false;
-
-  for (Instruction &I : *BB) {
-    if (auto *SF = llvh::dyn_cast<StoreFrameInst>(&I)) {
-      // Record the value stored to the frame:
-      knownValues[SF->getVariable()] = SF->getValue();
-      continue;
-    }
-
-    // Try to replace the LoadFrame with a recently saved value.
-    if (auto *LF = llvh::dyn_cast<LoadFrameInst>(&I)) {
-      // Check if we already have a known value for the load. If we do, use it,
-      // otherwise, populate it.
-      auto [it, first] = knownValues.try_emplace(LF->getLoadVariable(), LF);
-      if (first)
-        continue;
-
-      // Replace all uses of the load with the known value.
-      LF->replaceAllUsesWith(it->second);
-
-      // We have no use of this load now. Remove it.
-      destroyer.add(LF);
-      changed = true;
-      continue;
-    }
-
-    // Collect the captured variables of newly created closures if we're in
-    // the entry block.
-    if (auto *CLCI = llvh::dyn_cast<BaseCreateLexicalChildInst>(&I)) {
-      if (entryCV)
-        collectCapturedVariables(*entryCV, CLCI->getFunctionCode(), F);
-      continue;
-    }
-
-    // Invalidate the variable storage if the instruction may execute capturing
-    // stores that write the variable.
-    if (I.getSideEffect().getExecuteJS()) {
-      for (auto it = knownValues.begin(); it != knownValues.end(); it++) {
-        // Use incremental capture information in the entry block for owned
-        // variables, and global information for everything else.
-        bool ownedVar = it->first->getParent()->getFunction() == F;
-        const auto &cv = entryCV && ownedVar ? *entryCV : globalCV;
-
-        // If there are any captured stores of the variable, the value may be
-        // updated, so preceding stores cannot be propagated.
-        if (cv.stores.count(it->first))
-          knownValues.erase(it);
-      }
-    }
-  }
-
-  return changed;
-}
-
-/// Attempts to remove redundant stores to the frame in \p BB when we can
-/// determine they cannot be observed.
-bool eliminateStores(BasicBlock *BB) {
-  Function *F = BB->getParent();
-
-  // See comment in eliminateLoads above.
-  // Note that for store elimination, being in the entry block is also relevant
-  // because it implies that the block is not in a try, which means that stores
-  // to variables owned by the current function can be eliminated across
-  // instructions that may throw.
-  llvh::Optional<CapturedVariables> entryCV;
-  if (BB == &*F->begin())
-    entryCV.emplace();
-
-  // Map from a Variable to the last store to it that we have not found to be
-  // observable yet. If an entry exists when a subsequent store is
-  // encountered, the entry's store is not observable and may be eliminated.
-  llvh::DenseMap<Variable *, StoreFrameInst *> prevStores;
-
-  // Deletes instructions when we leave the function.
-  IRBuilder::InstructionDestroyer destroyer;
-
-  bool changed = false;
-
-  for (Instruction &I : *BB) {
-    // Try to delete the previous store based on the current store.
-    if (auto *SF = llvh::dyn_cast<StoreFrameInst>(&I)) {
-      auto [it, inserted] = prevStores.try_emplace(SF->getVariable(), SF);
-
-      if (!inserted) {
-        // There is a previous store, delete it and make this the previous
-        // store.
-        destroyer.add(it->second);
-        changed = true;
-        it->second = SF;
-      }
-      continue;
-    }
-
-    // If we are reading from the variable, the last known store cannot be
-    // eliminated.
-    if (auto *LF = llvh::dyn_cast<LoadFrameInst>(&I)) {
-      prevStores.erase(LF->getLoadVariable());
-      continue;
-    }
-
-    // Collect the captured variables of newly created closures if we're in
-    // the entry block.
-    if (auto *CLCI = llvh::dyn_cast<BaseCreateLexicalChildInst>(&I)) {
-      if (entryCV)
-        collectCapturedVariables(*entryCV, CLCI->getFunctionCode(), F);
-      continue;
-    }
-
-    // Invalidate the store frame storage if the instruction may execute
-    // capturing loads that observe this store, or throw an exception that
-    // allows prior stores to be observed.
-    auto sideEffect = I.getSideEffect();
-    if (sideEffect.getExecuteJS() || sideEffect.getThrow()) {
-      for (auto it = prevStores.begin(); it != prevStores.end(); it++) {
-        // If this variable is owned by the current function, and we are in the
-        // entry block, we know that throwing an exception will not make the
-        // store observable. So if the variable does not have any captures that
-        // load it, we know the previous store is not observable yet, and can
-        // leave it in prevStores.
-        bool ownedVar = it->first->getParent()->getFunction() == F;
-        if (entryCV && ownedVar && !entryCV->loads.count(it->first))
+      // Try to replace the LoadFrame with a recently saved value.
+      if (auto *LF = llvh::dyn_cast<LoadFrameInst>(&I)) {
+        // Check if we already have a known value for the load. If we do, use
+        // it, otherwise, populate it.
+        auto [it, first] = knownValues.try_emplace(LF->getLoadVariable(), LF);
+        if (first)
           continue;
 
-        prevStores.erase(it);
+        // Replace all uses of the load with the known value.
+        LF->replaceAllUsesWith(it->second);
+
+        // We have no use of this load now. Remove it.
+        destroyer.add(LF);
+        changed = true;
+        continue;
+      }
+
+      // Collect the captured variables of newly created closures if we're in
+      // the entry block.
+      if (auto *CLCI = llvh::dyn_cast<BaseCreateLexicalChildInst>(&I)) {
+        if (entryCV)
+          collectCapturedVariables(*entryCV, CLCI->getFunctionCode());
+        continue;
+      }
+
+      // Invalidate the variable storage if the instruction may execute
+      // capturing stores that write the variable.
+      if (I.getSideEffect().getExecuteJS()) {
+        for (auto it = knownValues.begin(); it != knownValues.end(); it++) {
+          // Use incremental capture information in the entry block for owned
+          // variables, and global information for everything else.
+          bool ownedVar = it->first->getParent()->getFunction() == F_;
+          const auto &cv = entryCV && ownedVar ? *entryCV : globalCV_;
+
+          // If there are any captured stores of the variable, the value may be
+          // updated, so preceding stores cannot be propagated.
+          if (cv.stores.count(it->first))
+            knownValues.erase(it);
+        }
       }
     }
+
+    return changed;
   }
-  return changed;
-}
+
+  /// Attempts to remove redundant stores to the frame in \p BB when we can
+  /// determine they cannot be observed.
+  bool eliminateStores(BasicBlock *BB) {
+    // See comment in eliminateLoads above.
+    // Note that for store elimination, being in the entry block is also
+    // relevant because it implies that the block is not in a try, which means
+    // that stores to variables owned by the current function can be eliminated
+    // across instructions that may throw.
+    llvh::Optional<CapturedVariables> entryCV;
+    if (BB == &*F_->begin())
+      entryCV.emplace();
+
+    // Map from a Variable to the last store to it that we have not found to be
+    // observable yet. If an entry exists when a subsequent store is
+    // encountered, the entry's store is not observable and may be eliminated.
+    llvh::DenseMap<Variable *, StoreFrameInst *> prevStores;
+
+    // Deletes instructions when we leave the function.
+    IRBuilder::InstructionDestroyer destroyer;
+
+    bool changed = false;
+
+    for (Instruction &I : *BB) {
+      // Try to delete the previous store based on the current store.
+      if (auto *SF = llvh::dyn_cast<StoreFrameInst>(&I)) {
+        auto [it, inserted] = prevStores.try_emplace(SF->getVariable(), SF);
+
+        if (!inserted) {
+          // There is a previous store, delete it and make this the previous
+          // store.
+          destroyer.add(it->second);
+          changed = true;
+          it->second = SF;
+        }
+        continue;
+      }
+
+      // If we are reading from the variable, the last known store cannot be
+      // eliminated.
+      if (auto *LF = llvh::dyn_cast<LoadFrameInst>(&I)) {
+        prevStores.erase(LF->getLoadVariable());
+        continue;
+      }
+
+      // Collect the captured variables of newly created closures if we're in
+      // the entry block.
+      if (auto *CLCI = llvh::dyn_cast<BaseCreateLexicalChildInst>(&I)) {
+        if (entryCV)
+          collectCapturedVariables(*entryCV, CLCI->getFunctionCode());
+        continue;
+      }
+
+      // Invalidate the store frame storage if the instruction may execute
+      // capturing loads that observe this store, or throw an exception that
+      // allows prior stores to be observed.
+      auto sideEffect = I.getSideEffect();
+      if (sideEffect.getExecuteJS() || sideEffect.getThrow()) {
+        for (auto it = prevStores.begin(); it != prevStores.end(); it++) {
+          // If this variable is owned by the current function, and we are in
+          // the entry block, we know that throwing an exception will not make
+          // the store observable. So if the variable does not have any captures
+          // that load it, we know the previous store is not observable yet, and
+          // can leave it in prevStores.
+          bool ownedVar = it->first->getParent()->getFunction() == F_;
+          if (entryCV && ownedVar && !entryCV->loads.count(it->first))
+            continue;
+
+          prevStores.erase(it);
+        }
+      }
+    }
+    return changed;
+  }
+
+ public:
+  FunctionLoadStoreOptimizer(Function *F, const CapturedVariables &globalCV)
+      : globalCV_(globalCV), F_(F) {}
+
+  bool run() {
+    bool changed = false;
+    for (auto &BB : *F_) {
+      changed |= eliminateLoads(&BB);
+      changed |= eliminateStores(&BB);
+    }
+    return changed;
+  }
+};
 
 bool runFrameLoadStoreOpts(Module *M) {
   CapturedVariables cv;
@@ -251,12 +265,8 @@ bool runFrameLoadStoreOpts(Module *M) {
   }
 
   bool changed = false;
-  for (auto &F : *M) {
-    for (auto &BB : F) {
-      changed |= eliminateLoads(&BB, cv);
-      changed |= eliminateStores(&BB);
-    }
-  }
+  for (auto &F : *M)
+    changed |= FunctionLoadStoreOptimizer(&F, cv).run();
   return changed;
 }
 
