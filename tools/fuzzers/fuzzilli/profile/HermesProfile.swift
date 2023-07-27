0c46db8a6772e352d042690aeba20910110d1947
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2023-07-27 08:20:35 -0700

    xplat
    Reviewed By: ebgraham
    
    Differential Revision: D47827663
    
    fbshipit-source-id: e987aa1071daaff2f6f6d233c50d55add738c329

--- tools/fuzzers/fuzzilli/profile/HermesProfile.swift
+++ tools/fuzzers/fuzzilli/profile/HermesProfile.swift
@@ -8,60 +8,60 @@
 import Fuzzilli
 
 let hermesProfile = Profile(
-    getProcessArguments:  { (randomizingArguments: Bool) -> [String] in
-      var args = ["--reprl"]
+  getProcessArguments: { (randomizingArguments: Bool) -> [String] in
+    var args = ["--reprl"]
 
-      guard randomizingArguments else { return args }
+    guard randomizingArguments else { return args }
 
-      if probability(0.5) { args.append("--compile") }
-      if probability(0.5) { args.append("--lazy-compilation") }
-      if probability(0.5) { args.append("--optimize") }
-      if probability(0.5) { args.append("--async-break") }
-      if probability(0.5) { args.append("--block-scoping") }
-      if probability(0.5) { args.append("--random-mem-layout") }
+    if probability(0.5) { args.append("--compile") }
+    if probability(0.5) { args.append("--lazy-compilation") }
+    if probability(0.5) { args.append("--optimize") }
+    if probability(0.5) { args.append("--async-break") }
+    if probability(0.5) { args.append("--block-scoping") }
+    if probability(0.5) { args.append("--random-mem-layout") }
 
-      return args
-    },
+    return args
+  },
 
-    processEnv: ["UBSAN_OPTIONS": "handle_segv=0"],
+  processEnv: ["UBSAN_OPTIONS": "handle_segv=0"],
 
-    maxExecsBeforeRespawn: 1000,
+  maxExecsBeforeRespawn: 1000,
 
-    timeout: 2000,
+  timeout: 2000,
 
-    codePrefix: """
+  codePrefix: """
                 function main(){
                 """,
 
-    codeSuffix: """
+  codeSuffix: """
                 }; main();
                 """,
 
-    ecmaVersion: ECMAScriptVersion.es6,
+  ecmaVersion: ECMAScriptVersion.es6,
 
-    crashTests: ["fuzzilli('FUZZILLI_CRASH', 0)", "fuzzilli('FUZZILLI_CRASH', 1)", "fuzzilli('FUZZILLI_CRASH', 2)"],
+  crashTests: ["fuzzilli('FUZZILLI_CRASH', 0)", "fuzzilli('FUZZILLI_CRASH', 1)", "fuzzilli('FUZZILLI_CRASH', 2)"],
 
-    additionalCodeGenerators: [],
+  additionalCodeGenerators: [],
 
-    additionalProgramTemplates: WeightedList<ProgramTemplate>([]),
+  additionalProgramTemplates: WeightedList<ProgramTemplate>([]),
 
-    disabledCodeGenerators: ["AsyncArrowFunctionGenerator", "AsyncGeneratorFunctionGenerator", "ClassGenerator", "WithStatementGenerator"],
+  disabledCodeGenerators: ["AsyncArrowFunctionGenerator", "AsyncGeneratorFunctionGenerator", "ClassGenerator", "WithStatementGenerator"],
 
-    additionalBuiltins: [
-        "gc"                                             : .function([] => .undefined),
-        "print"                                          : .function([.plain(.anything)] => .undefined),
-        "HermesInternal.enqueueJob"                      : .function([.plain(.function())] => .undefined),
-        "HermesInternal.setPromiseRejectionTrackingHook" : .function([.plain(.function())] => .undefined),
-        "HermesInternal.enablePromiseRejectionTracker"   : .function([.plain(.anything)] => .anything),
-        "HermesInternal.getEpilogues"                    : .function([] => .iterable),
-        "HermesInternal.getInstrumentedStats"            : .function([] => .object(ofGroup: "Object", withProperties: ["js_VMExperiments", "js_numGCs", "js_gcCPUTime", "js_gcTime", "js_totalAllocatedBytes", "js_allocatedBytes", "js_heapSize", "js_mallocSizeEstimate", "js_vaSize", "js_markStackOverflows"])),
-        "HermesInternal.getRuntimeProperties"            : .function([] => .object(ofGroup: "Object", withProperties: ["Snapshot VM", "Bytecode Version", "Builtins Frozen", "VM Experiments", "Build", "GC", "OSS Release Version", "CommonJS Modules"])),
-        "HermesInternal.ttiReached"                      : .function([] => .undefined),
-        "HermesInternal.getFunctionLocation"             : .function([.plain(.function())] => .object(ofGroup: "Object", withProperties: ["isNative", "lineNumber", "columnNumber", "fileName"])),
+  additionalBuiltins: [
+    "gc": .function([] => .undefined),
+    "print": .function([.plain(.anything)] => .undefined),
+    "HermesInternal.enqueueJob": .function([.plain(.function())] => .undefined),
+    "HermesInternal.setPromiseRejectionTrackingHook": .function([.plain(.function())] => .undefined),
+    "HermesInternal.enablePromiseRejectionTracker": .function([.plain(.anything)] => .anything),
+    "HermesInternal.getEpilogues": .function([] => .iterable),
+    "HermesInternal.getInstrumentedStats": .function([] => .object(ofGroup: "Object", withProperties: ["js_VMExperiments", "js_numGCs", "js_gcCPUTime", "js_gcTime", "js_totalAllocatedBytes", "js_allocatedBytes", "js_heapSize", "js_mallocSizeEstimate", "js_vaSize", "js_markStackOverflows"])),
+    "HermesInternal.getRuntimeProperties": .function([] => .object(ofGroup: "Object", withProperties: ["Snapshot VM", "Bytecode Version", "Builtins Frozen", "VM Experiments", "Build", "GC", "OSS Release Version", "CommonJS Modules"])),
+    "HermesInternal.ttiReached": .function([] => .undefined),
+    "HermesInternal.getFunctionLocation": .function([.plain(.function())] => .object(ofGroup: "Object", withProperties: ["isNative", "lineNumber", "columnNumber", "fileName"])),
 
-        // The methods below are disabled since they are not very interesting to fuzz
-        // "HermesInternal.hasPromise"                   : .function([] => .boolean),
-        // "HermesInternal.useEngineQueue"               : .function([] => .boolean),
-        // "HermesInternal.ttrcReached"                  : .function([] => .undefined),
-    ]
+    // The methods below are disabled since they are not very interesting to fuzz
+    // "HermesInternal.hasPromise"                   : .function([] => .boolean),
+    // "HermesInternal.useEngineQueue"               : .function([] => .boolean),
+    // "HermesInternal.ttrcReached"                  : .function([] => .undefined),
+  ]
 )
