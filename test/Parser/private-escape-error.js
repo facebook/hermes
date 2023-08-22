9e5003bd99d6696281608ba0e47a2e46c6cf870b
Aakash Patel <avp@meta.com>
2023-08-22 14:00:49 -0700

    Set token start earlier for private identifiers
    Summary:
    Error printing during the private identifier scan could result in
    reading uninitialized locations because the start wasn't set.
    
    Reviewed By: tmikov
    
    Differential Revision: D48133376
    
    fbshipit-source-id: 38b301d070540e128ee3cc1ad45e73ffd4fb69dc

---
+++ test/Parser/private-escape-error.js
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// RUN: (! %hermes -dump-ast --pretty-json %s 2>&1) | %FileCheckOrRegen %s --match-full-lines
+
+// Printing the warning here was failing due to incorrect location setting.
+#do\
+
+// Auto-generated content below. Please do not modify manually.
+
+// CHECK:{{.*}}private-escape-error.js:11:4: error: invalid Unicode escape
+// CHECK-NEXT:#do\
+// CHECK-NEXT:   ^
+// CHECK-NEXT:{{.*}}private-escape-error.js:11:4: error: Unicode escape \ufffd is not a valid identifier codepoint
+// CHECK-NEXT:#do\
+// CHECK-NEXT:   ^
+// CHECK-NEXT:{{.*}}private-escape-error.js:11:1: warning: scanning identifier with unicode escape as reserved word
+// CHECK-NEXT:#do\
+// CHECK-NEXT:^~~~
+// CHECK-NEXT:{{.*}}private-escape-error.js:11:1: error: Private name can only be used as left-hand side of `in` expression
+// CHECK-NEXT:#do\
+// CHECK-NEXT:^~~~
