17b99a6684a4f1f6d41a52068fb31d6df03451a0
Tzvetan Mikov <tmikov@meta.com>
2023-09-01 09:14:15 -0700

    ADT: OwningFoldingSet
    Summary:
    llvh::FoldingSet annoyingly doesn't own its nodes, leading to clumsy
    and potentially leaky solutions everywhere in the code. Perhaps using it
    in this way would make more sense if we transitioned to pool allocations
    for IR, but for now it is very inconvenient.
    
    Define a simpler "owning" wrapper class, exposing only the functionality
    we need, as well as a new convenient method for adding or creating new
    entries, covering 98% of our actual usage.
    
    Follow up commit cleans up and simplifies IR.h/.cpp.
    
    Reviewed By: avp
    
    Differential Revision: D48720348
    
    fbshipit-source-id: 4426ba111a8d6b65fdf75d7c8f6141c2a30e47c0

---
+++ include/hermes/ADT/OwningFoldingSet.h
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#ifndef HERMES_ADT_OWNINGFOLDINGSET_H
+#define HERMES_ADT_OWNINGFOLDINGSET_H
+
+#include "llvh/ADT/FoldingSet.h"
+
+namespace hermes {
+
+/// A FoldingSet that owns its entries and deletes them when it is destroyed.
+template <typename T, class Deleter = std::default_delete<T>>
+class OwningFoldingSet : Deleter {
+  llvh::FoldingSet<T> set_;
+
+ public:
+  explicit OwningFoldingSet(unsigned Log2InitSize = 6) : set_(Log2InitSize) {}
+  OwningFoldingSet(OwningFoldingSet &&Arg) = default;
+  OwningFoldingSet &operator=(OwningFoldingSet &&RHS) = default;
+
+  ~OwningFoldingSet() {
+    // The folding set is intrusive, so elements cannot be destroyed while they
+    // are still inserted in the set. At the same time, removal of individual
+    // elements from the set is rather slow as it involves iterating over
+    // the bucket to find the previous node. So, instead we just save all nodes,
+    // clear the set, and then delete them.
+    llvh::SmallVector<T *, 8> toDelete{};
+    toDelete.reserve(set_.size());
+    for (T &entry : set_)
+      toDelete.push_back(&entry);
+    set_.clear();
+    for (T *entry : toDelete)
+      Deleter::operator()(entry);
+  }
+
+  /// FindNodeOrInsertPos - Look up the node specified by ID.  If it exists,
+  /// return it.  If not, return the insertion token that will make insertion
+  /// faster.
+  T *FindNodeOrInsertPos(const llvh::FoldingSetNodeID &ID, void *&InsertPos) {
+    return set_.FindNodeOrInsertPos(ID, InsertPos);
+  }
+
+  /// InsertNode - Insert the specified node into the folding set, knowing that
+  /// it is not already in the folding set.  InsertPos must be obtained from
+  /// FindNodeOrInsertPos.
+  T *InsertNode(std::unique_ptr<T, Deleter> N, void *InsertPos) {
+    set_.InsertNode(N.get(), InsertPos);
+    return N.release();
+  }
+
+  /// A helper method wrapping FindNodeOrInsertPos() and InsertNode()
+  /// for types where the constructor parameters can be passed to a static
+  /// method \c T::Profile() in the same order.
+  /// The new node is created using
+  /// <code>new T(std::forward<Args>(args)...)</code>.
+  ///
+  /// \return a pair of the pointer to the node and a boolean indicating whether
+  ///     a new node was inserted.
+  template <typename... Args>
+  std::pair<T *, bool> getOrEmplaceWithNew(Args &&...args) {
+    llvh::FoldingSetNodeID ID;
+    T::Profile(ID, std::forward<Args>(args)...);
+    void *insertPos = nullptr;
+    if (auto *v = FindNodeOrInsertPos(ID, insertPos))
+      return {v, false};
+    return {
+        InsertNode(
+            std::unique_ptr<T, Deleter>(new T(std::forward<Args>(args)...)),
+            insertPos),
+        true};
+  }
+};
+
+} // namespace hermes
+
+#endif
---
+++ unittests/ADT/OwningFoldingSet.cpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "gtest/gtest.h"
+
+#include "hermes/ADT/OwningFoldingSet.h"
+
+namespace {
+
+class TestNode : public llvh::FoldingSetNode {
+ public:
+  int data_;
+
+  explicit TestNode(int data) : data_(data) {}
+
+  static void Profile(llvh::FoldingSetNodeID &ID, int data) {
+    ID.AddInteger(data);
+  }
+  void Profile(llvh::FoldingSetNodeID &ID) const {
+    Profile(ID, data_);
+  }
+};
+
+TEST(OwningFoldingSet, SmokeTest) {
+  hermes::OwningFoldingSet<TestNode> set{};
+  auto [node, ins] = set.getOrEmplaceWithNew(42);
+  EXPECT_TRUE(ins);
+  EXPECT_EQ(42, node->data_);
+  auto res = set.getOrEmplaceWithNew(42);
+  EXPECT_FALSE(res.second);
+  EXPECT_EQ(node, res.first);
+  res = set.getOrEmplaceWithNew(43);
+  EXPECT_TRUE(res.second);
+  EXPECT_NE(node, res.first);
+}
+
+} // anonymous namespace
