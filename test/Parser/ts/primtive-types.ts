600b7aefef42b59cfbdc80cd22d4ae1049e9ffea
Seonggun Kim <seonggun@amazon.com>
2023-09-11 10:52:25 -0700

    Parse TypeScript primitive and literal types. (#1115)
    Summary:
    TypeScript built-in type primitives: `boolean`, `string`, `number`, `undefined`, `any`, `unknown`, `never`, `void`, `bigint`, `symbol`.
    
    TypeScript literal types: `null`, `boolean`, `string`, `number`, `bigint` literals.
    
    Pull Request resolved: https://github.com/facebook/hermes/pull/1115
    
    Test Plan: Added regression tests.
    
    Reviewed By: tmikov
    
    Differential Revision: D49053990
    
    Pulled By: avp
    
    fbshipit-source-id: e1276f27094bc09e681537c78675dc1c565aa386

---
+++ test/Parser/ts/primtive-types.ts
@@ -0,0 +1,145 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// RUN: %hermesc -parse-ts -dump-ast -pretty-json %s | %FileCheck %s --match-full-lines
+
+// CHECK-LABEL: {
+// CHECK-NEXT:   "type": "Program",
+// CHECK-NEXT:   "body": [
+
+type A = boolean;
+// CHECK-NEXT:     {
+// CHECK-NEXT:       "type": "TSTypeAliasDeclaration",
+// CHECK-NEXT:       "id": {
+// CHECK-NEXT:         "type": "Identifier",
+// CHECK-NEXT:         "name": "A"
+// CHECK-NEXT:       },
+// CHECK-NEXT:       "typeParameters": null,
+// CHECK-NEXT:       "typeAnnotation": {
+// CHECK-NEXT:         "type": "TSBooleanKeyword"
+// CHECK-NEXT:       }
+// CHECK-NEXT:     },
+
+type A = string;
+// CHECK-NEXT:     {
+// CHECK-NEXT:       "type": "TSTypeAliasDeclaration",
+// CHECK-NEXT:       "id": {
+// CHECK-NEXT:         "type": "Identifier",
+// CHECK-NEXT:         "name": "A"
+// CHECK-NEXT:       },
+// CHECK-NEXT:       "typeParameters": null,
+// CHECK-NEXT:       "typeAnnotation": {
+// CHECK-NEXT:         "type": "TSStringKeyword"
+// CHECK-NEXT:       }
+// CHECK-NEXT:     },
+
+type A = number;
+// CHECK-NEXT:     {
+// CHECK-NEXT:       "type": "TSTypeAliasDeclaration",
+// CHECK-NEXT:       "id": {
+// CHECK-NEXT:         "type": "Identifier",
+// CHECK-NEXT:         "name": "A"
+// CHECK-NEXT:       },
+// CHECK-NEXT:       "typeParameters": null,
+// CHECK-NEXT:       "typeAnnotation": {
+// CHECK-NEXT:         "type": "TSNumberKeyword"
+// CHECK-NEXT:       }
+// CHECK-NEXT:     },
+
+type A = undefined;
+// CHECK-NEXT:     {
+// CHECK-NEXT:       "type": "TSTypeAliasDeclaration",
+// CHECK-NEXT:       "id": {
+// CHECK-NEXT:         "type": "Identifier",
+// CHECK-NEXT:         "name": "A"
+// CHECK-NEXT:       },
+// CHECK-NEXT:       "typeParameters": null,
+// CHECK-NEXT:       "typeAnnotation": {
+// CHECK-NEXT:         "type": "TSUndefinedKeyword"
+// CHECK-NEXT:       }
+// CHECK-NEXT:     },
+
+type A = any;
+// CHECK-NEXT:     {
+// CHECK-NEXT:       "type": "TSTypeAliasDeclaration",
+// CHECK-NEXT:       "id": {
+// CHECK-NEXT:         "type": "Identifier",
+// CHECK-NEXT:         "name": "A"
+// CHECK-NEXT:       },
+// CHECK-NEXT:       "typeParameters": null,
+// CHECK-NEXT:       "typeAnnotation": {
+// CHECK-NEXT:         "type": "TSAnyKeyword"
+// CHECK-NEXT:       }
+// CHECK-NEXT:     },
+
+type A = unknown;
+// CHECK-NEXT:     {
+// CHECK-NEXT:       "type": "TSTypeAliasDeclaration",
+// CHECK-NEXT:       "id": {
+// CHECK-NEXT:         "type": "Identifier",
+// CHECK-NEXT:         "name": "A"
+// CHECK-NEXT:       },
+// CHECK-NEXT:       "typeParameters": null,
+// CHECK-NEXT:       "typeAnnotation": {
+// CHECK-NEXT:         "type": "TSUnknownKeyword"
+// CHECK-NEXT:       }
+// CHECK-NEXT:     },
+
+type A = never;
+// CHECK-NEXT:     {
+// CHECK-NEXT:       "type": "TSTypeAliasDeclaration",
+// CHECK-NEXT:       "id": {
+// CHECK-NEXT:         "type": "Identifier",
+// CHECK-NEXT:         "name": "A"
+// CHECK-NEXT:       },
+// CHECK-NEXT:       "typeParameters": null,
+// CHECK-NEXT:       "typeAnnotation": {
+// CHECK-NEXT:         "type": "TSNeverKeyword"
+// CHECK-NEXT:       }
+// CHECK-NEXT:     },
+
+type A = void;
+// CHECK-NEXT:     {
+// CHECK-NEXT:       "type": "TSTypeAliasDeclaration",
+// CHECK-NEXT:       "id": {
+// CHECK-NEXT:         "type": "Identifier",
+// CHECK-NEXT:         "name": "A"
+// CHECK-NEXT:       },
+// CHECK-NEXT:       "typeParameters": null,
+// CHECK-NEXT:       "typeAnnotation": {
+// CHECK-NEXT:         "type": "TSVoidKeyword"
+// CHECK-NEXT:       }
+// CHECK-NEXT:     },
+
+type A = bigint;
+// CHECK-NEXT:     {
+// CHECK-NEXT:       "type": "TSTypeAliasDeclaration",
+// CHECK-NEXT:       "id": {
+// CHECK-NEXT:         "type": "Identifier",
+// CHECK-NEXT:         "name": "A"
+// CHECK-NEXT:       },
+// CHECK-NEXT:       "typeParameters": null,
+// CHECK-NEXT:       "typeAnnotation": {
+// CHECK-NEXT:         "type": "TSBigIntKeyword"
+// CHECK-NEXT:       }
+// CHECK-NEXT:     },
+
+type A = symbol;
+// CHECK-NEXT:     {
+// CHECK-NEXT:       "type": "TSTypeAliasDeclaration",
+// CHECK-NEXT:       "id": {
+// CHECK-NEXT:         "type": "Identifier",
+// CHECK-NEXT:         "name": "A"
+// CHECK-NEXT:       },
+// CHECK-NEXT:       "typeParameters": null,
+// CHECK-NEXT:       "typeAnnotation": {
+// CHECK-NEXT:         "type": "TSSymbolKeyword"
+// CHECK-NEXT:       }
+// CHECK-NEXT:     }
+
+// CHECK-NEXT:   ]
+// CHECK-NEXT: }
