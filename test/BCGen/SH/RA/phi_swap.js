23eaca5ceb5ad0fcde7abb1981dc47944fae8faf
Tzvetan Mikov <tmikov@meta.com>
2023-09-13 15:44:14 -0700

    Print types of locals in `--dump-ra`
    Summary: `--dump-ra` wasn't printing the types of the locals.
    
    Reviewed By: avp
    
    Differential Revision: D49234569
    
    fbshipit-source-id: b1c5ae0c1febf1fd1b171152d82acc0fe966fb75

--- test/BCGen/SH/RA/phi_swap.js
+++ test/BCGen/SH/RA/phi_swap.js
@@ -24,11 +24,11 @@ function foo (a, b) {
 // CHECK-NEXT:%BB0:
 // CHECK-NEXT:  $loc0      = DeclareGlobalVarInst "foo": string
 // CHECK-NEXT:  $loc0      = HBCCreateEnvironmentInst (:environment)
-// CHECK-NEXT:  $loc1      = HBCCreateFunctionInst (:object) %foo(): any, $loc0
+// CHECK-NEXT:  $loc1      = HBCCreateFunctionInst (:object) %foo(): any, $loc0: environment
 // CHECK-NEXT:  $loc0      = HBCGetGlobalObjectInst (:object)
-// CHECK-NEXT:  $loc0      = StorePropertyLooseInst $loc1, $loc0, "foo": string
+// CHECK-NEXT:  $loc0      = StorePropertyLooseInst $loc1: object, $loc0: object, "foo": string
 // CHECK-NEXT:  $np0       = HBCLoadConstInst (:undefined) undefined: undefined
-// CHECK-NEXT:  $loc0      = ReturnInst $np0
+// CHECK-NEXT:  $loc0      = ReturnInst $np0: undefined
 // CHECK-NEXT:function_end
 
 // CHECK:function foo(a: any, b: any): any
@@ -36,15 +36,15 @@ function foo (a, b) {
 // CHECK-NEXT:%BB0:
 // CHECK-NEXT:  $loc1      = LoadParamInst (:any) %a: any
 // CHECK-NEXT:  $loc0      = LoadParamInst (:any) %b: any
-// CHECK-NEXT:  $loc1      = MovInst (:any) $loc1
-// CHECK-NEXT:  $loc0      = MovInst (:any) $loc0
+// CHECK-NEXT:  $loc1      = MovInst (:any) $loc1: any
+// CHECK-NEXT:  $loc0      = MovInst (:any) $loc0: any
 // CHECK-NEXT:  $loc2      = BranchInst %BB1
 // CHECK-NEXT:%BB1:
-// CHECK-NEXT:  $loc1      = PhiInst (:any) $loc1, %BB0, $loc1, %BB1
-// CHECK-NEXT:  $loc0      = PhiInst (:any) $loc0, %BB0, $loc0, %BB1
-// CHECK-NEXT:  $loc2      = MovInst (:any) $loc1
-// CHECK-NEXT:  $loc0      = MovInst (:any) $loc0
-// CHECK-NEXT:  $loc1      = MovInst (:any) $loc0
-// CHECK-NEXT:  $loc0      = MovInst (:any) $loc2
+// CHECK-NEXT:  $loc1      = PhiInst (:any) $loc1: any, %BB0, $loc1: any, %BB1
+// CHECK-NEXT:  $loc0      = PhiInst (:any) $loc0: any, %BB0, $loc0: any, %BB1
+// CHECK-NEXT:  $loc2      = MovInst (:any) $loc1: any
+// CHECK-NEXT:  $loc0      = MovInst (:any) $loc0: any
+// CHECK-NEXT:  $loc1      = MovInst (:any) $loc0: any
+// CHECK-NEXT:  $loc0      = MovInst (:any) $loc2: any
 // CHECK-NEXT:  $loc0      = BranchInst %BB1
 // CHECK-NEXT:function_end
--- test/BCGen/SH/call-and-new-args.js
+++ test/BCGen/SH/call-and-new-args.js
@@ -29,15 +29,15 @@ function test_builtin(a) {
 // CHECK-NEXT:  $loc1      = DeclareGlobalVarInst "test_call": string
 // CHECK-NEXT:  $loc1      = DeclareGlobalVarInst "test_new": string
 // CHECK-NEXT:  $loc1      = DeclareGlobalVarInst "test_builtin": string
-// CHECK-NEXT:  $loc2      = HBCCreateFunctionInst (:object) %test_call(): any, $loc0
+// CHECK-NEXT:  $loc2      = HBCCreateFunctionInst (:object) %test_call(): any, $loc0: environment
 // CHECK-NEXT:  $loc1      = HBCGetGlobalObjectInst (:object)
-// CHECK-NEXT:  $loc2      = StorePropertyLooseInst $loc2, $loc1, "test_call": string
-// CHECK-NEXT:  $loc2      = HBCCreateFunctionInst (:object) %test_new(): object, $loc0
-// CHECK-NEXT:  $loc2      = StorePropertyLooseInst $loc2, $loc1, "test_new": string
-// CHECK-NEXT:  $loc0      = HBCCreateFunctionInst (:object) %test_builtin(): number, $loc0
-// CHECK-NEXT:  $loc0      = StorePropertyLooseInst $loc0, $loc1, "test_builtin": string
+// CHECK-NEXT:  $loc2      = StorePropertyLooseInst $loc2: object, $loc1: object, "test_call": string
+// CHECK-NEXT:  $loc2      = HBCCreateFunctionInst (:object) %test_new(): object, $loc0: environment
+// CHECK-NEXT:  $loc2      = StorePropertyLooseInst $loc2: object, $loc1: object, "test_new": string
+// CHECK-NEXT:  $loc0      = HBCCreateFunctionInst (:object) %test_builtin(): number, $loc0: environment
+// CHECK-NEXT:  $loc0      = StorePropertyLooseInst $loc0: object, $loc1: object, "test_builtin": string
 // CHECK-NEXT:  $np0       = HBCLoadConstInst (:undefined) undefined: undefined
-// CHECK-NEXT:  $loc0      = ReturnInst $np0
+// CHECK-NEXT:  $loc0      = ReturnInst $np0: undefined
 // CHECK-NEXT:function_end
 
 // CHECK:function test_call(bar: any): any
@@ -51,26 +51,26 @@ function test_builtin(a) {
 // CHECK-NEXT:  $stack[0]  = HBCLoadConstInst (:number) 13: number
 // CHECK-NEXT:  $stack[6]  = HBCLoadConstInst (:undefined) undefined: undefined
 // CHECK-NEXT:  $stack[4]  = HBCLoadConstInst (:undefined) undefined: undefined
-// CHECK-NEXT:  $loc0      = CallInst (:any) $stack[5], empty: any, empty: any, $np4, $stack[4], $stack[3], $stack[2], $stack[1], $stack[0]
-// CHECK-NEXT:  $loc0      = ReturnInst $loc0
+// CHECK-NEXT:  $loc0      = CallInst (:any) $stack[5]: any, empty: any, empty: any, $np4: undefined, $stack[4]: undefined, $stack[3]: number, $stack[2]: number, $stack[1]: number, $stack[0]: number
+// CHECK-NEXT:  $loc0      = ReturnInst $loc0: any
 // CHECK-NEXT:function_end
 
 // CHECK:function test_new(bar: any): object
 // CHECK-NEXT:frame = []
 // CHECK-NEXT:%BB0:
 // CHECK-NEXT:  $loc0      = LoadParamInst (:any) %bar: any
-// CHECK-NEXT:  $loc1      = LoadPropertyInst (:any) $loc0, "prototype": string
-// CHECK-NEXT:  $loc1      = CreateThisInst (:object) $loc1, $loc0
+// CHECK-NEXT:  $loc1      = LoadPropertyInst (:any) $loc0: any, "prototype": string
+// CHECK-NEXT:  $loc1      = CreateThisInst (:object) $loc1: any, $loc0: any
 // CHECK-NEXT:  $stack[3]  = HBCLoadConstInst (:number) 10: number
 // CHECK-NEXT:  $stack[2]  = HBCLoadConstInst (:number) 11: number
 // CHECK-NEXT:  $stack[1]  = HBCLoadConstInst (:number) 12: number
 // CHECK-NEXT:  $stack[0]  = HBCLoadConstInst (:number) 13: number
-// CHECK-NEXT:  $stack[6]  = MovInst (:any) $loc0
-// CHECK-NEXT:  $stack[5]  = MovInst (:any) $loc0
-// CHECK-NEXT:  $stack[4]  = MovInst (:object) $loc1
-// CHECK-NEXT:  $loc0      = CallInst (:any) $stack[5], empty: any, empty: any, $loc0, $stack[4], $stack[3], $stack[2], $stack[1], $stack[0]
-// CHECK-NEXT:  $loc0      = GetConstructedObjectInst (:object) $loc1, $loc0
-// CHECK-NEXT:  $loc0      = ReturnInst $loc0
+// CHECK-NEXT:  $stack[6]  = MovInst (:any) $loc0: any
+// CHECK-NEXT:  $stack[5]  = MovInst (:any) $loc0: any
+// CHECK-NEXT:  $stack[4]  = MovInst (:object) $loc1: object
+// CHECK-NEXT:  $loc0      = CallInst (:any) $stack[5]: any, empty: any, empty: any, $loc0: any, $stack[4]: object, $stack[3]: number, $stack[2]: number, $stack[1]: number, $stack[0]: number
+// CHECK-NEXT:  $loc0      = GetConstructedObjectInst (:object) $loc1: object, $loc0: any
+// CHECK-NEXT:  $loc0      = ReturnInst $loc0: object
 // CHECK-NEXT:function_end
 
 // CHECK:function test_builtin(a: any): number
@@ -81,6 +81,6 @@ function test_builtin(a) {
 // CHECK-NEXT:  $stack[4]  = ImplicitMovInst (:undefined) undefined: undefined
 // CHECK-NEXT:  $stack[3]  = ImplicitMovInst (:empty) empty: empty
 // CHECK-NEXT:  $stack[2]  = ImplicitMovInst (:undefined) undefined: undefined
-// CHECK-NEXT:  $loc0      = CallBuiltinInst (:any) [HermesBuiltin.exponentiationOperator]: number, empty: any, empty: any, undefined: undefined, undefined: undefined, $stack[1], $stack[0]
-// CHECK-NEXT:  $loc0      = ReturnInst $loc0
+// CHECK-NEXT:  $loc0      = CallBuiltinInst (:any) [HermesBuiltin.exponentiationOperator]: number, empty: any, empty: any, undefined: undefined, undefined: undefined, $stack[1]: any, $stack[0]: number
+// CHECK-NEXT:  $loc0      = ReturnInst $loc0: any
 // CHECK-NEXT:function_end
