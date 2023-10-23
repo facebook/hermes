1adad3d76872eeb9743a6ccc7b6862d4d2234552
Seonggun Kim <seonggun@amazon.com>
2023-10-23 16:52:06 +0100

    Parse this argument in TypeScript function.

--- lib/Parser/JSParserImpl.cpp
+++ lib/Parser/JSParserImpl.cpp
@@ -590,9 +590,9 @@ bool JSParserImpl::parseFormalParameters(
   // (
   SMLoc lparenLoc = advance().Start;
 
-#if HERMES_PARSE_FLOW
-  // The first parameter can be 'this' in Flow mode.
-  if (context_.getParseFlow() && check(TokenKind::rw_this)) {
+#if HERMES_PARSE_FLOW || HERMES_PARSE_TS
+  // The first parameter can be 'this' in Flow and TypeScript.
+  if (context_.getParseTypes() && check(TokenKind::rw_this)) {
     auto *name = tok_->getResWordIdentifier();
     SMLoc thisParamStart = advance().Start;
 
@@ -605,7 +605,7 @@ bool JSParserImpl::parseFormalParameters(
             thisParamStart))
       return false;
 
-    auto optType = parseTypeAnnotationFlow(annotStart);
+    auto optType = parseTypeAnnotation(annotStart);
     if (!optType)
       return false;
     ESTree::Node *type = *optType;
