71154dce83ecc53bc059121100879ad1989db61d
Aakash Patel <avp@meta.com>
2023-11-08 09:51:48 -0800

    Add TypeOperator and implement renders parsing
    Summary:
    Original Author: pieterv@meta.com
    Original Git: 030a2dd26890a9f89a64ec3a275dce7399102502
    Original Reviewed By: avp
    Original Revision: D48692978
    
    This change does two things:
    - Adds a new AST node type of "TypeOperator". This matches the `TSTypeOperator` node. Using generic nodes for type operators like TS does makes it much easier to add support to JS tooling without having to implement any new logic.
    - Adds support for "renders" type operator via the "TypeOperator" node.
    
    Reviewed By: neildhar
    
    Differential Revision: D50992806
    
    fbshipit-source-id: 5b85fa814b22360d45e6138eefbc9706700c8982

---
+++ test/Parser/flow/component-syntax/renders.js
@@ -0,0 +1,114 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// RUN: %hermesc -parse-flow -Xparse-component-syntax -dump-ast -pretty-json %s | %FileCheck %s --match-full-lines
+
+// CHECK-LABEL: {
+// CHECK-NEXT:   "type": "Program",
+// CHECK-NEXT:   "body": [
+
+type A = renders B;
+// CHECK-NEXT:     {
+// CHECK-NEXT:       "type": "TypeAlias",
+// CHECK-NEXT:       "id": {
+// CHECK-NEXT:         "type": "Identifier",
+// CHECK-NEXT:         "name": "A"
+// CHECK-NEXT:       },
+// CHECK-NEXT:       "typeParameters": null,
+// CHECK-NEXT:       "right": {
+// CHECK-NEXT:         "type": "TypeOperator",
+// CHECK-NEXT:         "operator": "renders",
+// CHECK-NEXT:         "typeAnnotation": {
+// CHECK-NEXT:           "type": "GenericTypeAnnotation",
+// CHECK-NEXT:           "id": {
+// CHECK-NEXT:             "type": "Identifier",
+// CHECK-NEXT:             "name": "B"
+// CHECK-NEXT:           },
+// CHECK-NEXT:           "typeParameters": null
+// CHECK-NEXT:         }
+// CHECK-NEXT:       }
+// CHECK-NEXT:     },
+
+type A = renders (B | C);
+// CHECK-NEXT:     {
+// CHECK-NEXT:       "type": "TypeAlias",
+// CHECK-NEXT:       "id": {
+// CHECK-NEXT:         "type": "Identifier",
+// CHECK-NEXT:         "name": "A"
+// CHECK-NEXT:       },
+// CHECK-NEXT:       "typeParameters": null,
+// CHECK-NEXT:       "right": {
+// CHECK-NEXT:         "type": "TypeOperator",
+// CHECK-NEXT:         "operator": "renders",
+// CHECK-NEXT:         "typeAnnotation": {
+// CHECK-NEXT:           "type": "UnionTypeAnnotation",
+// CHECK-NEXT:           "types": [
+// CHECK-NEXT:             {
+// CHECK-NEXT:               "type": "GenericTypeAnnotation",
+// CHECK-NEXT:               "id": {
+// CHECK-NEXT:                 "type": "Identifier",
+// CHECK-NEXT:                 "name": "B"
+// CHECK-NEXT:               },
+// CHECK-NEXT:               "typeParameters": null
+// CHECK-NEXT:             },
+// CHECK-NEXT:             {
+// CHECK-NEXT:               "type": "GenericTypeAnnotation",
+// CHECK-NEXT:               "id": {
+// CHECK-NEXT:                 "type": "Identifier",
+// CHECK-NEXT:                 "name": "C"
+// CHECK-NEXT:               },
+// CHECK-NEXT:               "typeParameters": null
+// CHECK-NEXT:             }
+// CHECK-NEXT:           ]
+// CHECK-NEXT:         }
+// CHECK-NEXT:       }
+// CHECK-NEXT:     },
+
+component Foo(bar: renders A) {}
+// CHECK-NEXT:     {
+// CHECK-NEXT:       "type": "ComponentDeclaration",
+// CHECK-NEXT:       "id": {
+// CHECK-NEXT:         "type": "Identifier",
+// CHECK-NEXT:         "name": "Foo"
+// CHECK-NEXT:       },
+// CHECK-NEXT:       "params": [
+// CHECK-NEXT:         {
+// CHECK-NEXT:           "type": "ComponentParameter",
+// CHECK-NEXT:           "name": {
+// CHECK-NEXT:             "type": "Identifier",
+// CHECK-NEXT:             "name": "bar"
+// CHECK-NEXT:           },
+// CHECK-NEXT:           "local": {
+// CHECK-NEXT:             "type": "Identifier",
+// CHECK-NEXT:             "name": "bar",
+// CHECK-NEXT:             "typeAnnotation": {
+// CHECK-NEXT:               "type": "TypeAnnotation",
+// CHECK-NEXT:               "typeAnnotation": {
+// CHECK-NEXT:                 "type": "TypeOperator",
+// CHECK-NEXT:                 "operator": "renders",
+// CHECK-NEXT:                 "typeAnnotation": {
+// CHECK-NEXT:                   "type": "GenericTypeAnnotation",
+// CHECK-NEXT:                   "id": {
+// CHECK-NEXT:                     "type": "Identifier",
+// CHECK-NEXT:                     "name": "A"
+// CHECK-NEXT:                   },
+// CHECK-NEXT:                   "typeParameters": null
+// CHECK-NEXT:                 }
+// CHECK-NEXT:               }
+// CHECK-NEXT:             }
+// CHECK-NEXT:           },
+// CHECK-NEXT:           "shorthand": true
+// CHECK-NEXT:         }
+// CHECK-NEXT:       ],
+// CHECK-NEXT:       "body": {
+// CHECK-NEXT:         "type": "BlockStatement",
+// CHECK-NEXT:         "body": []
+// CHECK-NEXT:       }
+// CHECK-NEXT:     }
+
+// CHECK-NEXT:   ]
+// CHECK-NEXT: }
