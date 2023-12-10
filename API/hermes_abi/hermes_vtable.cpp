e526f8393e14d1a99d1846f8891d8969ea823ca3
Neil Dhar <neildhar@fb.com>
2023-12-09 17:44:00 -0800

    Add pointer types and convenience helpers to ABI implementation
    Differential Revision: D50396059
    
    fbshipit-source-id: 50eb1f51973e22f22776ea66a9a83efa2b4983dc

--- API/hermes_abi/hermes_vtable.cpp
+++ API/hermes_abi/hermes_vtable.cpp
@@ -9,10 +9,14 @@
 
 #include "hermes_abi/hermes_abi.h"
 
+#include "hermes/ADT/ManagedChunkedList.h"
 #include "hermes/BCGen/HBC/BytecodeProviderFromSrc.h"
 #include "hermes/Public/RuntimeConfig.h"
 #include "hermes/VM/Callable.h"
+#include "hermes/VM/JSArray.h"
+#include "hermes/VM/JSArrayBuffer.h"
 #include "hermes/VM/Runtime.h"
+#include "hermes_abi/HermesABIHelpers.h"
 
 #include "llvh/Support/ConvertUTF.h"
 
@@ -20,6 +24,145 @@ using namespace hermes;
 using namespace facebook::hermes;
 
 namespace {
+
+/// A ManagedChunkedList element that indicates whether it's occupied based on
+/// a refcount.
+template <typename T>
+class ManagedValue : public HermesABIManagedPointer {
+  static void invalidate(HermesABIManagedPointer *ptr) {
+    static_cast<ManagedValue<T> *>(ptr)->dec();
+  }
+  static constexpr HermesABIManagedPointerVTable vt{invalidate};
+
+ public:
+  ManagedValue() : HermesABIManagedPointer{&vt}, refCount_(0) {}
+
+  /// Determine whether the element is occupied by inspecting the refcount.
+  bool isFree() const {
+    return refCount_.load(std::memory_order_relaxed) == 0;
+  }
+
+  /// Store a value and start the refcount at 1. After invocation, this
+  /// instance is occupied with a value, and the "nextFree" methods should
+  /// not be used until the value is released.
+  template <typename... Args>
+  void emplace(Args &&...args) {
+    assert(isFree() && "Emplacing already occupied value");
+    refCount_.store(1, std::memory_order_relaxed);
+    new (&value_) T(std::forward<Args>(args)...);
+  }
+
+  /// Get the next free element. Must not be called when this instance is
+  /// occupied with a value.
+  ManagedValue<T> *getNextFree() {
+    assert(isFree() && "Free pointer unusable while occupied");
+    return nextFree_;
+  }
+
+  /// Set the next free element. Must not be called when this instance is
+  /// occupied with a value.
+  void setNextFree(ManagedValue<T> *nextFree) {
+    assert(isFree() && "Free pointer unusable while occupied");
+    nextFree_ = nextFree;
+  }
+
+  T &value() {
+    assert(!isFree() && "Value not present");
+    return value_;
+  }
+
+  const T &value() const {
+    assert(!isFree() && "Value not present");
+    return value_;
+  }
+
+  void inc() {
+    // It is always safe to use relaxed operations for incrementing the
+    // reference count, because the only operation that may occur concurrently
+    // with it is decrementing the reference count, and we do not need to
+    // enforce any ordering between the two.
+    auto oldCount = refCount_.fetch_add(1, std::memory_order_relaxed);
+    assert(oldCount && "Cannot resurrect a pointer");
+    assert(oldCount + 1 != 0 && "Ref count overflow");
+    (void)oldCount;
+  }
+
+  void dec() {
+    // It is safe to use relaxed operations here because decrementing the
+    // reference count is the only access that may be performed without proper
+    // synchronisation. As a result, the only ordering we need to enforce when
+    // decrementing is that the vtable pointer used to call \c invalidate is
+    // loaded from before the decrement, in case the decrement ends up causing
+    // this value to be freed. We get this ordering from the fact that the
+    // vtable read and the reference count update form a load-store control
+    // dependency, which preserves their ordering on any reasonable hardware.
+    auto oldCount = refCount_.fetch_sub(1, std::memory_order_relaxed);
+    assert(oldCount > 0 && "Ref count underflow");
+    (void)oldCount;
+  }
+
+ private:
+  std::atomic<uint32_t> refCount_;
+  union {
+    T value_;
+    ManagedValue<T> *nextFree_;
+  };
+};
+
+/// Helper functions to create a handle from a HermesABI reference.
+template <typename T = vm::HermesValue>
+vm::Handle<T> toHandle(HermesABIManagedPointer *value) {
+  return vm::Handle<T>::vmcast(
+      &static_cast<ManagedValue<vm::PinnedHermesValue> *>(value)->value());
+}
+vm::Handle<vm::JSObject> toHandle(HermesABIObject obj) {
+  return toHandle<vm::JSObject>(obj.pointer);
+}
+vm::Handle<vm::StringPrimitive> toHandle(HermesABIString str) {
+  return toHandle<vm::StringPrimitive>(str.pointer);
+}
+vm::Handle<vm::SymbolID> toHandle(HermesABISymbol sym) {
+  return toHandle<vm::SymbolID>(sym.pointer);
+}
+vm::Handle<vm::SymbolID> toHandle(HermesABIPropNameID sym) {
+  return toHandle<vm::SymbolID>(sym.pointer);
+}
+vm::Handle<vm::JSArray> toHandle(HermesABIArray arr) {
+  return toHandle<vm::JSArray>(arr.pointer);
+}
+vm::Handle<vm::BigIntPrimitive> toHandle(HermesABIBigInt bi) {
+  return toHandle<vm::BigIntPrimitive>(bi.pointer);
+}
+vm::Handle<vm::Callable> toHandle(HermesABIFunction fn) {
+  return toHandle<vm::Callable>(fn.pointer);
+}
+vm::Handle<vm::JSArrayBuffer> toHandle(HermesABIArrayBuffer ab) {
+  return toHandle<vm::JSArrayBuffer>(ab.pointer);
+}
+
+/// Helper function to convert a HermesABIValue to a HermesValue.
+vm::HermesValue toHermesValue(const HermesABIValue &val) {
+  switch (abi::getValueKind(val)) {
+    case HermesABIValueKindUndefined:
+      return vm::HermesValue::encodeUndefinedValue();
+    case HermesABIValueKindNull:
+      return vm::HermesValue::encodeNullValue();
+    case HermesABIValueKindBoolean:
+      return vm::HermesValue::encodeBoolValue(abi::getBoolValue(val));
+    case HermesABIValueKindNumber:
+      return vm::HermesValue::encodeUntrustedNumberValue(
+          abi::getNumberValue(val));
+    case HermesABIValueKindString:
+    case HermesABIValueKindObject:
+    case HermesABIValueKindSymbol:
+    case HermesABIValueKindBigInt:
+      return *toHandle<>(val.data.pointer);
+    default:
+      // This incoming value is either an error, or from a newer version of the
+      // ABI, which violates our expectations.
+      hermes_fatal("Value has an unexpected tag.");
+  }
+}
 
 /// A thin wrapper around vm::Runtime to provide additional state for things
 /// like pointer management. It is intended to provide a small number of helper
@@ -30,10 +173,121 @@ class HermesABIRuntimeImpl : public HermesABIRuntime {
 
  public:
   std::shared_ptr<::hermes::vm::Runtime> rt;
+  ManagedChunkedList<ManagedValue<vm::PinnedHermesValue>> hermesValues;
+  ManagedChunkedList<ManagedValue<vm::WeakRoot<vm::JSObject>>> weakHermesValues;
 
   explicit HermesABIRuntimeImpl(const hermes::vm::RuntimeConfig &runtimeConfig)
       : HermesABIRuntime{&vtable},
-        rt(hermes::vm::Runtime::create(runtimeConfig)) {}
+        rt(hermes::vm::Runtime::create(runtimeConfig)),
+        hermesValues(runtimeConfig.getGCConfig().getOccupancyTarget(), 0.5),
+        weakHermesValues(
+            runtimeConfig.getGCConfig().getOccupancyTarget(),
+            0.5) {
+    // Add custom roots functions to the runtime to expose references retained
+    // through the API as roots.
+    rt->addCustomRootsFunction([this](vm::GC *, vm::RootAcceptor &acceptor) {
+      hermesValues.forEach(
+          [&acceptor](auto &element) { acceptor.accept(element.value()); });
+    });
+    rt->addCustomWeakRootsFunction(
+        [this](vm::GC *, vm::WeakRootAcceptor &acceptor) {
+          weakHermesValues.forEach([&acceptor](auto &element) {
+            acceptor.acceptWeak(element.value());
+          });
+        });
+  }
+
+  ~HermesABIRuntimeImpl() {
+    // Release the runtime to make sure that any remaining references held by
+    // things like HostObject are freed.
+    rt.reset();
+    assert(hermesValues.sizeForTests() == 0 && "Dangling references.");
+    assert(weakHermesValues.sizeForTests() == 0 && "Dangling references.");
+  }
+
+  /// Create a HermesABIValue from a HermesValue, including creating root
+  /// entries for pointers if necessary.
+  HermesABIValue createValue(vm::HermesValue hv) {
+    switch (hv.getETag()) {
+      case vm::HermesValue::ETag::Undefined:
+        return abi::createUndefinedValue();
+      case vm::HermesValue::ETag::Null:
+        return abi::createNullValue();
+      case vm::HermesValue::ETag::Bool:
+        return abi::createBoolValue(hv.getBool());
+      case vm::HermesValue::ETag::Symbol:
+        return abi::createSymbolValue(&hermesValues.add(hv));
+      case vm::HermesValue::ETag::Str1:
+      case vm::HermesValue::ETag::Str2:
+        return abi::createStringValue(&hermesValues.add(hv));
+      case vm::HermesValue::ETag::BigInt1:
+      case vm::HermesValue::ETag::BigInt2:
+        return abi::createBigIntValue(&hermesValues.add(hv));
+      case vm::HermesValue::ETag::Object1:
+      case vm::HermesValue::ETag::Object2:
+        return abi::createObjectValue(&hermesValues.add(hv));
+      default:
+        assert(hv.isNumber() && "No other types are permitted in the API.");
+        return abi::createNumberValue(hv.getNumber());
+    }
+  }
+
+  /// Convenience function to create a HermesABIValueOrError directly from a
+  /// HermesValue.
+  HermesABIValueOrError createValueOrError(vm::HermesValue hv) {
+    return abi::createValueOrError(createValue(hv));
+  }
+
+  /// Create a HermesABIManagedPointer entry using the given HermesValue. This
+  /// template should never be used directly, and is only useful so that we can
+  /// define the pointer creation functions using a macro below.
+  template <typename T>
+  HermesABIManagedPointer *createPointerImpl(vm::HermesValue hv) {
+    if constexpr (!std::is_same_v<T, HermesABIWeakObject>)
+      return &hermesValues.add(hv);
+
+    return &weakHermesValues.add(
+        vm::WeakRoot<vm::JSObject>(vm::vmcast<vm::JSObject>(hv), *rt));
+  }
+
+  /// Define functions to create each of the Hermes ABI pointer types from a
+  /// HermesValue.
+
+#define DECLARE_HERMES_ABI_POINTER_HELPERS(name)                               \
+  HermesABI##name create##name(vm::HermesValue hv) {                           \
+    return abi::create##name(createPointerImpl<HermesABI##name>(hv));          \
+  }                                                                            \
+  HermesABI##name##OrError create##name##OrError(vm::HermesValue hv) {         \
+    return abi::create##name##OrError(createPointerImpl<HermesABI##name>(hv)); \
+  }
+  HERMES_ABI_POINTER_TYPES(DECLARE_HERMES_ABI_POINTER_HELPERS)
+#undef DECLARE_HERMES_ABI_POINTER_HELPERS
+
+  /// Create a Handle<HermesValue> from a HermesABIValue. Note that the Handle
+  /// will be cheaply constructed from the existing PinnedHermesValue, so the
+  /// HermesABIValue must outlive the Handle.
+  vm::Handle<> makeHandle(const HermesABIValue &val) {
+    switch (abi::getValueKind(val)) {
+      case HermesABIValueKindUndefined:
+        return vm::Runtime::getUndefinedValue();
+      case HermesABIValueKindNull:
+        return vm::Runtime::getNullValue();
+      case HermesABIValueKindBoolean:
+        return vm::Runtime::getBoolValue(abi::getBoolValue(val));
+      case HermesABIValueKindNumber:
+        return rt->makeHandle(vm::HermesValue::encodeUntrustedNumberValue(
+            abi::getNumberValue(val)));
+      case HermesABIValueKindString:
+      case HermesABIValueKindObject:
+      case HermesABIValueKindSymbol:
+      case HermesABIValueKindBigInt:
+        return toHandle<>(val.data.pointer);
+      default:
+        // This incoming value is either an error, or from a newer version of
+        // the ABI, which violates our expectations.
+        hermes_fatal("Value has an unexpected tag.");
+    }
+  }
 };
 
 /// Convenience function to cast the given HermesABIRuntime to a
