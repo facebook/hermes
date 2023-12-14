7e43577920e7dc0cf6787c4ec00dc2f616dbacbc
Simon Corsin <simon@corsin.me>
2023-12-14 10:24:21 -0600

    added failing test

---
+++ test/hermes/es6-class-method-from-var.js
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// RUN: %hermes -Xes6-class %s | %FileCheck --match-full-lines %s
+
+const keys = {
+  private: Symbol(),
+  public: 'myPublicMethod'
+}
+
+class Test {
+  [keys.private]() {
+    console.log('Hello from public method!');
+  }
+
+  [keys.public]() {
+    console.log('Hello from private method!');
+  }
+}
+
+const object = new Test();
+
+object.myPublicMethod();
+// CHECK: Hello from public method!
+
+object[keys.private]();
+// CHECK: Hello from private method!
