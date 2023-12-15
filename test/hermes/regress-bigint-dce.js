ae826d0c4a1c7c4706fde64d447485e4629dc725
Tzvetan Mikov <tmikov@meta.com>
2023-12-14 20:26:35 -0800

    Fix bigint side effects for >>>, /, % (#1214)
    Summary:
    
    BigInt differs from numbers - several bigint operations can throw an
    exception, specifically:
    - >>> is not allowed at all and throws
    - / and % can result in division by zero
    
    Improve the side effect model and add a test.
    
    This diff was ported from SH.
    
    Closes https://github.com/facebook/hermes/issues/1212
    
    Differential Revision: D52013223

--- test/hermes/regress-bigint-dce.js
+++ test/hermes/regress-bigint-dce.js
@@ -8,11 +8,12 @@
 // RUN: %hermes -O0 %s | %FileCheck --match-full-lines %s
 // RUN: %hermes -O %s | %FileCheck --match-full-lines %s
 
+(function() {
+
 // Verify that arithmetic operations with a BigInt operand and non-BigInt
 // operand (except adds) are not DCE-d, because that would throw a runtime
 // exception.
 
-(function() {
 try {
     1n + 1;
 } catch (e) {
@@ -25,5 +26,29 @@ try {
 } catch (e) {
     print(e);
 }
-})();
 //CHECK-NEXT: TypeError: Cannot convert BigInt to number
+
+// Verify that BigInt operations that may throw are not eliminated:
+// unsigned right shift, division, modulo.
+try {
+    1n >>> 1n;
+} catch (e) {
+    print(e);
+}
+//CHECK-NEXT: TypeError: BigInts have no unsigned shift
+
+try {
+    1n / 0n;
+} catch (e) {
+    print(e);
+}
+//CHECK-NEXT: RangeError: Division by zero
+
+try {
+    1n % 0n;
+} catch (e) {
+    print(e);
+}
+//CHECK-NEXT: RangeError: Division by zero
+
+})();
