4bb59fa5736a0283931b209e785c9f293c6932fb
Michael Leon <fbmal7@meta.com>
2024-01-08 13:50:28 -0800

    Update restricted properties test
    Summary:
    We changed `caller` and `arguments` to always throw, but this test
    wasn't updated at the time.
    
    Reviewed By: avp
    
    Differential Revision: D51675956
    
    fbshipit-source-id: 87a80f0e6c4e56ee6b3d692fb5a8c5f3a5b872f8

--- test/hermes/function-non-strict.js
+++ test/hermes/function-non-strict.js
@@ -19,15 +19,22 @@ print('function properties');
 // CHECK-LABEL: function properties
 print(typeof strict, typeof nonStrict);
 // CHECK-NEXT: function function
-print(nonStrict.caller, nonStrict.arguments);
-// CHECK-NEXT: undefined undefined
+try { print(nonStrict.caller); } catch(e) { print('caught', e.name, e.message); }
+// CHECK-NEXT: caught TypeError Restricted property cannot be accessed
+try { print(nonStrict.arguments); } catch(e) { print('caught', e.name, e.message); }
+// CHECK-NEXT: caught TypeError Restricted property cannot be accessed
 try { print(strict.caller); } catch(e) { print('caught', e.name, e.message); }
-// CHECK-NEXT: caught TypeError Restricted in strict mode
+// CHECK-NEXT: caught TypeError Restricted property cannot be accessed
 try { print(strict.arguments); } catch(e) { print('caught', e.name, e.message); }
-// CHECK-NEXT: caught TypeError Restricted in strict mode
+// CHECK-NEXT: caught TypeError Restricted property cannot be accessed
 
 var bound = nonStrict.bind(42);
 try { print(bound.caller); } catch(e) { print('caught', e.name, e.message); }
-// CHECK-NEXT: caught TypeError Restricted in strict mode
+// CHECK-NEXT: caught TypeError Restricted property cannot be accessed
 try { print(bound.arguments); } catch(e) { print('caught', e.name, e.message); }
-// CHECK-NEXT: caught TypeError Restricted in strict mode
+// CHECK-NEXT: caught TypeError Restricted property cannot be accessed
+
+try { print(Function.prototype.caller); } catch(e) { print('caught', e.name, e.message); }
+// CHECK-NEXT: caught TypeError Restricted property cannot be accessed
+try { print(Function.prototype.arguments); } catch(e) { print('caught', e.name, e.message); }
+// CHECK-NEXT: caught TypeError Restricted property cannot be accessed
