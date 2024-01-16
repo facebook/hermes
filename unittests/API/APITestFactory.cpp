005897de9ac561b02ace6a61885d75e397491819
Neil Dhar <neildhar@meta.com>
2024-01-15 16:25:42 -0800

    Run API tests against the sandbox runtime (#1248)
    Summary:
    Pull Request resolved: https://github.com/facebook/hermes/pull/1248
    
    X-link: https://github.com/facebook/react-native/pull/42264
    
    Add a buck build for the sandbox, and run our existing API tests
    against it.
    
    Reviewed By: avp
    
    Differential Revision: D48086019
    
    fbshipit-source-id: 2ee2eca93e27557d2609eaaec2fce3218d8b873f

--- unittests/API/APITestFactory.cpp
+++ unittests/API/APITestFactory.cpp
@@ -8,6 +8,7 @@
 #include <hermes/hermes.h>
 #include <hermes_abi/HermesABIRuntimeWrapper.h>
 #include <hermes_abi/hermes_vtable.h>
+#include <hermes_sandbox/HermesSandboxRuntime.h>
 #include <jsi/test/testlib.h>
 #include <jsi/threadsafe.h>
 
@@ -20,7 +21,8 @@ std::vector<RuntimeFactory> runtimeGenerators() {
   return {
       [] { return makeHermesRuntime(); },
       [] { return makeThreadSafeHermesRuntime(); },
-      [] { return makeHermesABIRuntimeWrapper(get_hermes_abi_vtable()); }};
+      [] { return makeHermesABIRuntimeWrapper(get_hermes_abi_vtable()); },
+      [] { return makeHermesSandboxRuntime(); }};
 }
 
 } // namespace jsi
