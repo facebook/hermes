c28180242ed00aea5776bf0390afd2ac552cb015
Tzvetan Mikov <tmikov@meta.com>
2024-01-17 20:51:13 -0800

    FunctionAnalysis: follow through CheckTypedCastInst
    Summary:
    As long as CheckTypedCast is known to succeed, we should be able to
    follow through it.
    
    Reviewed By: neildhar, avp
    
    Differential Revision: D52771157
    
    fbshipit-source-id: d71629d447bca82cd2cab3e528f3a311340d65be

---
+++ test/Optimizer/flow/func-analysis-ccast.js
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// RUN: %shermes -typed -dump-ir -Xdump-functions=bar -Xcustom-opt=frameloadstoreopts,functionanalysis %s | %FileCheckOrRegen %s --match-full-lines
+
+// Ensure that FunctionAnalysis needs to follow checked casts.
+// bar() below uses a checked cast before calling foo().
+// The IR output we are checking shows the cast and that CallInst instruction has the
+// correct target.
+
+var foo: any = function () {}
+
+function bar() {
+    return (foo as () => void)();
+}
+
+// Auto-generated content below. Please do not modify manually.
+
+// CHECK:function bar(): any [allCallsitesKnownInStrictMode]
+// CHECK-NEXT:frame = []
+// CHECK-NEXT:%BB0:
+// CHECK-NEXT:  %0 = LoadFrameInst (:any) [foo@""]: any
+// CHECK-NEXT:  %1 = CheckedTypeCastInst (:object) %0: any, type(object)
+// CHECK-NEXT:  %2 = CallInst [njsf] (:any) %1: object, %foo(): functionCode, empty: any, undefined: undefined, undefined: undefined
+// CHECK-NEXT:       ReturnInst %2: any
+// CHECK-NEXT:function_end
