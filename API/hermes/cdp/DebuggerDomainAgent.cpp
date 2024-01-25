f8079e383bfba9119f90898a205eabcd994c85f8
Danny Su <dsc@meta.com>
2024-01-25 13:23:34 -0800

    Add DebuggerDomainAgent
    Summary:
    Initial implementation of DebuggerDomainAgent. Currently only handles 
    `Debugger.enable` and `Debugger.disable`.
    
    Reviewed By: mattbfb
    
    Differential Revision: D52803668

---
+++ API/hermes/cdp/DebuggerDomainAgent.cpp
@@ -0,0 +1,161 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "DebuggerDomainAgent.h"
+
+#include <hermes/inspector/chrome/RemoteObjectConverters.h>
+
+namespace facebook {
+namespace hermes {
+namespace cdp {
+
+using namespace facebook::hermes::debugger;
+
+DebuggerDomainAgent::DebuggerDomainAgent(
+    HermesRuntime &runtime,
+    AsyncDebuggerAPI &asyncDebugger,
+    OutboundMessageFunc messageCallback)
+    : DomainAgent(messageCallback),
+      runtime_(runtime),
+      asyncDebugger_(asyncDebugger),
+      debuggerEventCallbackId_(kInvalidDebuggerEventCallbackID),
+      enabled_(false) {}
+
+DebuggerDomainAgent::~DebuggerDomainAgent() {
+  // Also remove DebuggerEventCallback here in case we don't receive a
+  // Debugger.disable command prior to destruction.
+  asyncDebugger_.removeDebuggerEventCallback_TS(debuggerEventCallbackId_);
+}
+
+void DebuggerDomainAgent::handleDebuggerEvent(
+    HermesRuntime &runtime,
+    AsyncDebuggerAPI &asyncDebugger,
+    DebuggerEventType event) {
+  assert(enabled_ && "Should only be receiving debugger events while enabled");
+
+  switch (event) {
+    case DebuggerEventType::ScriptLoaded:
+      processNewLoadedScript();
+      asyncDebugger_.setNextCommand(debugger::Command::continueExecution());
+      break;
+    case DebuggerEventType::Exception:
+      break;
+    case DebuggerEventType::EvalComplete:
+      break;
+    case DebuggerEventType::Resumed:
+      break;
+    case DebuggerEventType::DebuggerStatement:
+      break;
+    case DebuggerEventType::Breakpoint:
+      break;
+    case DebuggerEventType::StepFinish:
+      break;
+    case DebuggerEventType::ExplicitPause:
+      break;
+  }
+}
+
+void DebuggerDomainAgent::enable(const m::debugger::EnableRequest &req) {
+  if (enabled_) {
+    sendResponseToClient(m::makeErrorResponse(
+        req.id,
+        m::ErrorCode::InvalidRequest,
+        "Debugger domain already enabled"));
+    return;
+  }
+  enabled_ = true;
+  sendResponseToClient(m::makeOkResponse(req.id));
+
+  // The debugger just got enabled; inform the client about all scripts.
+  for (auto &srcLoc : runtime_.getDebugger().getLoadedScripts()) {
+    sendScriptParsedNotificationToClient(srcLoc);
+  }
+
+  runtime_.getDebugger().setShouldPauseOnScriptLoad(true);
+  asyncDebugger_.addDebuggerEventCallback_TS(
+      [this](
+          HermesRuntime &runtime,
+          AsyncDebuggerAPI &asyncDebugger,
+          DebuggerEventType event) {
+        handleDebuggerEvent(runtime, asyncDebugger, event);
+      });
+
+  // Check to see if debugger is currently paused. There could be multiple debug
+  // clients and the program could have already been paused.
+  if (asyncDebugger_.isWaitingForCommand()) {
+    sendPausedNotificationToClient();
+  }
+}
+
+void DebuggerDomainAgent::disable(const m::debugger::DisableRequest &req) {
+  if (!enabled_) {
+    sendResponseToClient(m::makeErrorResponse(
+        req.id, m::ErrorCode::InvalidRequest, "Debugger domain not enabled"));
+    return;
+  }
+
+  // This doesn't support other debug clients also setting breakpoints. If we
+  // need that functionality, then we might need to track breakpoints set by
+  // this client and only remove those.
+  runtime_.getDebugger().deleteAllBreakpoints();
+
+  asyncDebugger_.removeDebuggerEventCallback_TS(debuggerEventCallbackId_);
+  // This doesn't work well if there are other debug clients that also toggle
+  // this flag. If we need that functionality, then DebuggerAPI needs to be
+  // changed.
+  runtime_.getDebugger().setShouldPauseOnScriptLoad(false);
+
+  enabled_ = false;
+
+  sendResponseToClient(m::makeOkResponse(req.id));
+}
+
+void DebuggerDomainAgent::sendPausedNotificationToClient() {
+  m::debugger::PausedNotification note;
+  note.reason = "other";
+  note.callFrames = m::debugger::makeCallFrames(
+      runtime_.getDebugger().getProgramState(), objTable_, runtime_);
+  sendNotificationToClient(note);
+}
+
+void DebuggerDomainAgent::sendScriptParsedNotificationToClient(
+    const debugger::SourceLocation srcLoc) {
+  m::debugger::ScriptParsedNotification note;
+  note.scriptId = std::to_string(srcLoc.fileId);
+  note.url = srcLoc.fileName;
+  note.executionContextId = kHermesExecutionContextId;
+  std::string sourceMappingUrl =
+      runtime_.getDebugger().getSourceMappingUrl(srcLoc.fileId);
+  if (!sourceMappingUrl.empty()) {
+    note.sourceMapURL = sourceMappingUrl;
+  }
+  sendNotificationToClient(note);
+}
+
+void DebuggerDomainAgent::processNewLoadedScript() {
+  assert(
+      asyncDebugger_.isWaitingForCommand() &&
+      "Can only be called while paused");
+
+  auto stackTrace = runtime_.getDebugger().getProgramState().getStackTrace();
+
+  if (stackTrace.callFrameCount() > 0) {
+    debugger::SourceLocation loc = stackTrace.callFrameForIndex(0).location;
+
+    // Invalid fileId indicates debug info isn't included when compilation took
+    // place. E.g. compiling to bytecode without -g.
+    if (loc.fileId == debugger::kInvalidLocation) {
+      return;
+    }
+
+    sendScriptParsedNotificationToClient(loc);
+  }
+}
+
+} // namespace cdp
+} // namespace hermes
+} // namespace facebook
