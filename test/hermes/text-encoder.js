7f9d9d5e44e2df5170f33d1604bea3b085f40e39
Danny Su <dsc@meta.com>
2024-02-16 21:27:39 -0800

    Add TextEncoder.prototype.encode()
    Summary: Implement TextEncoder's `encode()` function.
    
    Reviewed By: avp
    
    Differential Revision: D53213625
    
    fbshipit-source-id: 706d57c9bd155872e07b2547028db7bdb045609f

--- test/hermes/text-encoder.js
+++ test/hermes/text-encoder.js
@@ -23,3 +23,44 @@ print(desc.configurable);
 
 print(encoder.encoding);
 // CHECK-NEXT: utf-8
+
+try {
+  const b = {};
+  TextEncoder.prototype.encode.call(b, '');
+} catch (e) {
+  print(e.message);
+  // CHECK-NEXT: TextEncoder.prototype.encode() called on non-TextEncoder object
+}
+
+let result = encoder.encode('test');
+print(Object.prototype.toString.call(result));
+// CHECK-NEXT: [object Uint8Array]
+
+print(result.length, result.join(' '));
+// CHECK-NEXT: 4 116 101 115 116
+
+result = encoder.encode('');
+print(result.length);
+// CHECK-NEXT: 0
+
+result = encoder.encode('\x0D');
+print(result.length, result[0]);
+// CHECK-NEXT: 1 13
+
+// Test UTF-16 conversion
+result = encoder.encode('\u{2191}\u{2192}');
+print(Object.prototype.toString.call(result));
+// CHECK-NEXT: [object Uint8Array]
+
+print(result.length, result.join(' '));
+// CHECK-NEXT: 6 226 134 145 226 134 146
+
+// A surrogate pair
+result = encoder.encode('\u{D83D}\u{DE03}');
+print(result.length, result.join(' '));
+// CHECK-NEXT: 4 240 159 152 131
+
+// Incomplete surrogate
+result = encoder.encode('\u{D83D}');
+print(result.length, result.join(' '));
+// CHECK-NEXT: 3 239 191 189
