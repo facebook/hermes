1c4e560f1dc0ab80e9158f0078319e1a7ffc9400
Neil Dhar <neildhar@meta.com>
2024-02-22 18:24:09 -0800

    Split the sandbox file into 8 for parallel compilation (#1271) (#1271)
    Summary:
    Pull Request resolved: https://github.com/facebook/hermes/pull/1271
    
    Original Author: neildhar@meta.com
    Original Git: c65d640e1710cf9717b2898c9a0f110e1385dc07
    Original Reviewed By: lavenzg
    Original Revision: D53029684
    
    Reviewed By: dannysu
    
    Differential Revision: D54092054
    
    fbshipit-source-id: 870199b3f539302e58417e3752e4d75efd111bb2

---
+++ API/hermes_sandbox/external/hermes_sandbox_impl_compiled_1.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#ifdef NDEBUG
+#include "hermes_sandbox_impl_opt_compiled_1.c"
+#else
+#include "hermes_sandbox_impl_dbg_compiled_1.c"
+#endif
