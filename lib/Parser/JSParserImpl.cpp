df2d0060eb5ea580a436bd730298da1369ca34c6
Neil Dhar <neildhar@meta.com>
2024-02-23 10:12:31 -0800

    Fix GrammarContext for 'as' expression type
    Summary:
    Original Author: avp@meta.com
    Original Git: 3f29575c2232f22637acbbc5165dd1cedec3c0d9
    Original Reviewed By: tmikov
    Original Revision: D53135103
    
    The RHS of the `as` should be parsed with `Type` GrammarContext,
    which can be done by moving the `advance()` call after the check.
    
    Tested on a large Fb4a bundle and this doesn't appear to regress overall
    parser performance at all (no extra work is done, just some reordering).
    
    Reviewed By: avp
    
    Differential Revision: D54092021
    
    fbshipit-source-id: c8337aa675ecd14ff544dbe2509e68b706076cca

--- lib/Parser/JSParserImpl.cpp
+++ lib/Parser/JSParserImpl.cpp
@@ -4170,11 +4170,11 @@ Optional<ESTree::Node *> JSParserImpl::parseBinaryExpression(Param param) {
     //                 We are here
     // Push topExpr and the '*', so we can parse rightExpr.
     stack.emplace_back(topExpr, tok_->getKind(), topExprStartLoc);
-    advance();
 
-    topExprStartLoc = tok_->getStartLoc();
 #if HERMES_PARSE_TS || HERMES_PARSE_FLOW
     if (LLVM_UNLIKELY(stack.back().opKind == TokenKind::as_operator)) {
+      advance(JSLexer::GrammarContext::Type);
+      topExprStartLoc = tok_->getStartLoc();
       auto optRightExpr = parseTypeAnnotation();
       if (!optRightExpr)
         return None;
@@ -4182,6 +4182,8 @@ Optional<ESTree::Node *> JSParserImpl::parseBinaryExpression(Param param) {
     } else
 #endif
     {
+      advance();
+      topExprStartLoc = tok_->getStartLoc();
       if (LLVM_UNLIKELY(check(TokenKind::private_identifier))) {
         topExpr = consumePrivateIdentifier();
       } else {
