36d0e126566de36a1d9042aab12cdecd79671fe7
Neil Dhar <neildhar@meta.com>
2024-02-23 11:17:18 -0800

    Add TextEncoder.prototype.encode()
    Summary:
    Implement TextEncoder's `encode()` function.
    
    Original Author: dsc@meta.com
    Original Git: 7f9d9d5e44e2df5170f33d1604bea3b085f40e39
    Original Reviewed By: avp
    Original Revision: D53213625
    
    Reviewed By: tmikov
    
    Differential Revision: D54092019
    
    fbshipit-source-id: 111ad4222fd119245223245712a461d158071163

--- lib/VM/JSLib/TextEncoder.cpp
+++ lib/VM/JSLib/TextEncoder.cpp
@@ -7,6 +7,10 @@
 
 #include "JSLibInternal.h"
 
+#include "llvh/Support/ConvertUTF.h"
+
+#include "hermes/VM/JSTypedArray.h"
+
 namespace hermes {
 namespace vm {
 
@@ -38,6 +42,14 @@ Handle<JSObject> createTextEncoderConstructor(Runtime &runtime) {
       nullptr,
       /* enumerable */ true,
       /* configurable */ true);
+
+  defineMethod(
+      runtime,
+      textEncoderPrototype,
+      Predefined::getSymbolID(Predefined::encode),
+      nullptr,
+      textEncoderPrototypeEncode,
+      1);
 
   auto cons = defineSystemConstructor<JSObject>(
       runtime,
@@ -101,6 +113,73 @@ textEncoderPrototypeEncoding(void *, Runtime &runtime, NativeArgs args) {
 
   return HermesValue::encodeStringValue(
       runtime.getPredefinedString(Predefined::utf8));
+}
+
+CallResult<HermesValue>
+textEncoderPrototypeEncode(void *, Runtime &runtime, NativeArgs args) {
+  GCScope gcScope{runtime};
+  auto selfHandle = args.dyncastThis<JSObject>();
+  NamedPropertyDescriptor desc;
+  bool exists = JSObject::getOwnNamedDescriptor(
+      selfHandle,
+      runtime,
+      Predefined::getSymbolID(Predefined::InternalPropertyTextEncoderType),
+      desc);
+  if (LLVM_UNLIKELY(!exists)) {
+    return runtime.raiseTypeError(
+        "TextEncoder.prototype.encode() called on non-TextEncoder object");
+  }
+
+  auto strRes = toString_RJS(runtime, args.getArgHandle(0));
+  if (LLVM_UNLIKELY(strRes == ExecutionStatus::EXCEPTION)) {
+    return ExecutionStatus::EXCEPTION;
+  }
+  Handle<StringPrimitive> string = runtime.makeHandle(std::move(*strRes));
+
+  // If input string is empty, then the function can return early. This also
+  // avoids having to check later before calling std::memcpy to avoid undefined
+  // behavior.
+  if (LLVM_UNLIKELY(string->getStringLength() == 0)) {
+    auto result = Uint8Array::allocate(runtime);
+    if (LLVM_UNLIKELY(result == ExecutionStatus::EXCEPTION)) {
+      return ExecutionStatus::EXCEPTION;
+    }
+    return result->getHermesValue();
+  }
+
+  if (string->isASCII()) {
+    // ASCII string can trivially be converted to UTF-8 because ASCII is a
+    // strict subset.
+    auto result = Uint8Array::allocate(runtime, string->getStringLength());
+    if (LLVM_UNLIKELY(result == ExecutionStatus::EXCEPTION)) {
+      return ExecutionStatus::EXCEPTION;
+    }
+
+    Handle<JSTypedArrayBase> typedArray = result.getValue();
+    llvh::ArrayRef<char> strRef = string->getStringRef<char>();
+
+    std::memcpy(
+        typedArray->begin(runtime), strRef.data(), string->getStringLength());
+    return typedArray.getHermesValue();
+  } else {
+    // Convert UTF-16 to UTF-8
+    llvh::ArrayRef<char16_t> strRef = string->getStringRef<char16_t>();
+    std::string converted;
+    bool success = convertUTF16ToUTF8WithReplacements(converted, strRef);
+    if (LLVM_UNLIKELY(!success)) {
+      return runtime.raiseError("Failed to convert from UTF-16 to UTF-8");
+    }
+
+    auto result = Uint8Array::allocate(runtime, converted.length());
+    if (LLVM_UNLIKELY(result == ExecutionStatus::EXCEPTION)) {
+      return ExecutionStatus::EXCEPTION;
+    }
+
+    Handle<JSTypedArrayBase> typedArray = result.getValue();
+    std::memcpy(
+        typedArray->begin(runtime), converted.data(), converted.length());
+    return typedArray.getHermesValue();
+  }
 }
 
 } // namespace vm
