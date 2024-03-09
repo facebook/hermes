a93b2c5e86a949249a565fb46e39b51a45737832
Aakash Patel <avp@meta.com>
2024-03-08 16:08:35 -0800

    Fix Array functions with large lengths
    Summary:
    `toLength` can return numbers that are greater than uint32_t,
    so that needs to be returned and checked.
    
    Hermes version: D54667593
    
    Reviewed By: tmikov
    
    Differential Revision: D54667592
    
    fbshipit-source-id: bababa6e0b509443a8f43a7ccd305e86e25cfdfc

--- test/hermes/array-functions.js
+++ test/hermes/array-functions.js
@@ -1199,3 +1199,6 @@ try {
   print(e.name)
 }
 // CHECK-NEXT: RangeError
+var obj = {length: 2 ** 32};
+try { Array.prototype.with.call(obj, 0, 123) } catch (e) { print(e.name) }
+// CHECK-NEXT: RangeError
