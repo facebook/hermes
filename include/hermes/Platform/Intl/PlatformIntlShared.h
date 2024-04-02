1b92f8c5e6fd4f042484f45f20daa5b9b2d4c5d2
Manuel Puyol <manuelpuyol@github.com>
2024-04-02 15:23:04 -0700

    copy doc comments to .h

--- include/hermes/Platform/Intl/PlatformIntlShared.h
+++ include/hermes/Platform/Intl/PlatformIntlShared.h
@@ -25,26 +25,32 @@ struct ResolvedLocale {
   std::unordered_map<std::u16string, std::u16string> extensions;
 };
 
+/// https://402.ecma-international.org/8.0/#sec-canonicalizelocalelist
 vm::CallResult<std::vector<std::u16string>> canonicalizeLocaleList(
     vm::Runtime &runtime,
     const std::vector<std::u16string> &locales);
 
+/// https://402.ecma-international.org/8.0/#sec-intl.getcanonicallocales
 vm::CallResult<std::vector<std::u16string>> getCanonicalLocales(
     vm::Runtime &runtime,
     const std::vector<std::u16string> &locales);
 
+/// https://402.ecma-international.org/8.0/#sec-bestavailablelocale
 std::optional<std::u16string> bestAvailableLocale(
     const std::vector<std::u16string> &availableLocales,
     const std::u16string &locale);
 
+/// https://402.ecma-international.org/8.0/#sec-lookupsupportedlocales
 std::vector<std::u16string> lookupSupportedLocales(
     const std::vector<std::u16string> &availableLocales,
     const std::vector<std::u16string> &requestedLocales);
 
+/// https://402.ecma-international.org/8.0/#sec-supportedlocales
 std::vector<std::u16string> supportedLocales(
     const std::vector<std::u16string> &availableLocales,
     const std::vector<std::u16string> &requestedLocales);
 
+/// https://402.ecma-international.org/8.0/#sec-getoption
 vm::CallResult<std::optional<std::u16string>> getOptionString(
     vm::Runtime &runtime,
     const Options &options,
@@ -52,20 +58,24 @@ vm::CallResult<std::optional<std::u16string>> getOptionString(
     llvh::ArrayRef<std::u16string_view> values,
     std::optional<std::u16string_view> fallback);
 
+/// https://402.ecma-international.org/8.0/#sec-getoption
 std::optional<bool> getOptionBool(
     vm::Runtime &runtime,
     const Options &options,
     const std::u16string &property,
     std::optional<bool> fallback);
 
+/// https://402.ecma-international.org/8.0/#sec-todatetimeoptions
 vm::CallResult<Options> toDateTimeOptions(
     vm::Runtime &runtime,
     Options options,
     std::u16string_view required,
     std::u16string_view defaults);
 
+/// https://402.ecma-international.org/8.0/#sec-case-sensitivity-and-case-mapping
 std::u16string toASCIIUppercase(std::u16string_view tz);
 
+/// https://402.ecma-international.org/8.0/#sec-defaultnumberoption
 vm::CallResult<std::optional<uint8_t>> defaultNumberOption(
     vm::Runtime &runtime,
     const std::u16string &property,
@@ -74,6 +84,7 @@ vm::CallResult<std::optional<uint8_t>> defaultNumberOption(
     const std::uint8_t maximum,
     std::optional<uint8_t> fallback);
 
+/// https://402.ecma-international.org/8.0/#sec-getoption
 vm::CallResult<std::optional<uint8_t>> getNumberOption(
     vm::Runtime &runtime,
     const Options &options,
