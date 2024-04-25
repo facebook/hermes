fbee5bf8e80293642365451e402afede84af9782
Danny Su <dsc@meta.com>
2024-04-25 14:57:20 -0700

    Add CDP Agent
    Summary:
    Original Author: mattblagden@meta.com
    Original Git: beec32e05dca6b100e6c6016ab6bd3c8dc3fe158
    Original Reviewed By: dannysu
    Original Revision: D52802495
    
    Add an agent that provides an CDP-based interface to
    interact with a runtime being debugged.
    
    It is currently a framework for setting up handlers for
    each CDP domain, and does not process any messages
    yet.
    
    Reviewed By: mattbfb
    
    Differential Revision: D56482788
    
    fbshipit-source-id: 6c93332692cdfe0fca1485e8f852f12709972689

---
+++ API/hermes/cdp/CDPAgent.h
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#ifndef HERMES_CDP_CDPAGENT_H
+#define HERMES_CDP_CDPAGENT_H
+
+#include <string>
+
+#include <hermes/AsyncDebuggerAPI.h>
+#include <hermes/Public/HermesExport.h>
+#include <hermes/RuntimeTaskRunner.h>
+#include <hermes/hermes.h>
+
+namespace facebook {
+namespace hermes {
+namespace cdp {
+
+using OutboundMessageFunc = std::function<void(const std::string &)>;
+
+class CDPAgentImpl;
+
+/// An agent for interacting with the provided \p runtime and
+/// \p asyncDebuggerAPI via CDP messages in the Debugger, Runtime, Profiler,
+/// HeapProfiler domains.
+/// The integrator of the agent is expected to manage a queue of tasks to be
+/// executed with exclusive access to the runtime (i.e. executed when
+/// JavaScript is not running). Tasks to be run are delivered to the integrator
+/// via the provided \p enqueueRuntimeTaskCallback, and should be executed in
+/// order, at the first opportunity between evaluating JavaScript.
+/// The integrator can deliver CDP commands to the agent via the
+/// \p handleCommand method. When a CDP response or event is generated, it will
+/// be delivered to the integrator via the provided \p messageCallback.
+/// Both callbacks may be invoked from arbitrary threads.
+class HERMES_EXPORT CDPAgent {
+  /// Hide the constructor so users can only construct via static create
+  /// methods.
+  CDPAgent(
+      HermesRuntime &runtime,
+      debugger::AsyncDebuggerAPI &asyncDebuggerAPI,
+      debugger::EnqueueRuntimeTaskFunc enqueueRuntimeTaskCallback,
+      OutboundMessageFunc messageCallback);
+
+ public:
+  /// Create a new CDP Agent. This can be done on an arbitrary thread; the
+  /// runtime will not be accessed during execution of this function.
+  static std::unique_ptr<CDPAgent> create(
+      HermesRuntime &runtime,
+      debugger::AsyncDebuggerAPI &asyncDebuggerAPI,
+      debugger::EnqueueRuntimeTaskFunc enqueueRuntimeTaskCallback,
+      OutboundMessageFunc messageCallback);
+
+  /// Destroy the CDP Agent. This can be done on an arbitrary thread.
+  /// It's expected that the integrator will continue to process any runtime
+  /// tasks enqueued during destruction.
+  ~CDPAgent();
+
+  /// Process a CDP command encoded in \p json. This can be called from
+  /// arbitrary threads.
+  void handleCommand(std::string json);
+
+ private:
+  std::shared_ptr<CDPAgentImpl> impl_;
+};
+
+} // namespace cdp
+} // namespace hermes
+} // namespace facebook
+
+#endif // HERMES_CDP_CDPAGENT_H
