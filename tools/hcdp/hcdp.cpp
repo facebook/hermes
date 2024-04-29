f2a1debcc67d2e18260ba36843bd0cea4aa6ba7c
Danny Su <dsc@meta.com>
2024-04-29 10:45:41 -0700

    Add Hermes CDP CLI
    Summary:
    Original Author: mattblagden@meta.com
    Original Git: b5ce8b02b2ed74899879870c444f55133ba18fca
    Original Reviewed By: dannysu
    Original Revision: D54383479
    
    Add an initial version of the `hcdp` tool, which is a utility
    to run Hermes CDP from the CLI.
    
    This tool is a successor to the "hermes chrome debug tool"
    from previous versions of Hermes CDP.
    
    Like the the hermes chrome debug tool, this currently
    carries out debugging so long as the script is running,
    so longer-running scripts are ideal (e.g. scripts with a
    loop, or a `debugger;` statement).
    
    As the tool expands, we may move to have a more
    sophisticated build process (e.g. typescript compilation,
    multiple files, bundling, etc.), but it's kept simple for now.
    
    Reviewed By: mattbfb
    
    Differential Revision: D56482711
    
    fbshipit-source-id: 83400436a6ca5521d830108823a94405a4e8361d

---
+++ tools/hcdp/hcdp.cpp
@@ -0,0 +1,178 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#ifndef HERMES_ENABLE_DEBUGGER
+
+#include <iostream>
+
+int main(void) {
+  std::cout << "hcdp compiled without Hermes debugger enabled." << std::endl;
+  return EXIT_FAILURE;
+}
+
+#else // defined(HERMES_ENABLE_DEBUGGER)
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+#include <hermes/SerialExecutor/SerialExecutor.h>
+#include <hermes/cdp/CDPAgent.h>
+#include <hermes/cdp/CDPDebugAPI.h>
+#include <hermes/hermes.h>
+
+#include "IPC.h"
+
+namespace hermes {
+
+namespace fbhermes = ::facebook::hermes;
+namespace cdp = fbhermes::cdp;
+using namespace facebook::hermes;
+
+constexpr uint32_t kExecutionContextId = 1;
+
+namespace {
+/// Manages a runtime executing a script on a thread.
+class RuntimeInstance {
+ public:
+  /// Create a new CDP-debuggable runtime, and execute the specified
+  /// script on a new runtime thread.
+  explicit RuntimeInstance(std::string scriptSource, std::string scriptUrl)
+      : executor_(std::make_unique<SerialExecutor>()),
+        runtime_(
+            fbhermes::makeHermesRuntime(::hermes::vm::RuntimeConfig::Builder()
+                                            .withEnableSampleProfiling(true)
+                                            .build())) {
+    cdpDebugAPI_ = cdp::CDPDebugAPI::create(*runtime_);
+
+    executor_->add([this,
+                    source = std::move(scriptSource),
+                    url = std::move(scriptUrl)]() mutable {
+      fbhermes::HermesRuntime::DebugFlags flags{};
+      runtime_->debugJavaScript(std::move(source), std::move(url), flags);
+    });
+  }
+
+  ~RuntimeInstance() {
+    // Wait for tasks to complete
+    executor_.reset();
+
+    // Tear down the debug API and runtime.
+    cdpDebugAPI_.reset();
+    runtime_.reset();
+  }
+
+  /// Get the underlying Hermes runtime.
+  fbhermes::HermesRuntime &runtime() {
+    return *runtime_;
+  }
+
+  /// Gets the CDP debugging interface for this runtime instance.
+  cdp::CDPDebugAPI &cdpDebugAPI() {
+    return *cdpDebugAPI_;
+  }
+
+  /// Add a task to be executed by the runtime thread.
+  void addTask(std::function<void()> task) {
+    executor_->add(std::move(task));
+  }
+
+ private:
+  std::unique_ptr<SerialExecutor> executor_;
+  std::unique_ptr<fbhermes::HermesRuntime> runtime_;
+  std::unique_ptr<cdp::CDPDebugAPI> cdpDebugAPI_;
+};
+} // namespace
+
+/// Read a script from the specified path.
+static std::optional<std::string> readScript(const char *path) {
+  std::ifstream stream(path);
+  if (!stream.is_open()) {
+    return std::nullopt;
+  }
+  return std::string{
+      std::istreambuf_iterator<char>(stream), std::istreambuf_iterator<char>()};
+}
+
+/// Run a script, launching the VM and carrying out CDPAgent sessions as
+/// clients connect and disconnect.
+static void debugScript(std::string scriptSource, std::string scriptUrl) {
+  // Start a runtime thread to execute the script.
+  std::unique_ptr<RuntimeInstance> runtimeInstance =
+      std::make_unique<RuntimeInstance>(
+          std::move(scriptSource), std::move(scriptUrl));
+
+  // Process IPC messages
+  std::unordered_map<ClientID, std::unique_ptr<cdp::CDPAgent>> agents;
+  while (std::optional<IPCCommand> ipc = receiveIPC()) {
+    ClientID clientID = ipc.value().clientID;
+    switch (ipc.value().type) {
+      case kConnectIPCType: {
+        // Client connected; start an agent for it.
+        agents.emplace(
+            clientID,
+            cdp::CDPAgent::create(
+                kExecutionContextId,
+                runtimeInstance->cdpDebugAPI(),
+                [&runtimeInstance](
+                    std::function<void(fbhermes::HermesRuntime &)> task) {
+                  // Enqueue any runtime tasks on the runtime thread.
+                  fbhermes::HermesRuntime &runtime = runtimeInstance->runtime();
+                  runtimeInstance->addTask(
+                      [&runtime, task = std::move(task)]() { task(runtime); });
+                },
+                [clientID](const std::string &message) {
+                  // Forward message to the client, via an IPC message.
+                  sendIPC(kMessageIPCType, clientID, message);
+                }));
+      } break;
+
+      case kMessageIPCType: {
+        // Message from a client; handle it via the client's agent.
+        auto agent = agents.find(clientID);
+        if (agent == agents.end()) {
+          // Received a message for an agent that was previously-destroyed,
+          // or one that never existed.
+          throw std::runtime_error("No such agent");
+        }
+        agent->second->handleCommand(ipc.value().message);
+      } break;
+
+      case kDisconnectIPCType: {
+        // Client disconnected; destroy its agent.
+        agents.erase(clientID);
+      } break;
+
+      default:
+        throw std::runtime_error("Unknown IPC type");
+    }
+  }
+}
+
+} // namespace hermes
+
+using namespace hermes;
+
+int main(int argc, char **argv) {
+  // Unbuffer stdout to avoid storing large lines (e.g. profiling results).
+  setbuf(stdout, nullptr);
+
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " scriptPath" << std::endl;
+    return EXIT_FAILURE;
+  }
+  const char *scriptPath = argv[1];
+  std::optional<std::string> script = readScript(scriptPath);
+  if (!script) {
+    std::cerr << "Failed to read script at " << scriptPath << std::endl;
+  }
+  std::string filename = std::filesystem::path(scriptPath).filename().string();
+  debugScript(std::move(*script), std::move(filename));
+  return EXIT_SUCCESS;
+}
+
+#endif // defined(HERMES_ENABLE_DEBUGGER)
