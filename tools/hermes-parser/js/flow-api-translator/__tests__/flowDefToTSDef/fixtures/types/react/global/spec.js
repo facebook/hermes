4dfc2bf9cef450db85b1c1f98067853b39c15836
Sam Zhou <samzhou19815@meta.com>
2024-05-13 09:14:57 -0700

    Support 3-arg React.AbstractComponent translation
    Summary: Changelog: flow-api-translator now supports translating React.AbstractComponent with 3 type arguments into TS. The third type argument containing render type information will be ignored.
    
    Reviewed By: jbrown215
    
    Differential Revision: D57263528
    
    fbshipit-source-id: 516bacac6bb4d0de27d8c159d90620f1b276d5c9

--- tools/hermes-parser/js/flow-api-translator/__tests__/flowDefToTSDef/fixtures/types/react/global/spec.js
+++ tools/hermes-parser/js/flow-api-translator/__tests__/flowDefToTSDef/fixtures/types/react/global/spec.js
@@ -13,16 +13,17 @@ type T2 = React$MixedElement; // JSX.Element
 type T3 = React$Element<typeof Component>; // React.ReactElement<typeof Component>
 type T4 = React$ElementRef<typeof Component>; // React.ElementRef<typeof Component>
 type T5 = React$AbstractComponent<Props, HTMLElement>; // React.ComponentType<Props & React.RefAttributes<HTMLElement>>
-type T6 = React$Context<Foo>; // React.Context<Foo>
-type T7 = React$ComponentType<Props>; // React.ComponentType<Props>;
-type T8 = React$FragmentType; // React.Fragment
-type T9 = React$ElementConfig<typeof Component>; // JSX.LibraryManagedAttributes<typeof Component, React.ComponentProps<typeof Component>>
-type T10 = React$Ref<typeof Component>; // NonNullable<React.Ref<typeof Component> | string | number>
-type T11 = React$Key; // React.Key
-type T12 = React$Component<Props, {}>; // React.Component<Props, {}>
-type T13 = React$ElementType; // React.ElementType
-type T14 = React$ChildrenArray<T>; // T | ReadonlyArray<T>
-type T15 = React$ElementProps<T>; // React.ComponentProps<T>
+type T6 = React$AbstractComponent<Props, HTMLElement, T5>; // React.ComponentType<Props & React.RefAttributes<HTMLElement>>
+type T7 = React$Context<Foo>; // React.Context<Foo>
+type T8 = React$ComponentType<Props>; // React.ComponentType<Props>;
+type T9 = React$FragmentType; // React.Fragment
+type T10 = React$ElementConfig<typeof Component>; // JSX.LibraryManagedAttributes<typeof Component, React.ComponentProps<typeof Component>>
+type T11 = React$Ref<typeof Component>; // NonNullable<React.Ref<typeof Component> | string | number>
+type T12 = React$Key; // React.Key
+type T13 = React$Component<Props, {}>; // React.Component<Props, {}>
+type T14 = React$ElementType; // React.ElementType
+type T15 = React$ChildrenArray<T>; // T | ReadonlyArray<T>
+type T16 = React$ElementProps<T>; // React.ComponentProps<T>
 
 type Props = {A: string};
 declare function Component(props: Props): React$Node;
