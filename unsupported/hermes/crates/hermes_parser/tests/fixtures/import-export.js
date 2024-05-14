a9c59e32b550305c1f1d948a55c313b651bd6e4c
Tianyu Yao <skyyao@meta.com>
2024-05-14 11:09:19 -0700

    Add hermes_parser snapshots tests for imports and exports
    Summary: To see how the AST looks like and make sure they are correct
    
    Reviewed By: evanyeung
    
    Differential Revision: D57238204
    
    fbshipit-source-id: 7f92f2dd6e4c73c61026c9a15f9ab04ccfa34a55

---
+++ unsupported/hermes/crates/hermes_parser/tests/fixtures/import-export.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import a from 'A';
+import * as a from 'A';
+import type {a} from 'A';
+import {a} from 'A';
+
+export default A;
+export type B = A;
