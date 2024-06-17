87419f8c7f32483855f9cc008a0fbceac4c65e05
Tzvetan Mikov <tmikov@meta.com>
2024-06-17 15:45:35 -0700

    Fix benchmarks/raytracer/typed/raytracer.js
    Summary: .
    
    Reviewed By: neildhar, avp
    
    Differential Revision: D58696057
    
    fbshipit-source-id: 364bded0405e267046d22943c7d1ba086994d00b

--- benchmarks/raytracer/typed/raytracer.js
+++ benchmarks/raytracer/typed/raytracer.js
@@ -441,20 +441,20 @@ class RayTracer {
 }
 
 function defaultScene(): Scene {
-  return {
-    things: [
+  return new Scene(
+    [
       new Plane(new Vector(0.0, 1.0, 0.0), 0.0, Surfaces_checkerboard),
       new Sphere(new Vector(0.0, 1.0, -0.25), 1.0, Surfaces_shiny),
       new Sphere(new Vector(-1.0, 0.5, 1.5), 0.5, Surfaces_shiny),
+    ] as Thing[],
+    [
+      new Light(new Vector(-2.0, 2.5, 0.0), new Color(0.49, 0.07, 0.07)),
+      new Light(new Vector(1.5, 2.5, 1.5), new Color(0.07, 0.07, 0.49)),
+      new Light(new Vector(1.5, 2.5, -1.5), new Color(0.07, 0.49, 0.071)),
+      new Light(new Vector(0.0, 3.5, 0.0), new Color(0.21, 0.21, 0.35)),
     ],
-    lights: [
-      { pos: new Vector(-2.0, 2.5, 0.0), color: new Color(0.49, 0.07, 0.07) },
-      { pos: new Vector(1.5, 2.5, 1.5), color: new Color(0.07, 0.07, 0.49) },
-      { pos: new Vector(1.5, 2.5, -1.5), color: new Color(0.07, 0.49, 0.071) },
-      { pos: new Vector(0.0, 3.5, 0.0), color: new Color(0.21, 0.21, 0.35) },
-    ],
-    camera: new Camera(new Vector(3.0, 2.0, 4.0), new Vector(-1.0, 0.5, 0.0)),
-  };
+    new Camera(new Vector(3.0, 2.0, 4.0), new Vector(-1.0, 0.5, 0.0)),
+  );
 }
 
 function exec(width: number, height: number, buf: c_ptr) {
