ad31330d3a64effa9a6f30afe18b5fc516d7adb9
Gang Zhao (Hermes) <zhaogang@meta.com>
2024-06-24 00:48:54 -0700

    Add support for flow type annotations in hermes_parser crate
    Summary:
    Original Author: skyyao@meta.com
    Original Git: e6ae6960a37474a877bad0f3c186b29f76da7a84
    Original Reviewed By: monicatang
    Original Revision: D53881129
    
    This stack will be adding flow types support to the Rust hermes wrapper. The main change needed is to complete the ecmascript.json to generate correct wrapper code.
    This diff adds flow type annotations for a few primitive types.
    
    Reviewed By: neildhar
    
    Differential Revision: D58849785
    
    fbshipit-source-id: 3380da7015dd83480d023da4df556eba55029e1f

---
+++ unsupported/hermes/crates/hermes_parser/tests/fixtures/type-annotations.flow.js
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+let BooleanTypeAnnotation: boolean;
+let NumberTypeAnnotation: number;
+let StringTypeAnnotation: string;
+let MixedTypeAnnotation: mixed;
+let EmptyTypeAnnotation: empty;
+let VoidTypeAnnotation: void;
+let NullLiteralTypeAnnotation: null;
+let AnyTypeAnnotation: any;
+let StringLiteralTypeAnnotation: '123';
+let NumberLiteralTypeAnnotation: 123;
+let SymbolTypeAnnotation: symbol;
+let BigIntLiteralTypeAnnotation: 10n;
+let ExistsTypeAnnotation: *;
+let BooleanLiteralTypeAnnotation: true;
