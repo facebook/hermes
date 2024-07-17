c095e35e69d0f463f8d8c62e194f1b311b85648a
George Zahariev <gkz@meta.com>
2024-07-16 18:46:33 -0700

    Add support for `StringPrefix` and `StringSuffix`
    Summary:
    Adds support for `StringPrefix` to the flow-api-translator, by turning it into a TS string literal type.
    
    - `StringPrefix<foo>` is turned into `foo${string}`
    - `StringPrefix<foo, T>` is turned into `foo${T}`
    and
    
    - `StringSuffix<foo>` is turned into `${string}foo`
    - `StringSuffix<foo, T>` is turned into `${T}foo`
    
    Reviewed By: pieterv
    
    Differential Revision: D59833819
    
    fbshipit-source-id: a78edaa6de12b502aa54472715e7d906faadb516

--- tools/hermes-parser/js/flow-api-translator/src/flowDefToTSDef.js
+++ tools/hermes-parser/js/flow-api-translator/src/flowDefToTSDef.js
@@ -1915,6 +1915,31 @@ const getTransforms = (
         return [];
       };
 
+      function assertHasTypeParametersInRange(
+        min: number,
+        max: number,
+      ): $ReadOnlyArray<TSESTree.TypeNode> {
+        const {typeParameters} = node;
+        if (typeParameters == null) {
+          if (min > 0) {
+            throw translationError(
+              node,
+              `Expected between ${min} and ${max} type parameters with \`${fullTypeName}\`, but got none.`,
+            );
+          }
+          return [];
+        }
+        const params = typeParameters.params;
+        if (params.length < min || params.length > max) {
+          throw translationError(
+            node,
+            `Expected between ${min} and ${max} type parameters with \`${fullTypeName}\`, but got ${params.length}.`,
+          );
+        }
+
+        return typeParameters.params.map(transformTypeAnnotationType);
+      }
+
       switch (fullTypeName) {
         case '$Call':
         case '$ObjMap':
@@ -2240,6 +2265,100 @@ const getTransforms = (
               loc: DUMMY_LOC,
               typeAnnotation: param,
             },
+          };
+        }
+
+        case 'StringPrefix': {
+          // `StringPrefix<foo>` => `foo${string}`
+          // `StringPrefix<foo, T>` => `foo${T}`
+          const params = assertHasTypeParametersInRange(1, 2);
+          const prefix = params[0];
+          if (
+            prefix.type !== 'TSLiteralType' ||
+            typeof prefix.literal.value !== 'string'
+          ) {
+            throw translationError(
+              node,
+              'Expected a string literal for the first type parameter.',
+            );
+          }
+          const prefixStr = prefix.literal.value;
+          const remainder = params[1] ?? {
+            type: 'TSStringKeyword',
+            loc: DUMMY_LOC,
+          };
+
+          return {
+            type: 'TSTemplateLiteralType',
+            loc: DUMMY_LOC,
+            quasis: [
+              {
+                type: 'TemplateElement',
+                loc: DUMMY_LOC,
+                value: {
+                  raw: prefixStr,
+                  cooked: prefixStr,
+                },
+                tail: false,
+              },
+              {
+                type: 'TemplateElement',
+                loc: DUMMY_LOC,
+                value: {
+                  raw: '',
+                  cooked: '',
+                },
+                tail: true,
+              },
+            ],
+            types: [remainder],
+          };
+        }
+
+        case 'StringSuffix': {
+          // `StringSuffix<foo>` => `${string}foo`
+          // `StringSuffix<foo, T>` => `${T}foo`
+          const params = assertHasTypeParametersInRange(1, 2);
+          const suffix = params[0];
+          if (
+            suffix.type !== 'TSLiteralType' ||
+            typeof suffix.literal.value !== 'string'
+          ) {
+            throw translationError(
+              node,
+              'Expected a string literal for the first type parameter.',
+            );
+          }
+          const suffixStr = suffix.literal.value;
+          const remainder = params[1] ?? {
+            type: 'TSStringKeyword',
+            loc: DUMMY_LOC,
+          };
+
+          return {
+            type: 'TSTemplateLiteralType',
+            loc: DUMMY_LOC,
+            quasis: [
+              {
+                type: 'TemplateElement',
+                loc: DUMMY_LOC,
+                value: {
+                  raw: '',
+                  cooked: '',
+                },
+                tail: false,
+              },
+              {
+                type: 'TemplateElement',
+                loc: DUMMY_LOC,
+                value: {
+                  raw: suffixStr,
+                  cooked: suffixStr,
+                },
+                tail: true,
+              },
+            ],
+            types: [remainder],
           };
         }
       }
