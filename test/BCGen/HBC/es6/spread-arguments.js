80afaefd8fde1e57aeeeaf56302740039f59fc27
Riccardo Cipolleschi <cipolleschi@meta.com>
2024-08-19 11:23:29 +0100

    Revert "Recycle block local registers in fast pass (#1448)"
    This reverts commit 8f6067371ae1f287f7c303106e20ecb299fb98f3.

--- test/BCGen/HBC/es6/spread-arguments.js
+++ test/BCGen/HBC/es6/spread-arguments.js
@@ -12,42 +12,40 @@ function foo(fn, x) {
   new fn(...x);
 }
 
-// CHECK-LABEL:Function<foo>(3 params, 17 registers, 2 symbols):
+// CHECK-LABEL: Function<foo>(3 params, 28 registers, 2 symbols):
 // CHECK-NEXT: Offset in debug table: source 0x{{.*}}, scope 0x{{.*}}
-// CHECK-NEXT:     CreateEnvironment r3
-// CHECK-NEXT:     LoadParam         r5, 1
-// CHECK-NEXT:     LoadParam         r1, 2
-// CHECK-NEXT:     StoreToEnvironment r3, 0, r5
-// CHECK-NEXT:     StoreToEnvironment r3, 1, r1
-// CHECK-NEXT:     LoadFromEnvironment r1, r3, 0
-// CHECK-NEXT:     LoadConstZero     r2
-// CHECK-NEXT:     Mov               r5, r2
-// CHECK-NEXT:     LoadFromEnvironment r2, r3, 1
-// CHECK-NEXT:     NewArray          r4, 0
-// CHECK-NEXT:     Mov               r6, r5
-// CHECK-NEXT:     Mov               r9, r4
-// CHECK-NEXT:     Mov               r8, r2
-// CHECK-NEXT:     Mov               r7, r6
-// CHECK-NEXT:     CallBuiltin       r2, "HermesBuiltin.arraySpread", 4
-// CHECK-NEXT:     Mov               r5, r2
-// CHECK-NEXT:     LoadConstUndefined r2
-// CHECK-NEXT:     Mov               r9, r1
-// CHECK-NEXT:     Mov               r8, r4
-// CHECK-NEXT:     Mov               r7, r2
-// CHECK-NEXT:     CallBuiltin       r0, "HermesBuiltin.apply", 4
-// CHECK-NEXT:     LoadFromEnvironment r1, r3, 0
-// CHECK-NEXT:     LoadConstZero     r2
-// CHECK-NEXT:     Mov               r4, r2
-// CHECK-NEXT:     LoadFromEnvironment r3, r3, 1
-// CHECK-NEXT:     NewArray          r2, 0
-// CHECK-NEXT:     Mov               r5, r4
-// CHECK-NEXT:     Mov               r9, r2
-// CHECK-NEXT:     Mov               r8, r3
-// CHECK-NEXT:     Mov               r7, r5
-// CHECK-NEXT:     CallBuiltin       r3, "HermesBuiltin.arraySpread", 4
-// CHECK-NEXT:     Mov               r4, r3
-// CHECK-NEXT:     Mov               r9, r1
-// CHECK-NEXT:     Mov               r8, r2
-// CHECK-NEXT:     CallBuiltin       r0, "HermesBuiltin.apply", 3
-// CHECK-NEXT:     LoadConstUndefined r1
-// CHECK-NEXT:     Ret               r1
+// CHECK-NEXT:     CreateEnvironment r0
+// CHECK-NEXT:     LoadParam         r1, 1
+// CHECK-NEXT:     LoadParam         r2, 2
+// CHECK-NEXT:     LoadConstZero     r3
+// CHECK-NEXT:     LoadConstUndefined r4
+// CHECK-NEXT:     StoreToEnvironment r0, 0, r1
+// CHECK-NEXT:     StoreToEnvironment r0, 1, r2
+// CHECK-NEXT:     LoadFromEnvironment r5, r0, 0
+// CHECK-NEXT:     Mov               r6, r3
+// CHECK-NEXT:     LoadFromEnvironment r7, r0, 1
+// CHECK-NEXT:     NewArray          r8, 0
+// CHECK-NEXT:     Mov               r9, r6
+// CHECK-NEXT:     Mov               r20, r8
+// CHECK-NEXT:     Mov               r19, r7
+// CHECK-NEXT:     Mov               r18, r9
+// CHECK-NEXT:     CallBuiltin       r10, "HermesBuiltin.arraySpread", 4
+// CHECK-NEXT:     Mov               r6, r10
+// CHECK-NEXT:     Mov               r20, r5
+// CHECK-NEXT:     Mov               r19, r8
+// CHECK-NEXT:     Mov               r18, r4
+// CHECK-NEXT:     CallBuiltin       r11, "HermesBuiltin.apply", 4
+// CHECK-NEXT:     LoadFromEnvironment r11, r0, 0
+// CHECK-NEXT:     Mov               r12, r3
+// CHECK-NEXT:     LoadFromEnvironment r13, r0, 1
+// CHECK-NEXT:     NewArray          r14, 0
+// CHECK-NEXT:     Mov               r15, r12
+// CHECK-NEXT:     Mov               r20, r14
+// CHECK-NEXT:     Mov               r19, r13
+// CHECK-NEXT:     Mov               r18, r15
+// CHECK-NEXT:     CallBuiltin       r16, "HermesBuiltin.arraySpread", 4
+// CHECK-NEXT:     Mov               r12, r16
+// CHECK-NEXT:     Mov               r20, r11
+// CHECK-NEXT:     Mov               r19, r14
+// CHECK-NEXT:     CallBuiltin       r17, "HermesBuiltin.apply", 3
+// CHECK-NEXT:     Ret               r4
--- test/IRGen/instance_of.js
+++ test/IRGen/instance_of.js
@@ -17,29 +17,30 @@ function simple_test0(x, y) {
 // CHECK:function global#0()#1
 // CHECK-NEXT:frame = [], globals = [simple_test0]
 // CHECK-NEXT:%BB0:
-// CHECK-NEXT:  $Reg1           	%0 = HBCCreateEnvironmentInst %S{global#0()#1}
-// CHECK-NEXT:  $Reg1           	%1 = HBCCreateFunctionInst %simple_test0#0#1()#2, %0
-// CHECK-NEXT:  $Reg2           	%2 = HBCGetGlobalObjectInst
-// CHECK-NEXT:  $Reg0           	%3 = StorePropertyInst %1 : closure, %2 : object, "simple_test0" : string
-// CHECK-NEXT:  $Reg1           	%4 = AllocStackInst $?anon_0_ret
-// CHECK-NEXT:  $Reg2           	%5 = HBCLoadConstInst undefined : undefined
-// CHECK-NEXT:  $Reg0           	%6 = StoreStackInst %5 : undefined, %4
-// CHECK-NEXT:  $Reg1           	%7 = LoadStackInst %4
-// CHECK-NEXT:  $Reg0           	%8 = ReturnInst %7
+// CHECK-NEXT:  $Reg0           	%0 = HBCCreateEnvironmentInst %S{global#0()#1}
+// CHECK-NEXT:  $Reg1           	%1 = HBCGetGlobalObjectInst
+// CHECK-NEXT:  $Reg2           	%2 = HBCLoadConstInst undefined : undefined
+// CHECK-NEXT:  $Reg3           	%3 = HBCCreateFunctionInst %simple_test0#0#1()#2, %0
+// CHECK-NEXT:  $Reg4           	%4 = StorePropertyInst %3 : closure, %1 : object, "simple_test0" : string
+// CHECK-NEXT:  $Reg4           	%5 = AllocStackInst $?anon_0_ret
+// CHECK-NEXT:  $Reg5           	%6 = StoreStackInst %2 : undefined, %5
+// CHECK-NEXT:  $Reg5           	%7 = LoadStackInst %5
+// CHECK-NEXT:  $Reg6           	%8 = ReturnInst %7
 // CHECK-NEXT:function_end
 
 // CHECK:function simple_test0#0#1(x, y)#2
 // CHECK-NEXT:frame = [x#2, y#2]
 // CHECK-NEXT:%BB0:
-// CHECK-NEXT:  $Reg2           	%0 = HBCCreateEnvironmentInst %S{simple_test0#0#1()#2}
-// CHECK-NEXT:  $Reg3           	%1 = HBCLoadParamInst 1 : number
-// CHECK-NEXT:  $Reg1           	%2 = HBCLoadParamInst 2 : number
-// CHECK-NEXT:  $Reg0           	%3 = HBCStoreToEnvironmentInst %0, %1, [x#2]
-// CHECK-NEXT:  $Reg0           	%4 = HBCStoreToEnvironmentInst %0, %2, [y#2]
-// CHECK-NEXT:  $Reg1           	%5 = HBCLoadFromEnvironmentInst %0, [x#2]
-// CHECK-NEXT:  $Reg2           	%6 = HBCLoadFromEnvironmentInst %0, [y#2]
-// CHECK-NEXT:  $Reg1           	%7 = BinaryOperatorInst 'instanceof', %5, %6
-// CHECK-NEXT:  $Reg0           	%8 = ReturnInst %7
+// CHECK-NEXT:  $Reg0           	%0 = HBCCreateEnvironmentInst %S{simple_test0#0#1()#2}
+// CHECK-NEXT:  $Reg1           	%1 = HBCLoadParamInst 1 : number
+// CHECK-NEXT:  $Reg2           	%2 = HBCLoadParamInst 2 : number
+// CHECK-NEXT:  $Reg3           	%3 = HBCLoadConstInst undefined : undefined
+// CHECK-NEXT:  $Reg4           	%4 = HBCStoreToEnvironmentInst %0, %1, [x#2]
+// CHECK-NEXT:  $Reg4           	%5 = HBCStoreToEnvironmentInst %0, %2, [y#2]
+// CHECK-NEXT:  $Reg4           	%6 = HBCLoadFromEnvironmentInst %0, [x#2]
+// CHECK-NEXT:  $Reg5           	%7 = HBCLoadFromEnvironmentInst %0, [y#2]
+// CHECK-NEXT:  $Reg6           	%8 = BinaryOperatorInst 'instanceof', %6, %7
+// CHECK-NEXT:  $Reg7           	%9 = ReturnInst %8
 // CHECK-NEXT:%BB1:
 // CHECK-NEXT:  $???           	%10 = ReturnInst %3 : undefined
 // CHECK-NEXT:function_end
