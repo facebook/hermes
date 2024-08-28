d9f24724aae2061b6db277de9ce1d4b2cebb71f3
Tzvetan Mikov <tmikov@meta.com>
2024-08-28 08:20:36 -0700

    JSError: look for stack data in the prototype chain (#1497)
    Summary:
    Pull Request resolved: https://github.com/facebook/hermes/pull/1497
    
    Look for stack data in the entire prototype chain in order to accommodate
    usage like the one in the test.
    
    See https://github.com/facebook/hermes/issues/1496
    
    Reviewed By: avp
    
    Differential Revision: D61870728

--- lib/VM/JSError.cpp
+++ lib/VM/JSError.cpp
@@ -54,7 +54,11 @@ void JSErrorBuildMeta(const GCCell *cell, Metadata::Builder &mb) {
 CallResult<Handle<JSError>> JSError::getErrorFromStackTarget(
     Runtime &runtime,
     Handle<JSObject> targetHandle) {
-  if (targetHandle) {
+  MutableHandle<JSObject> mutHnd =
+      runtime.makeMutableHandle<JSObject>(targetHandle.get());
+  targetHandle = mutHnd;
+
+  while (targetHandle) {
     NamedPropertyDescriptor desc;
     bool exists = JSObject::getOwnNamedDescriptor(
         targetHandle,
@@ -68,6 +72,8 @@ CallResult<Handle<JSError>> JSError::getErrorFromStackTarget(
     if (vmisa<JSError>(*targetHandle)) {
       return Handle<JSError>::vmcast(targetHandle);
     }
+
+    mutHnd.set(targetHandle->getParent(runtime));
   }
   return runtime.raiseTypeError(
       "Error.stack getter called with an invalid receiver");
