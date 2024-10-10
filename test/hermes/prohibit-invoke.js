9c8458950ed3efe95a64719ccba56901ca94ea64
Neil Dhar <neildhar@meta.com>
2024-10-10 09:56:16 -0700

    Include stacks for ProhibitInvoke tests
    Summary:
    ProhibitInvoke exceptions are handled specially in that the exception is
    raised in the caller even though the check technically happens in the
    callee.
    
    Add testing for this behaviour to validate that upcoming changes to
    stack traces, the JIT, and ProhibitInvoke itself do not violate the
    expected stack trace.
    
    Reviewed By: avp
    
    Differential Revision: D64159277
    
    fbshipit-source-id: ba418f465a358c0d6b4105d4fd315d5dea4021ac

--- test/hermes/prohibit-invoke.js
+++ test/hermes/prohibit-invoke.js
@@ -13,8 +13,9 @@
   try {
     new arrow1();
   } catch (e) {
-    print("caught", e.name, e.message);
-//CHECK: caught TypeError Function is not a constructor
+    print("caught", e.stack);
+//CHECK: caught TypeError: Function is not a constructor
+//CHECK-NEXT: at global ({{.*}}prohibit-invoke.js:14:15)
   }
 
   arrow1();
@@ -37,15 +38,17 @@ globalThis.a = "a";
   try {
     new obj.m1();
   } catch (e) {
-    print(e);
+    print(e.stack);
 //CHECK-NEXT: TypeError: Function is not a constructor
+//CHECK-NEXT: at global ({{.*}}prohibit-invoke.js:39:15)
   }
 
   try {
     new obj[computedKey]();
   } catch (e) {
-    print(e);
+    print(e.stack);
 //CHECK-NEXT: TypeError: Function is not a constructor
+//CHECK-NEXT: at global ({{.*}}prohibit-invoke.js:47:25)
   }
 })();
 
