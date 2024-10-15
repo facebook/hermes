e61ed5d9cf096b5165c96ef87f656f385b844461
Danny Su <dsc@meta.com>
2024-10-14 21:55:49 -0700

    feat: add tvOS support (#1529) (#1529)
    Summary:
    Original Author: douglowder@mac.com
    Original Git: 924f1d1114aa413275e5bff295bbbf2773b420eb
    Original Reviewed By: tmikov
    Original Revision: D64030986
    
    Add Apple TV support for Hermes.
    
    *Note:* This support requires a few source file changes to work around code that will not compile for tvOS. Comments containing the compiler errors have been added.
    
    Pull Request resolved: https://github.com/facebook/hermes/pull/1529
    
    Reviewed By: neildhar
    
    Differential Revision: D64362592
    
    fbshipit-source-id: 6c3a698b1d87ac444869f232963a9323e8b1a05f

--- utils/build-ios-framework.sh
+++ utils/build-ios-framework.sh
@@ -8,12 +8,18 @@
 
 if [ ! -d destroot/Library/Frameworks/universal/hermes.xcframework ]; then
     ios_deployment_target=$(get_ios_deployment_target)
+    visionos_deployment_target=$(get_visionos_deployment_target)
+    tvos_deployment_target=$(get_tvos_deployment_target)
 
     build_apple_framework "iphoneos" "arm64" "$ios_deployment_target"
     build_apple_framework "iphonesimulator" "x86_64;arm64" "$ios_deployment_target"
     build_apple_framework "catalyst" "x86_64;arm64" "$ios_deployment_target"
+    build_apple_framework "xros" "arm64" "$visionos_deployment_target"
+    build_apple_framework "xrsimulator" "arm64" "$visionos_deployment_target"
+    build_apple_framework "appletvos" "arm64" "$tvos_deployment_target"
+    build_apple_framework "appletvsimulator" "x86_64;arm64" "$tvos_deployment_target"
 
-    create_universal_framework "iphoneos" "iphonesimulator" "catalyst"
+    create_universal_framework "iphoneos" "iphonesimulator" "catalyst" "xros" "xrsimulator" "appletvos" "appletvsimulator"
 else
     echo "Skipping; Clean \"destroot\" to rebuild".
 fi
