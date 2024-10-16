7c946f5523c0ecec67b8a54dfe8bd1aa8a46df3d
Neil Dhar <neildhar@meta.com>
2024-10-16 16:02:34 -0700

    Throw register stack overflow in parent in native backend
    Summary:
    Like the interpreter, the native backend should throw a register stack
    overflow exception before setting up the callee stack.
    
    With this, we can also start validating that the top few frames of the
    stack trace match those created by the interpreter.
    
    Reviewed By: avp
    
    Differential Revision: D64432363
    
    fbshipit-source-id: f935e450765f36fbcc1dad786f3fb6c73208ac75

--- test/hermes/regress-register-stack-overflow.js
+++ test/hermes/regress-register-stack-overflow.js
@@ -6,7 +6,7 @@
  */
 
 // RUN: %hermes -max-register-stack=256 %s | %FileCheck --match-full-lines %s
-// RUN: %shermes -exec -Wx,-max-register-stack=256 %s | %FileCheck --match-full-lines %s
+// RUN: %shermes -exec -Wx,-max-register-stack=256 -g %s | %FileCheck --match-full-lines %s
 
 // Test cases of stack overflows where there is either JS mutual recursion or
 // native C++ recursion in the interpreter. This checks that the handling for
@@ -22,12 +22,10 @@ function test() {
     try {
         foo();
     } catch (e){
-        print(e.message);
+        print(e.stack);
     }
 }
 
-// TODO: For single function recursion, we should be able to validate the top
-//       few stack frames, but these currently differ between shermes and hermes.
 // Test a simple foo-foo recursive call, which tests handling in tail calls in
 // the interpreter.
 print("fooCallee: foo")
@@ -35,14 +33,20 @@ fooCallee = foo;
 test();
 
 // CHECK: fooCallee: foo
-// CHECK-NEXT: Maximum call stack size exceeded
+// CHECK-NEXT: RangeError: Maximum call stack size exceeded
+// CHECK-NEXT:   at foo ({{.*}}.js:18:14)
+// CHECK-NEXT:   at foo ({{.*}}.js:18:14)
+// CHECK-NEXT:   at foo ({{.*}}.js:18:14)
 
 // Test a call to foo.bind(), which forces a recursive interpreter invocation.
 print("fooCallee: foo.bind()")
 fooCallee = foo.bind();
 test();
 // CHECK: fooCallee: foo.bind()
-// CHECK-NEXT: Maximum call stack size exceeded
+// CHECK-NEXT: RangeError: Maximum call stack size exceeded
+// CHECK-NEXT:   at foo ({{.*}}.js:18:14)
+// CHECK-NEXT:   at foo ({{.*}}.js:18:14)
+// CHECK-NEXT:   at foo ({{.*}}.js:18:14)
 
 // Test mutual recursion with different mixes of tail and recursive calls.
 print("Mutual recursion");
@@ -57,7 +61,7 @@ for(fooCallee of [bar, bar.bind()]){
 }
 
 // CHECK: Mutual recursion
-// CHECK-NEXT: Maximum call stack size exceeded
-// CHECK-NEXT: Maximum call stack size exceeded
-// CHECK-NEXT: Maximum call stack size exceeded
-// CHECK-NEXT: Maximum call stack size exceeded
+// CHECK: RangeError: Maximum call stack size exceeded
+// CHECK: RangeError: Maximum call stack size exceeded
+// CHECK: RangeError: Maximum call stack size exceeded
+// CHECK: RangeError: Maximum call stack size exceeded
