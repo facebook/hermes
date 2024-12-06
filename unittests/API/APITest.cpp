ac379647cfd47fbb2601b8d8baf2e6283ec0d564
Chi Tsai <chitsai@meta.com>
2024-12-06 01:35:18 -0800

    Add utf16 implementation for Hermes runtime
    Summary:
    Adds the utf16 implementation for SH runtime. This makes use of the SH
    StringView implementation which already stores the ASCII or UTF16 string
    content.
    
    Reviewed By: avp
    
    Differential Revision: D65366621
    
    fbshipit-source-id: 9525c490fcad813f3973a0463d685d9419db844a

--- unittests/API/APITest.cpp
+++ unittests/API/APITest.cpp
@@ -1253,6 +1253,31 @@ TEST_P(HermesRuntimeTest, NativeExceptionDoesNotUseGlobalError) {
       test.call(*rt).getString(*rt).utf8(*rt));
 }
 
+TEST_P(HermesRuntimeTest, UTF16Test) {
+  String ascii = String::createFromUtf8(*rt, "z");
+  EXPECT_EQ(ascii.utf16(*rt), u"z");
+
+  String foobar = String::createFromUtf8(*rt, "foobar");
+  EXPECT_EQ(foobar.utf16(*rt), u"foobar");
+
+  String chineseHello = String::createFromUtf8(*rt, "你好");
+  EXPECT_EQ(chineseHello.utf16(*rt), u"你好");
+
+  String thumbsUpEmoji = String::createFromUtf8(*rt, "👍");
+  EXPECT_EQ(thumbsUpEmoji.utf16(*rt), u"👍");
+
+  String combined = String::createFromUtf8(*rt, "foobar👍你好");
+  EXPECT_EQ(combined.utf16(*rt), u"foobar👍你好");
+
+  // Thumbs up emoji is encoded as 0xd83d 0xdc4d. These test UTF16 with lone
+  // high and low surrogates.
+  String loneHighSurrogate = eval("'\\ud83d'").getString(*rt);
+  EXPECT_EQ(loneHighSurrogate.utf16(*rt), std::u16string(u"\xd83d"));
+
+  String loneLowSurrogate = eval("'\\udc4d'").getString(*rt);
+  EXPECT_EQ(loneLowSurrogate.utf16(*rt), std::u16string(u"\xdc4d"));
+}
+
 INSTANTIATE_TEST_CASE_P(
     Runtimes,
     HermesRuntimeTest,
