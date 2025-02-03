6afa03a6a5049965892357f7c30bbc377070e8c9
Neil Dhar <neildhar@meta.com>
2025-02-03 10:51:56 -0800

    Disable large_obj_prop_limit.js under MallocGC
    Summary: MallocGC has a much larger property limit, so disable the test.
    
    Reviewed By: dannysu
    
    Differential Revision: D69007363
    
    fbshipit-source-id: 1dc27f15595847cdbdac0c0244d66c872a2ca685

--- test/hermes/literal_obj_prop_limit.js
+++ test/hermes/literal_obj_prop_limit.js
@@ -6,7 +6,7 @@
  */
 
 // RUN: %hermes %s | (! %hermes - 2>&1 -gc-sanitize-handles=0 ) | %FileCheck --match-full-lines %s
-// REQUIRES: !slow_debug
+// REQUIRES: !slow_debug && !gc_malloc
 
 // Print a huge object literal with 300,000 entries.
 
