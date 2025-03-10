263778e2c744455739691fc4ef4079695aa718ea
Charles Pritchard <chuck@jumis.com>
2025-03-09 22:05:14 -0700

    Update raw_ostream.cpp

--- external/llvh/lib/Support/raw_ostream.cpp
+++ external/llvh/lib/Support/raw_ostream.cpp
@@ -737,10 +737,10 @@ uint64_t raw_fd_ostream::seek(uint64_t off) {
 #ifdef _WIN32
   pos = ::_lseeki64(FD, off, SEEK_SET);
 # FIXME: https://reviews.llvm.org/D139752?id=484363
-#elif defined(HAVE_LSEEK64)
+#elif defined(NOT_HAVE_LSEEK64)
   pos = ::lseek64(FD, off, SEEK_SET);
 #else
-  pos = ::lseek(FD, off, SEEK_SET);
+  pos = lseek(FD, off, SEEK_SET);  
 #endif
   if (pos == (uint64_t)-1)
     error_detected(std::error_code(errno, std::generic_category()));
