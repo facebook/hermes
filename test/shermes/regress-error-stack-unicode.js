1b9a84231f5c90a6b90ca783c9bf91774b2d0e95
Gang Zhao <zhaogang@meta.com>
2025-03-18 00:15:09 -0700

    Fix a few LIT test failures in certain platforms
    Summary:
    Fixed two tests that fail in some emulators due to permission issue when
    `cp` files with unicode names.
    Fixed function-inline-directive-error.js when shermes and hermesvm use
    different glibc version.
    
    Reviewed By: neildhar
    
    Differential Revision: D69610941
    
    fbshipit-source-id: ca9cf304548092a5a06a5ff7881c6773ac5e9131

--- test/shermes/regress-error-stack-unicode.js
+++ test/shermes/regress-error-stack-unicode.js
@@ -8,7 +8,7 @@
 // We shouldn't check-in a file with a name containing unicode. So instead,
 // we create the file with the unicode character, containing the contents
 // of this test.
-// RUN: cp %s %t.unicÓde.js && %shermes %t.unicÓde.js -g1 -exec | %FileCheck --match-full-lines %s
+// RUN: cat %s > %t.unicÓde.js && %shermes %t.unicÓde.js -g1 -exec | %FileCheck --match-full-lines %s
 "use strict";
 
 // This filename has a unicode character in it. Make sure that
