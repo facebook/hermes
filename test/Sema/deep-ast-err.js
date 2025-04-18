1cbeadff433c35ebfc794e70f7203ce2c075698c
Gang Zhao <zhaogang@meta.com>
2025-04-18 14:44:28 -0700

    Disable deep-ast-err.js on Linux when ASAN is enabled
    Summary:
    On certain Linux platforms, when building SH with GCC and ASAN enabled,
    stack-overflow is reported on deep-ast-err.js. We can't  add a
    preprocessor flag to specify a smaller AST recursion depth because it
    breaks other tests (far-environment-access.js). So for now let's
    disable it on Linux when ASAN is enabled.
    
    Reviewed By: avp
    
    Differential Revision: D73264897
    
    fbshipit-source-id: 5c53775b925d15e8e7383481721e0c3e71b32c31

--- test/Sema/deep-ast-err.js
+++ test/Sema/deep-ast-err.js
@@ -8,6 +8,9 @@
 // RUN: echo "x" "+"{1..10000} | %shermes -dump-sema - >/dev/null
 // RUN: (! echo "x" "+"{1..10000} | %shermes -typed -parse-flow -dump-sema - 2>&1 ) | %FileCheck -match-full-lines %s
 // RUN: (! echo "x" "+"{1..10000} | %shermes -typed -parse-ts -dump-sema - 2>&1 ) | %FileCheck -match-full-lines %s --check-prefix=CHECK-TS
+// We need to skip this test on Linux with ASAN due to stack overflow (building
+// with GCC).
+// UNSUPPORTED: linux && asan
 
 // CHECK: {{.*}}: error: ft: too many nested expressions/statements/declarations
 // CHECK-TS: {{.*}}: error: ts2flow: too many nested expressions/statements/declarations
