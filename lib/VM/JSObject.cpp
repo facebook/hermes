5f089f4e88d78242156fb7fe7a9f9b9e37a65a60
Aakash Patel <avp@meta.com>
2025-04-23 13:04:12 -0700

    VM: Use Locals in addOwnPropertyImpl
    Summary:
    About to make some changes to addOwnPropertyImpl, and using Locals makes
    it easier to track the values.
    
    Reviewed By: neildhar
    
    Differential Revision: D73394012
    
    fbshipit-source-id: cd15a2e01558e76d23edb7a4c31e56184e0f8546

--- lib/VM/JSObject.cpp
+++ lib/VM/JSObject.cpp
@@ -2871,15 +2871,23 @@ ExecutionStatus JSObject::addOwnPropertyImpl(
       propertyFlags.privateName ||
       !selfHandle->flags_.proxyObject &&
           "Internal non-private properties cannot be added to Proxy objects");
+
+  struct : public Locals {
+    PinnedValue<HiddenClass> clazz;
+  } lv;
+  LocalsRAII lraii{runtime, &lv};
+
   // Add a new property to the class.
   // TODO: if we check for OOM here in the future, we must undo the slot
   // allocation.
-  auto addResult = HiddenClass::addProperty(
-      runtime.makeHandle(selfHandle->clazz_), runtime, name, propertyFlags);
+  lv.clazz = selfHandle->clazz_.get(runtime);
+  auto addResult =
+      HiddenClass::addProperty(lv.clazz, runtime, name, propertyFlags);
   if (LLVM_UNLIKELY(addResult == ExecutionStatus::EXCEPTION)) {
     return ExecutionStatus::EXCEPTION;
   }
-  selfHandle->clazz_.setNonNull(runtime, *addResult->first, runtime.getHeap());
+  lv.clazz = *addResult->first;
+  selfHandle->clazz_.setNonNull(runtime, *lv.clazz, runtime.getHeap());
 
   if (LLVM_UNLIKELY(
           allocateNewSlotStorage(
@@ -2889,8 +2897,7 @@ ExecutionStatus JSObject::addOwnPropertyImpl(
   }
 
   // If this is an index-like property, we need to clear the fast path flags.
-  if (LLVM_UNLIKELY(
-          selfHandle->clazz_.getNonNull(runtime)->getHasIndexLikeProperties()))
+  if (LLVM_UNLIKELY(lv.clazz->getHasIndexLikeProperties()))
     selfHandle->flags_.fastIndexProperties = false;
 
   return ExecutionStatus::RETURNED;
