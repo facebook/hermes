0431326630e56f5fa703b03e279ee4b934aa77e5
David Detlefs <ddetlefs@meta.com>
2025-05-06 13:09:23 -0700

    Benchmark for string switches.
    Summary:
    Introduces a microbenchmark emphasizing the performance of for "string
    switches": switch statements whose cases are all literal strings.  The
    next diff will optimizer the performance of this.  (Putting it in a
    base diff so we can get a "before" number.)
    
    Reviewed By: dannysu
    
    Differential Revision: D72581824
    
    fbshipit-source-id: 0045ce475cc748a1dfe5a7d1d1f6f2e545a53776

---
+++ benchmarks/bench-runner/resource/test-suites/micros/switchOfStrings.js
@@ -0,0 +1,75 @@
+var start = new Date();
+(function () {
+  function stringSwitch(s) {
+    switch (s) {
+    case "a":
+      return 1;
+    case "b":
+      return 2;
+    case "c":
+      return 3;
+    case "d":
+      return 4;
+    case "e":
+      return 5;
+    case "f":
+      return 6;
+    case "g":
+      return 7;
+    case "h":
+      return 8;
+    case "i":
+      return 9;
+    case "j":
+      return 10;
+    case "k":
+      return 11;
+    case "l":
+      return 12;
+    case "m":
+      return 13;
+    case "n":
+      return 14;
+    case "o":
+      return 15;
+    case "p":
+      return 16;
+    case "q":
+      return 17;
+    case "r":
+      return 18;
+    case "s":
+      return 19;
+    case "t":
+      return 20;
+    case "u":
+      return 21;
+    case "v":
+      return 22;
+    case "w":
+      return 23;
+    case "x":
+      return 24;
+    case "y":
+      return 25;
+    case "z":
+      return 26;
+    }
+    return 0;
+  }
+
+  var numIter = 500000;
+  var cases = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+               "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+               "u", "v", "w", "x", "y", "z"];
+
+  var sum = 0;
+  for (var i = 0; i < numIter; i++) {
+    for (var j = 0; j < 26; j++) {
+      sum += stringSwitch(cases[j]);
+    }
+  }
+  print(sum);
+})();
+var end = new Date();
+print("Time: " + (end - start));
