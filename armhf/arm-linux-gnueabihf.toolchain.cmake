cc2a3b2b20a53096c31e9a138bc28369ad7f6e28
Tzvetan Mikov <tmikov@meta.com>
2025-07-25 10:28:16 -0700

    Update arm32 toolchain file
    Summary:
    Use Clang-16 by default, set the CPU architecture to armv7-a. Most
    importantly, enable Thumb, which is 7% faster and 25% smaller in my
    tests on a Pi3.
    
    Reviewed By: avp
    
    Differential Revision: D78944090
    
    fbshipit-source-id: c10a2a437653c2aa031017f843ec762f360adecf

--- armhf/arm-linux-gnueabihf.toolchain.cmake
+++ armhf/arm-linux-gnueabihf.toolchain.cmake
@@ -8,10 +8,13 @@
 # Specify the system name and processor
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
+# Specify the Clang compiler
+set(CMAKE_C_COMPILER clang-16)
+set(CMAKE_CXX_COMPILER clang++-16)
 # Specify the target triple for Clang
 set(CMAKE_C_COMPILER_TARGET arm-linux-gnueabihf)
 set(CMAKE_CXX_COMPILER_TARGET arm-linux-gnueabihf)
 set(CMAKE_ASM_COMPILER_TARGET arm-linux-gnueabihf)
-# Ensure the correct architecture is used
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -m32")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32")
+# Ensure the correct architecture is used and enable Thumb-2
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mthumb -march=armv7-a")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mthumb -march=armv7-a")
