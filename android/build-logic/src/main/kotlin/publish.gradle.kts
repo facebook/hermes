32aa4330535e6ff731e2bd10f8fced39fc5f7147
maleckidawid <>
2025-08-22 03:05:19 -0700

    Generated from a GitHub Pull Request. Run 'jf sync' on this diff to load the correct commit data.
    Differential Revision: D79996969

---
+++ android/build-logic/src/main/kotlin/publish.gradle.kts
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+plugins {
+  id("maven-publish")
+  id("signing")
+}
+
+val signingKey: String? = findProperty("SIGNING_KEY")?.toString()
+val signingPwd: String? = findProperty("SIGNING_PWD")?.toString()
+
+val mavenTempLocalUrl = "file:///tmp/maven-local"
+
+publishing {
+  publications {
+    create<MavenPublication>("release") {
+      afterEvaluate {
+        from(components["default"])
+        version = project.version.toString()
+        groupId = project.group.toString()
+        artifactId = "hermes-android"
+      }
+
+      pom {
+        name.set("hermes")
+        description.set("A JavaScript engine optimized for fast start-up of React Native apps")
+        url.set("https://github.com/facebook/hermes")
+
+        developers {
+          developer {
+            id.set("facebook")
+            name.set("Facebook")
+          }
+        }
+
+        licenses {
+          license {
+            name.set("MIT License")
+            url.set("https://github.com/facebook/hermes/blob/HEAD/LICENSE")
+            distribution.set("repo")
+          }
+        }
+
+        scm {
+          url.set("https://github.com/facebook/hermes.git")
+          connection.set("scm:git:https://github.com/facebook/hermes.git")
+          developerConnection.set("scm:git:git@github.com:facebook/hermes.git")
+        }
+      }
+    }
+  }
+
+  repositories {
+    maven {
+      name = "mavenTempLocal"
+      url = uri(mavenTempLocalUrl)
+    }
+  }
+}
+
+if (signingKey != null && signingPwd != null) {
+  logger.info("PGP Key found - Signing enabled")
+  signing {
+    useInMemoryPgpKeys(signingKey, signingPwd)
+    sign(publishing.publications["release"])
+  }
+} else {
+  logger.info("Signing disabled as the PGP key was not found")
+}
