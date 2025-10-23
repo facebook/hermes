461a92696c1dfec4a29effda6361921efa6cb143
Jakub Piasecki <piaseckijakub@meta.com>
2025-10-23 08:35:09 -0700

    Remove unused build scripts in hermes (#1814)
    Summary:
    Pull Request resolved: https://github.com/facebook/hermes/pull/1814
    
    Removes old scripts used to build apple frameworks which became obsolete after D77869024.
    
    Reviewed By: cipolleschi
    
    Differential Revision: D85324112
    
    fbshipit-source-id: 574794b77b8049e9a46e4e49c81c83fd1677e854

--- utils/build-mac-framework.sh
+++ utils/build-mac-framework.sh
@@ -4,7 +4,15 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-. ./utils/build-apple-framework.sh
+if [ "$CI" ]; then
+  set -x
+fi
+set -e
+
+CURR_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+# shellcheck source=xplat/hermes/utils/build-apple-framework.sh
+source "${CURR_SCRIPT_DIR}/build-apple-framework.sh"
+
 if [ ! -d destroot/Library/Frameworks/macosx/hermesvm.framework ]; then
     mac_deployment_target=$(get_mac_deployment_target)
 
