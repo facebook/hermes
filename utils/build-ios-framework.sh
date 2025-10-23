ba39425b94240c91f5866c79b4e2d42bbf764d07
Jakub Piasecki <piaseckijakub@meta.com>
2025-10-23 03:08:28 -0700

    Remove unused build scripts in static_h (#1813)
    Summary:
    
    Removes old scripts used to build apple frameworks which became obsolete after D77869024.
    
    Reviewed By: cipolleschi
    
    Differential Revision: D85324111

--- utils/build-ios-framework.sh
+++ utils/build-ios-framework.sh
@@ -4,37 +4,87 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-. ./utils/build-apple-framework.sh
+if [ "$CI" ]; then
+  set -x
+fi
+set -e
 
-ios_deployment_target=$(get_ios_deployment_target)
-visionos_deployment_target=$(get_visionos_deployment_target)
-tvos_deployment_target=$(get_tvos_deployment_target)
+# Given a specific target, retrieve the right architecture for it
+# $1 the target you want to build. Allowed values: iphoneos, iphonesimulator, catalyst, xros, xrsimulator
+function get_architecture {
+    if [[ $1 == "iphoneos" || $1 == "xros" ]]; then
+      echo "arm64"
+    elif [[ $1 == "iphonesimulator" || $1 == "xrsimulator" ]]; then
+      echo "x86_64;arm64"
+    elif [[ $1 == "appletvos" ]]; then
+      echo "arm64"
+    elif [[ $1 == "appletvsimulator" ]]; then
+      echo "x86_64;arm64"
+    elif [[ $1 == "catalyst" ]]; then
+      echo "x86_64;arm64"
+    else
+      echo "Error: unknown architecture passed $1"
+      exit 1
+    fi
+}
 
-function build_all {
+function get_deployment_target {
+    if [[ $1 == "xros" || $1 == "xrsimulator" ]]; then
+      echo "$(get_visionos_deployment_target)"
+    else # tvOS and iOS use the same deployment target
+      echo "$(get_ios_deployment_target)"
+    fi
+}
+
+# build a single framework
+# $1 is the target to build
+function build_framework {
   if [ ! -d destroot/Library/Frameworks/universal/hermesvm.xcframework ]; then
-    build_apple_framework "iphoneos" "arm64" "$ios_deployment_target"
-    build_apple_framework "iphonesimulator" "x86_64;arm64" "$ios_deployment_target"
-    build_apple_framework "catalyst" "x86_64;arm64" "$ios_deployment_target"
-    build_apple_framework "xros" "arm64" "$visionos_deployment_target"
-    build_apple_framework "xrsimulator" "arm64" "$visionos_deployment_target"
-    build_apple_framework "appletvos" "arm64" "$tvos_deployment_target"
-    build_apple_framework "appletvsimulator" "x86_64;arm64" "$tvos_deployment_target"
+    deployment_target=$(get_deployment_target "$1")
 
-    create_universal_framework "iphoneos" "iphonesimulator" "catalyst" "xros" "xrsimulator" "appletvos" "appletvsimulator"
+    architecture=$(get_architecture "$1")
+
+    build_apple_framework "$1" "$architecture" "$deployment_target"
   else
     echo "Skipping; Clean \"destroot\" to rebuild".
   fi
 }
 
-if [[ $1 == "iphoneos" ]]; then
-  build_apple_framework "iphoneos" "arm64" "$ios_deployment_target"
-elif [[ $1 == "iphonesimulator" ]]; then
-  build_apple_framework "iphonesimulator" "x86_64;arm64" "$ios_deployment_target"
-elif [[ $1 == "appletvos" ]]; then
-  build_apple_framework "appletvos" "arm64" "$tvos_deployment_target"
-elif [[ $1 == "appletvsimulator" ]]; then
-  build_apple_framework "appletvsimulator" "x86_64;arm64" "$tvos_deployment_target"
+# group the frameworks together to create a universal framework
+function build_universal_framework {
+    if [ ! -d destroot/Library/Frameworks/universal/hermesvm.xcframework ]; then
+        create_universal_framework "iphoneos" "iphonesimulator" "catalyst" "xros" "xrsimulator" "appletvos" "appletvsimulator"
+    else
+        echo "Skipping; Clean \"destroot\" to rebuild".
+    fi
+}
+
+# single function that builds sequentially iphoneos, iphonesimulator and catalyst
+# this is used to preserve backward compatibility
+function create_framework {
+    if [ ! -d destroot/Library/Frameworks/universal/hermesvm.xcframework ]; then
+        build_framework "iphoneos"
+        build_framework "iphonesimulator"
+        build_framework "appletvos"
+        build_framework "appletvsimulator"
+        build_framework "catalyst"
+        build_framework "xros"
+        build_framework "xrsimulator"
+        build_universal_framework
+    else
+        echo "Skipping; Clean \"destroot\" to rebuild".
+    fi
+}
+
+
+CURR_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+# shellcheck source=xplat/hermes/utils/build-apple-framework.sh
+source "${CURR_SCRIPT_DIR}/build-apple-framework.sh"
+
+if [[ -z $1 ]]; then
+  create_framework
+elif [[ $1 == "build_framework" ]]; then
+  build_universal_framework
 else
-  echo "Build for all supported platforms and create universal framework."
-  build_all
+  build_framework "$1"
 fi
