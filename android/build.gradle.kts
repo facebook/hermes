0a82bd66f462cb0f86c20c5d31b8914e116346db
Nivaldo Bondança <nbondanca@meta.com>
2026-08-02 08:20:26 -0700

    Update ktfmt component on FBS:master
    Reviewed By: cortinico
    
    Differential Revision: D112366935
    
    fbshipit-source-id: e0b509473d93d3fb4a465a3007bd0a7b63fa553d

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -98,7 +98,7 @@ val installCMake by
     tasks.registering(CustomExecTask::class) {
       onlyIfProvidedPathDoesNotExists.set(cmakePath)
       commandLine(
-          windowsAwareCommandLine(getSDKManagerPath(), "--install", "cmake;${cmakeVersion}")
+          windowsAwareCommandLine(getSDKManagerPath(), "--install", "cmake;${cmakeVersion}"),
       )
     }
 
