6fbc3706d600abaf465bc50d988fb45c5f557e8e
Tzvetan Mikov <tmikov@meta.com>
2026-08-02 17:37:42 -0700

    Back out "Guard dead local-eval block with `#if 0` (clang17 -Wunreachable-code)"
    Summary:
    Original commit changeset: 8ce9275178c4
    
    Original Phabricator Diff: D113782482
    
    Reviewed By: micleo2
    
    Differential Revision: D114544914
    
    fbshipit-source-id: 386db81067abf0244f07eefd178377a4b12e8115

--- lib/Sema/SemanticResolver.cpp
+++ lib/Sema/SemanticResolver.cpp
@@ -1929,16 +1929,13 @@ void SemanticResolver::visitFunctionBodyAfterParamsVisited(
     return;
 
   // Check for local eval and run the unresolver pass in non-strict mode.
-  // TODO: enable this when non-strict direct eval is supported. Guarded with
-  // `#if 0` rather than `if (false && ...)` so clang17 does not flag the body
-  // as -Werror,-Wunreachable-code on the Windows clang17 toolchain.
-#if 0
+  // TODO: enable this when non-strict direct eval is supported.
   LexicalScope *lexScope = curFunctionInfo()->getFunctionBodyScope();
-  if (lexScope->localEval && !curFunctionInfo()->strict) {
-    uint32_t depth = lexScope->depth;
-    Unresolver::run(semCtx_, depth, node);
-  }
-#endif
+  if ((false))
+    if (lexScope->localEval && !curFunctionInfo()->strict) {
+      uint32_t depth = lexScope->depth;
+      Unresolver::run(semCtx_, depth, node);
+    }
 
   // Determine whether the function can run the implicit return.
   if (!sm_.getErrorCount()) {
