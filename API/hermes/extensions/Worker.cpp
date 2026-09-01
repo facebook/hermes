860c2d3e359f0f49ce4c802421e3dbd57b9e9a1e
Chi Tsai <chitsai@meta.com>
2026-09-01 09:01:17 -0700

    Graft "[SH] Let WebWorkers run script after creation" to shermes/stable
    Summary:
    Graft of D90875624 (authored by Chi Tsai) to shermes/stable.
    Grafted `9f794f41e912` from `xplat/static_h` to
    `xplat/shermes/stable`; applied with no conflicts.
    
    This diff allows the source code script provides by the user to
    actually be executed in a separate thread and runtime owned by the
    Worker object.
    
    Upon failure of any kind in the Worker thread, just return. Later in
    the diff stack, `onerror` handlers will be supported.
    
    When the Worker NS object is destroyed, it will request the Worker
    runtime to stop execution. Then wait for the Worker thread to join.
    
    Differential Revision: D118200637

--- API/hermes/extensions/Worker.cpp
+++ API/hermes/extensions/Worker.cpp
@@ -8,7 +8,11 @@
 #ifdef JSI_UNSTABLE
 #include "Worker.h"
 #include "Intrinsics.h"
+#include "hermes/Public/RuntimeConfig.h"
+#include "hermes/hermes.h"
 #include "llvh/Support/Compiler.h"
+
+#include <thread>
 
 namespace facebook {
 namespace hermes {
@@ -23,8 +27,62 @@ namespace {
 /// its execution and the thread will be joined.
 class WorkerNativeState : public jsi::NativeState {
  public:
-  WorkerNativeState() = default;
+  WorkerNativeState(std::unique_ptr<jsi::Runtime> workerRuntime)
+      : workerRuntime(std::move(workerRuntime)) {};
+  ~WorkerNativeState();
+
+  /// Start and assign the Worker a new thread to run \p script using the Worker
+  /// runtime.
+  void startWorkerThread(std::string script);
+
+  /// Worker runtime. Created when the Worker is created and used to execute all
+  /// JS for the Worker.
+  std::unique_ptr<jsi::Runtime> workerRuntime;
+
+ private:
+  /// Worker thread used to run all JS execution and event loop processing in
+  /// the worker. When the Worker is terminated, the thread is joined.
+  std::thread workerThread_;
 };
+
+/// Request the \p workerRuntime to stop execution. May be called multiple
+/// times.
+void setTerminationState(jsi::Runtime &workerRuntime) {
+  // Request the Worker runtime to terminate the execution at a convenient
+  // time. The timeout exception thrown doesn't matter because the Worker
+  // will be terminated.
+  auto *hermesInterface = jsi::castInterface<IHermes>(&workerRuntime);
+  assert(hermesInterface && "IHermes is not supported");
+  hermesInterface->asyncTriggerTimeout();
+}
+
+WorkerNativeState::~WorkerNativeState() {
+  setTerminationState(*workerRuntime);
+  // If the Worker thread is still active, wait for it to finish.
+  if (workerThread_.joinable()) {
+    workerThread_.join();
+  }
+}
+
+void WorkerNativeState::startWorkerThread(std::string script) {
+  // workerRuntime will outlive this lambda scope because we explicitly join
+  // the worker thread in the WorkerNativeState destructor. Thus, it is safe
+  // to capture the workerRuntime here.
+  workerThread_ = std::thread(
+      [scriptCopy = std::move(script), workerRuntime = workerRuntime.get()]() {
+        try {
+          workerRuntime->evaluateJavaScript(
+              std::make_unique<jsi::StringBuffer>(std::move(scriptCopy)), "");
+        } catch (const jsi::JSIException &) {
+          // Script failed. End worker thread.
+          // This is different from HTML behavior, where Worker report these
+          // error events, and an 'onerror' handler can be attached to the
+          // Worker to handle the event in the main thread. However, that event
+          // handling is not implemented yet.
+          return;
+        }
+      });
+}
 
 /// Called by the JS constructor in `11-Worker.js` to mark the first argument.
 /// The arguments in \p args must be provided in the following order:
@@ -49,8 +107,15 @@ jsi::Value initializeWorker(
   // This is provided by the `11-Worker.js` script, so it should always be an
   // Object.
   auto self = args[0].asObject(rt);
-  self.setNativeState(rt, std::make_shared<WorkerNativeState>());
+  auto *api = jsi::castInterface<IHermesRootAPI>(makeHermesRootAPI());
+  auto workerRuntime = api->makeHermesRuntime(::hermes::vm::RuntimeConfig());
+  auto workerNativeState =
+      std::make_shared<WorkerNativeState>(std::move(workerRuntime));
+  self.setNativeState(rt, workerNativeState);
 
+  // Start the worker thread
+  std::string script = args[1].asString(rt).utf8(rt);
+  workerNativeState->startWorkerThread(std::move(script));
   return jsi::Value::undefined();
 }
 
